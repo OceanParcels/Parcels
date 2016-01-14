8e5052f7ec329dcc8c29ec2a7d2489b42e32f125
Michael Lange <michael.lange@imperial.ac.uk>
2016-01-14 11:49:04 +0000

    JIT: Implement linear temporal interpolation for JIT mode

--- include/parcels.h
+++ include/parcels.h
@@ -17,6 +17,13 @@ static inline int search_linear_float(float x, int i, int size, float *xvals)
     return i;
 }
 
+/* Local linear search to update time index */
+static inline int search_linear_double(double t, int i, int size, double *tvals)
+{
+    while (i < size-1 && t > tvals[i+1]) ++i;
+    while (i > 0 && t < tvals[i]) --i;
+    return i;
+}
 
 /* Bilinear interpolation routine for 2D grid */
 static inline float spatial_interpolation_bilinear(float x, float y, int i, int j, int ydim,
@@ -37,10 +44,21 @@ static inline float temporal_interpolation_linear(float x, float y, int xi, int 
 {
   /* Cast data array intp data[time][lat][lon] as per NEMO convention */
   float (*data)[f->xdim][f->ydim] = (float (*)[f->xdim][f->ydim]) f->data;
-  float x0;
-  int i = xi, j = yi;
+  float f0, f1;
+  double t0, t1;
+  int i = xi, j = yi, t = 0;
+  /* Identify grid cell to sample through local linear search */
   i = search_linear_float(x, i, f->xdim, f->lon);
   j = search_linear_float(y, j, f->ydim, f->lat);
-  x0 = spatial_interpolation_bilinear(x, y, i, j, f->ydim, f->lon, f->lat, (float**)(data[0]));
-  return x0;
+  /* Find time index for temporal interpolation */
+  t = search_linear_double(time, t, f->tdim, f->time);
+
+  if (t < f->tdim-1) {
+    t0 = f->time[t]; t1 = f->time[t+1];
+    f0 = spatial_interpolation_bilinear(x, y, i, j, f->ydim, f->lon, f->lat, (float**)(data[t]));
+    f1 = spatial_interpolation_bilinear(x, y, i, j, f->ydim, f->lon, f->lat, (float**)(data[t+1]));
+    return f0 + (f1 - f0) * (float)((time - t0) / (t1 - t0));
+  } else {
+    return spatial_interpolation_bilinear(x, y, i, j, f->ydim, f->lon, f->lat, (float**)(data[t]));
+  }
 }
