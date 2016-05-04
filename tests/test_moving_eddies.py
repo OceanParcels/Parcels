Tests: Fixing grid units for peninsula and moving_eddies

tests/test_moving_eddies.py
@@ -1,4 +1,4 @@
-from parcels import Grid, Particle, JITParticle, AdvectionRK4, AdvectionEE
+from parcels import Grid, Particle, JITParticle, AdvectionRK4, AdvectionEE, Geographic, GeographicPolar
 from argparse import ArgumentParser
 import numpy as np
 import math
@@ -61,8 +61,8 @@ def moving_eddies_grid(xdim=200, ydim=350):
         U[:, :-1, t] = np.diff(P[:, :, t], axis=1) / dy / corio_0 * g
         U[:, -1, t] = U[:, -2, t]  # Fill in the last row
 
-    return Grid.from_data(U, lon, lat, V, lon, lat,
-                          depth, time, field_data={'P': P})
+    return Grid.from_data(U, lon, lat, V, lon, lat, depth, time, field_data={'P': P},
+                          u_units=GeographicPolar(), v_units=Geographic())
 
 
 def moving_eddies_example(grid, npart=2, mode='jit', verbose=False,