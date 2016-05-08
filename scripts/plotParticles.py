4848b45099aa116adb136d1240b20487dfd89b43
jacketless <jscuttphillips@gmail.com>
2016-05-08 16:24:13 +1000

    Bug fixing and minor additions for aesthetics
    A bugs squashed
    Options for xy-limits of plot and separate colormaps for background and
    particles
    Baseman land drawing dependency removed

--- scripts/plotParticles.py
+++ scripts/plotParticles.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from netCDF4 import Dataset
 import numpy as np
-from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 import matplotlib.animation as animation
 
 
-def particleplotting(filename, psize, recordedvar, backgroundfield, cmap, drawland, mode):
+def particleplotting(filename, psize, recordedvar, rcmap, backgroundfield, dimensions, cmap, limits, mode):
     """Quick and simple plotting of PARCELS trajectories"""
 
     pfile = Dataset(filename, 'r')
@@ -16,15 +15,21 @@ def particleplotting(filename, psize, recordedvar, backgroundfield, cmap, drawla
     time = pfile.variables['time']
     z = pfile.variables['z']
 
-    if(recordedvar is not 'none'):
+    if limits is -1:
+        limits = [np.min(lon), np.max(lon), np.min(lat), np.max(lat)]
+
+    if recordedvar is not 'none':
+        print('Particles coloured by: %s' % recordedvar)
+        rMin = np.min(pfile.variables[recordedvar])
         rMax = np.max(pfile.variables[recordedvar])
-        record = pfile.variables[recordedvar]
+        print('Min = %f, Max = %f' % (rMin, rMax))
+        record = (pfile.variables[recordedvar]-rMin)/(rMax-rMin)
 
     if backgroundfield is not 'none':
         bfile = Dataset(backgroundfield.values()[0], 'r')
-        bX = bfile.variables['x']
-        bY = bfile.variables['y']
-        bT = bfile.variables['time_counter']
+        bX = bfile.variables[dimensions[0]]
+        bY = bfile.variables[dimensions[1]]
+        bT = bfile.variables[dimensions[2]]
         # Find the variable that exists across at least two spatial and one time dimension
         if backgroundfield.keys()[0] is 'none':
             def checkShape(var):
@@ -34,9 +39,10 @@ def particleplotting(filename, psize, recordedvar, backgroundfield, cmap, drawla
             for v in bfile.variables:
                 if checkShape(bfile.variables[v]):
                     bVar = bfile.variables[v]
+                    print('Background variable is %s' % v)
+                    break
         else:
             bVar = bfile.variables[backgroundfield.keys()[0]]
-        bMax = np.max(bVar)
 
     if mode == '3d':
         fig = plt.figure(1)
@@ -48,54 +54,45 @@ def particleplotting(filename, psize, recordedvar, backgroundfield, cmap, drawla
         ax.set_zlabel('Depth')
     elif mode == '2d':
         if backgroundfield is not 'none':
-            plt.contourf(bX[:], bY[:], bVar[0, 0, :, :], zorder=-1, vmin=0, vmax=bMax,
-                         levels=np.linspace(0, bMax, 10), xlim=[np.amin(lon), np.amax(lon)],
-                         ylim=[np.amin(lat), np.amax(lat)], cmap=cmap)
+            plt.contourf(bX[:], bY[:], bVar[0, 0, :, :], zorder=-1, vmin=0, vmax=np.max(bVar[0, 0, :, :]),
+                         levels=np.linspace(0, np.max(bVar[0, 0, :, :]), 100), xlim=[limits[0], limits[1]],
+                         ylim=[limits[2], limits[3]], cmap=cmap)
             plt.plot(np.transpose(lon), np.transpose(lat), '.-', linewidth=psize,
                      markersize=psize, c='black')
+            plt.xlim([limits[0], limits[1]])
+            plt.ylim([limits[2], limits[3]])
         else:
             plt.plot(np.transpose(lon), np.transpose(lat), '.-', linewidth=psize,
                      markersize=psize)
+            plt.xlim([limits[0], limits[1]])
+            plt.ylim([limits[2], limits[3]])
+
         plt.xlabel('Longitude')
         plt.ylabel('Latitude')
-        if drawland:
-            m = Basemap(width=12000000, height=9000000, projection='cyl',
-                        resolution='c', llcrnrlon=np.amin(lon), llcrnrlat=np.amin(lat),
-                        urcrnrlon=np.amax(lon), urcrnrlat=np.amax(lat))
-            m.drawcoastlines()
-            m.fillcontinents(color='forestgreen', lake_color='aqua')
 
     elif mode == 'movie2d':
 
         fig = plt.figure(1)
-        print((np.amin(lon), np.amax(lon), np.amin(lat), np.amax(lat)))
-        ax = plt.axes(xlim=[np.amin(lon), np.amax(lon)], ylim=[np.amin(lat), np.amax(lat)])
-        ax.set_xlim([np.amin(lon), np.amax(lon)])
-        ax.set_ylim([np.amin(lat), np.amax(lat)])
+        ax = plt.axes(xlim=[limits[0], limits[1]], ylim=[limits[2], limits[3]])
+        ax.set_xlim(limits[0], limits[1])
+        ax.set_ylim(limits[2], limits[3])
         scat = ax.scatter(lon[:, 0], lat[:, 0], s=psize, c='black')
 
         def animate(i):
             ax.cla()
-            if drawland:
-                m.drawcoastlines()
-                m.fillcontinents(color='forestgreen', lake_color='aqua')
             if recordedvar is not 'none':
-                scat = ax.scatter(lon[:, i], lat[:, i], s=psize, c=record[:, i]/rMax, cmap=cmap)
+                scat = ax.scatter(lon[:, i], lat[:, i], s=psize, c=record[:, i], cmap=rcmap, vmin=0, vmax=1)
             else:
                 scat = ax.scatter(lon[:, i], lat[:, i], s=psize, c='white', edgecolors='black')
-            ax.set_xlim([np.amin(lon), np.amax(lon)])
-            ax.set_ylim([np.amin(lat), np.amax(lat)])
+            ax.set_xlim([limits[0], limits[1]])
+            ax.set_ylim([limits[2], limits[3]])
             if backgroundfield is not 'none':
                 field_time = np.argmax(bT > time[0, i]) - 1
-                plt.contourf(bX[:], bY[:], bVar[field_time, 0, :, :], zorder=-1, vmin=0, vmax=bMax,
-                             levels=np.linspace(0, bMax, 10), xlim=[np.amin(lon), np.amax(lon)],
-                             ylim=[np.amin(lat), np.amax(lat)], cmap=cmap)
+                plt.contourf(bX[:], bY[:], bVar[field_time, 0, :, :], zorder=-1, vmin=0, vmax=np.max(bVar[field_time, 0, :, :]),
+                             levels=np.linspace(0, np.max(bVar[field_time, 0, :, :]), 100), xlim=[limits[0], limits[1]],
+                             ylim=[limits[2], limits[3]], cmap=cmap)
+            plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
             return scat,
-
-        if drawland:
-            m = Basemap(width=12000000, height=9000000, projection='cyl',
-                        resolution='c', llcrnrlon=np.round(np.amin(lon)), llcrnrlat=np.amin(lat),
-                        urcrnrlon=np.amax(lon), urcrnrlat=np.amax(lat))
 
         anim = animation.FuncAnimation(fig, animate, frames=np.arange(1, lon.shape[1]),
                                        interval=100, blit=False)
@@ -116,10 +113,14 @@ if __name__ == "__main__":
                    help='Name of variable to display in background field')
     p.add_argument('-c', '--colourmap', type=str, default='jet',
                    help='Colourmap for field data')
-    p.add_argument('-s', '--size', type=int, default='none',
+    p.add_argument('-cr', '--colourmap_recorded', type=str, default='autumn',
+                   help='Colourmap for particle recorded data')
+    p.add_argument('-s', '--size', type=str, default='none',
                    help='Size of drawn particles and tracks')
-    p.add_argument('-l', '--landdraw', type=bool, default=False,
-                   help='Boolean for whether to draw land using mpl.basemap package')
+    p.add_argument('-l', '--limits', type=float, nargs=4, default=-1,
+                   help='Limits for plotting, given min_lon, max_lon, min_lat, max_lat')
+    p.add_argument('-d', '--dimensions', type=str, nargs=3, default=['nav_lon', 'nav_lat', 'time_counter'],
+                   help='Name of background field dimensions in order of lon, lat, and time')
     args = p.parse_args()
 
     if args.background is not 'none':
@@ -127,9 +128,11 @@ if __name__ == "__main__":
 
     if args.size is 'none':
         if args.mode is 'movie2d':
-            args.size = 60
+            psize = 60
         else:
-            args.size = 1
+            psize = 1
+    else:
+        psize = int(args.size)
 
-    particleplotting(args.particlefile, args.size, args.recordedvar, args.background, args.colourmap, args.landdraw,
-                     mode=args.mode)
+    particleplotting(args.particlefile, psize, args.recordedvar, args.colourmap_recorded, args.background, args.dimensions, args.colourmap,
+                     args.limits, mode=args.mode)
