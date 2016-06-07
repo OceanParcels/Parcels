af029a5a85591cd1ce89665bab29373839834494
Michael Lange <michael.lange@imperial.ac.uk>
2016-06-07 15:27:06 +0100

    Advection: Adding Explicit Euler to the test battery with reduced `rtol`

--- tests/test_advection.py
+++ tests/test_advection.py
@@ -1,4 +1,5 @@
-from parcels import Grid, Particle, JITParticle, AdvectionRK4
+from parcels import Grid, Particle, JITParticle
+from parcels import AdvectionEE, AdvectionRK4
 import numpy as np
 import pytest
 import math
@@ -6,6 +7,7 @@ from datetime import timedelta as delta
 
 
 ptype = {'scipy': Particle, 'jit': JITParticle}
+kernel = {'EE': AdvectionEE, 'RK4': AdvectionRK4}
 
 # Some constants
 f = 1.e-4
@@ -80,17 +82,18 @@ def grid_stationary(xdim=100, ydim=100, maxtime=delta(hours=6)):
 
 
 @pytest.mark.parametrize('mode', ['scipy', 'jit'])
-def test_stationary_eddy(grid_stationary, mode, npart=1):
+@pytest.mark.parametrize('method, rtol', [('EE', 1e-2), ('RK4', 1e-5)])
+def test_stationary_eddy(grid_stationary, mode, method, rtol, npart=1):
     grid = grid_stationary
     lon = np.linspace(12000, 21000, npart, dtype=np.float32)
     lat = np.linspace(12500, 12500, npart, dtype=np.float32)
     pset = grid.ParticleSet(size=npart, pclass=ptype[mode], lon=lon, lat=lat)
     endtime = delta(hours=6).total_seconds()
-    pset.execute(AdvectionRK4, dt=delta(minutes=3), endtime=endtime)
+    pset.execute(kernel[method], dt=delta(minutes=3), endtime=endtime)
     exp_lon = [truth_stationary(x, y, endtime)[0] for x, y, in zip(lon, lat)]
     exp_lat = [truth_stationary(x, y, endtime)[1] for x, y, in zip(lon, lat)]
-    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=1e-5)
-    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=1e-5)
+    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=rtol)
+    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=rtol)
 
 
 def truth_moving(x_0, y_0, t):
@@ -115,17 +118,18 @@ def grid_moving(xdim=100, ydim=100, maxtime=delta(hours=6)):
 
 
 @pytest.mark.parametrize('mode', ['scipy', 'jit'])
-def test_moving_eddy(grid_moving, mode, npart=1):
+@pytest.mark.parametrize('method, rtol', [('EE', 1e-2), ('RK4', 1e-5)])
+def test_moving_eddy(grid_moving, mode, method, rtol, npart=1):
     grid = grid_moving
     lon = np.linspace(12000, 21000, npart, dtype=np.float32)
     lat = np.linspace(12500, 12500, npart, dtype=np.float32)
     pset = grid.ParticleSet(size=npart, pclass=ptype[mode], lon=lon, lat=lat)
     endtime = delta(hours=6).total_seconds()
-    pset.execute(AdvectionRK4, dt=delta(minutes=3), endtime=endtime)
+    pset.execute(kernel[method], dt=delta(minutes=3), endtime=endtime)
     exp_lon = [truth_moving(x, y, endtime)[0] for x, y, in zip(lon, lat)]
     exp_lat = [truth_moving(x, y, endtime)[1] for x, y, in zip(lon, lat)]
-    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=1e-5)
-    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=1e-5)
+    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=rtol)
+    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=rtol)
 
 
 def truth_decaying(x_0, y_0, t):
@@ -156,14 +160,15 @@ def grid_decaying(xdim=100, ydim=100, maxtime=delta(hours=6)):
 
 
 @pytest.mark.parametrize('mode', ['scipy', 'jit'])
-def test_decaying_eddy(grid_decaying, mode, npart=1):
+@pytest.mark.parametrize('method, rtol', [('EE', 1e-2), ('RK4', 1e-5)])
+def test_decaying_eddy(grid_decaying, mode, method, rtol, npart=1):
     grid = grid_decaying
     lon = np.linspace(12000, 21000, npart, dtype=np.float32)
     lat = np.linspace(12500, 12500, npart, dtype=np.float32)
     pset = grid.ParticleSet(size=npart, pclass=ptype[mode], lon=lon, lat=lat)
     endtime = delta(hours=6).total_seconds()
-    pset.execute(AdvectionRK4, dt=delta(minutes=3), endtime=endtime)
+    pset.execute(kernel[method], dt=delta(minutes=3), endtime=endtime)
     exp_lon = [truth_decaying(x, y, endtime)[0] for x, y, in zip(lon, lat)]
     exp_lat = [truth_decaying(x, y, endtime)[1] for x, y, in zip(lon, lat)]
-    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=1e-5)
-    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=1e-5)
+    assert np.allclose(np.array([p.lon for p in pset]), exp_lon, rtol=rtol)
+    assert np.allclose(np.array([p.lat for p in pset]), exp_lat, rtol=rtol)
