784e6b13d660dc915f317351f0d1f3bcf5a48215
jacketless <jscuttphillips@gmail.com>
2016-07-29 12:40:35 +1000

    Temporary fix for out-of-bounds field sampling
    Added a simple, and wholly incorrect, try-execute catch in `field.py`to
    return zero values for attempts at out-of-bounds field.data sampling.
    
    This is not anticipated as a final solution, but rather is a temporary
    fix to allow some simulation work to continue on HPC clusters.

--- parcels/field.py
+++ parcels/field.py
@@ -238,11 +238,17 @@ class Field(object):
             f0 = self.data[idx-1, :]
             f1 = self.data[idx, :]
         else:
-            f0 = self.interpolator2D(idx-1)((y, x))
-            f1 = self.interpolator2D(idx)((y, x))
-            t0 = self.time[idx-1]
-            t1 = self.time[idx]
-        return f0 + (f1 - f0) * ((time - t0) / (t1 - t0))
+            try:
+                f0 = self.interpolator2D(idx-1)((y, x))
+                f1 = self.interpolator2D(idx)((y, x))
+                t0 = self.time[idx-1]
+                t1 = self.time[idx]
+            except (IndexError, ValueError):
+                print("Caught outside of bounds error in PARCELS! Returning zero for field value...")
+                val = 0
+            else:
+                val = f0 + (f1 - f0) * ((time - t0) / (t1 - t0))
+        return val
 
     @cachedmethod(operator.attrgetter('time_index_cache'))
     def time_index(self, time):
