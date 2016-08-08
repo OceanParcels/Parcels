1140cc30d5a9bae17cdcd75ee36b701a97a903ed
erikvansebille <erikvansebille@gmail.com>
2016-08-08 10:31:07 +0100

    Adding .execute to test_case_globcurrent (currently fails because of #83)

--- examples/test_case_globcurrent.py
+++ examples/test_case_globcurrent.py
@@ -1,20 +1,19 @@
-from parcels import Grid
+from parcels import Grid, Particle, JITParticle, AdvectionRK4
+from datetime import timedelta as delta
 import pytest
-from os import path
 
 
-@pytest.fixture(scope='module')
-def filepath():
-    return path.join(path.dirname(__file__), "GlobCurrent_example_data")
-
-
-def test_globcurrent_grid():
+def set_globcurrent_grid():
     filenames = {'U': "examples/GlobCurrent_example_data/20*-GLOBCURRENT-L4-CUReul_hs-ALT_SUM-v02.0-fv01.0.nc",
                  'V': "examples/GlobCurrent_example_data/20*-GLOBCURRENT-L4-CUReul_hs-ALT_SUM-v02.0-fv01.0.nc"}
     variables = {'U': 'eastward_eulerian_current_velocity', 'V': 'northward_eulerian_current_velocity'}
     dimensions = {'lat': 'lat', 'lon': 'lon',
                   'time': 'time'}
-    grid = Grid.from_netcdf(filenames, variables, dimensions)
+    return Grid.from_netcdf(filenames, variables, dimensions)
+
+
+def test_globcurrent_grid():
+    grid = set_globcurrent_grid()
     assert(grid.U.lon.size == 81)
     assert(grid.U.lat.size == 41)
     assert(grid.U.data.shape == (365, 41, 81))
@@ -23,5 +22,19 @@ def test_globcurrent_grid():
     assert(grid.V.data.shape == (365, 41, 81))
 
 
-if __name__ == "__main__":
-    test_globcurrent_grid()
+@pytest.mark.xfail(run=False, reason="32/64 bit bug in time (github PR #83)")
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_globcurrent_particles(mode):
+    grid = set_globcurrent_grid()
+
+    lonstart = [25]
+    latstart = [-35]
+
+    ParticleClass = JITParticle if mode == 'jit' else Particle
+    pset = grid.ParticleSet(len(lonstart), pclass=ParticleClass, lon=lonstart, lat=latstart)
+
+    pset.execute(AdvectionRK4, runtime=delta(days=1), dt=delta(minutes=5),
+                 interval=delta(hours=1))
+
+    assert(abs(pset[0].lon - 23.8) < 1)
+    assert(abs(pset[0].lat - -35.3) < 1)
