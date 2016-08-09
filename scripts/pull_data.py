0e81709e26e26bfaacdb318200f437e8f1131c98
Thomas-95 <thomass4695@gmail.com>
2016-08-09 10:55:46 +0100

    Changed functions to classes, cleaned up code.

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -9,72 +9,42 @@ from datetime import datetime, timedelta
 from progressbar import ProgressBar
 
 
-def import_MovingEddies_example():
-        url = "http://oceanparcels.org/examples-data/MovingEddies_data"
-        filenames = ["moving_eddiesP.nc", "moving_eddiesU.nc", "moving_eddiesV.nc"]
-        path = "examples/MovingEddies_data"
-        if not os.path.exists(path):
-                os.makedirs(path)
+moving_eddies_example = {'url': "http://oceanparcels.org/examples-data/MovingEddies_data",
+                         'filenames': ["moving_eddiesP.nc", "moving_eddiesU.nc", "moving_eddiesV.nc"],
+                         'path': "examples/MovingEddies_data"}
 
-        filecount = 0
-        for filename in filenames:
-                filecount += 1
-                if not os.path.exists(os.path.join(path, filename)):
-                        f = urlopen(url + "/" + filename)
-                        print("Downloading %s; file %i of %i" % (filename, filecount, len(filenames)))
-                        with open(os.path.join(path, filename), 'wb') as temp_file:
-                                temp_file.write(f.read())
-                        print("%s written to %s" % (filename, path))
-                else:
-                        print("%s already exists within %s" % (filename, path))
+OFAM_example = {'url': "http://oceanparcels.org/examples-data/OFAM_example_data",
+                'filenames': ["OFAM_simple_U.nc", "OFAM_simple_V.nc"],
+                'path': "examples/OFAM_example_data"}
+
+dates = [datetime(2002, 1, 1) + timedelta(days=x) for x in range(0, 365)]
+globfiles = [x.strftime("%Y%m%d") + "000000-GLOBCURRENT-L4-CUReul_hs-ALT_SUM-v02.0-fv01.0.nc" for x in dates]
+globcurrent_example = {'url': "http://oceanparcels.org/examples-data/GlobCurrent_example_data",
+                       'filenames': globfiles,
+                       'path': "examples/GlobCurrent_example_data"}
 
 
-def import_globcurrent_example():
-        url = "http://oceanparcels.org/examples-data/GlobCurrent_example_data"
-        filenames = []
-        dt = datetime(2002, 1, 1)
-        end = datetime(2003, 1, 1)
-        step = timedelta(days=1)
-        while dt < end:
-                filenames.append(dt.strftime("%Y%m%d") + "000000-GLOBCURRENT-L4-CUReul_hs-ALT_SUM-v02.0-fv01.0.nc")
-                dt += step
-        path = "examples/GlobCurrent_example_data"
+class pulldata():
 
-        if not os.path.exists(path):
-                os.makedirs(path)
+    def __init__(self, url, filenames, path):
+        self.url = url
+        self.filenames = filenames
+        self.path = path
+
+    def download(self):
+        if not os.path.exists(self.path):
+                os.makedirs(self.path)
 
         pbar = ProgressBar()
-        print("Downloading GlobCurrent data...")
-        for filename in pbar(filenames):
-                if not os.path.exists(os.path.join(path, filename)):
-                        f = urlopen(url + "/" + filename)
-                        with open(os.path.join(path, filename), 'wb') as temp_file:
+        print("Downloading %s ...") % self.url.split("/")[-1]
+        for filename in pbar(self.filenames):
+                if not os.path.exists(os.path.join(self.path, filename)):
+                        f = urlopen(self.url + "/" + filename)
+                        with open(os.path.join(self.path, filename), 'wb') as temp_file:
                                 temp_file.write(f.read())
-        print("GlobCurrent data downloaded.")
-
-
-def import_OFAM_example():
-        url = "http://oceanparcels.org/examples-data/OFAM_example_data"
-        filenames = ["OFAM_simple_U.nc", "OFAM_simple_V.nc"]
-        path = "examples/OFAM_example_data"
-
-        if not os.path.exists(path):
-                os.makedirs(path)
-
-        filecount = 0
-        for filename in filenames:
-                filecount += 1
-                if not os.path.exists(os.path.join(path, filename)):
-                        f = urlopen(url + "/" + filename)
-                        print("Downloading %s; file %i of %i" % (filename, filecount, len(filenames)))
-                        with open(os.path.join(path, filename), 'wb') as temp_file:
-                                temp_file.write(f.read())
-                        print("%s written to %s" % (filename, path))
-                else:
-                        print("%s already exists within %s" % (filename, path))
 
 
 if __name__ == "__main__":
-        import_MovingEddies_example()
-        import_globcurrent_example()
-        import_OFAM_example()
+    pulldata(**moving_eddies_example).download()
+    pulldata(**OFAM_example).download()
+    pulldata(**globcurrent_example).download()
