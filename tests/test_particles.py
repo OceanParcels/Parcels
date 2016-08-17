112fbecf8458541b2277d877c58837ec93911a35
Michael Lange <michael.lange@imperial.ac.uk>
2016-08-17 06:40:58 +0100

    Variable: Add new test for custom variable initialisation to check types

---
+++ tests/test_particles.py
@@ -0,0 +1,32 @@
+from parcels import Grid, ScipyParticle, JITParticle, Variable
+import numpy as np
+import pytest
+
+
+ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
+
+
+@pytest.fixture
+def grid(xdim=100, ydim=100):
+    U = np.zeros((xdim, ydim), dtype=np.float32)
+    V = np.zeros((xdim, ydim), dtype=np.float32)
+    lon = np.linspace(0, 1, xdim, dtype=np.float32)
+    lat = np.linspace(0, 1, ydim, dtype=np.float32)
+    return Grid.from_data(U, lon, lat, V, lon, lat, mesh='flat')
+
+
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_variable_init(grid, mode, npart=10):
+    class TestParticle(ptype[mode]):
+        p_float = Variable('p_float', dtype=np.float32, default=10.)
+        p_double = Variable('p_double', dtype=np.float64, default=11.)
+        p_int = Variable('p_int', dtype=np.int32, default=12)
+    pset = grid.ParticleSet(npart, pclass=TestParticle,
+                            lon=np.linspace(0, 1, npart, dtype=np.float32),
+                            lat=np.linspace(1, 0, npart, dtype=np.float32))
+    assert np.array([isinstance(p.p_float, np.float32) for p in pset]).all()
+    assert np.allclose([p.p_float for p in pset], 10., rtol=1e-12)
+    assert np.array([isinstance(p.p_double, np.float64) for p in pset]).all()
+    assert np.allclose([p.p_double for p in pset], 11., rtol=1e-12)
+    assert np.array([isinstance(p.p_int, np.int32) for p in pset]).all()
+    assert np.allclose([p.p_int for p in pset], 12., rtol=1e-12)
