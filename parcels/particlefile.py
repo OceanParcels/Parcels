9ea0fc500b275dac02afbe0beacc56ed86635dac
Erik van Sebille <erikvansebille@gmail.com>
2016-12-04 19:38:02 +0000

    Adding capability for type='indexed' in ParticleFile
    Adding option for either ‘array’ (default) or ‘indexed’ writing. Latter
    allows for deleting and adding particles during .execute

--- parcels/particlefile.py
+++ parcels/particlefile.py
@@ -7,7 +7,7 @@ __all__ = ['ParticleFile']
 
 class ParticleFile(object):
 
-    def __init__(self, name, particleset, initial_dump=True):
+    def __init__(self, name, particleset, initial_dump=True, type='array'):
         """Initialise netCDF4.Dataset for trajectory output.
 
         The output follows the format outlined in the Discrete
@@ -17,6 +17,11 @@ class ParticleFile(object):
         The current implementation is based on the NCEI template:
         http://www.nodc.noaa.gov/data/formats/netcdf/v2.0/trajectoryIncomplete.cdl
 
+        Both 'Orthogonal multidimensional array' and 'Indexed ragged array' represenation
+        are implemented. The former is simpler to post-process, but the latter is required
+        when particles will be added and deleted during the .execute (i.e. the number of
+        particles in the pset is not fixed).
+
         Developer note: We cannot use xray.Dataset here, since it does
         not yet allow incremental writes to disk:
         https://github.com/xray/xray/issues/199
@@ -25,22 +30,32 @@ class ParticleFile(object):
         :param particlset: ParticleSet to output
         :param initial_dump: Perform initial output at time 0.
         :param user_vars: A list of additional user defined particle variables to write
+        :param type: Either 'array' for default matrix style, or 'indexed' for indexed ragged array
         """
+        self.type = type
         self.dataset = netCDF4.Dataset("%s.nc" % name, "w", format="NETCDF4")
         self.dataset.createDimension("obs", None)
-        self.dataset.createDimension("trajectory", particleset.size)
+        if self.type is 'array':
+            self.dataset.createDimension("trajectory", particleset.size)
+            coords = ("trajectory", "obs")
+        elif self.type is 'indexed':
+            coords = ("obs")
         self.dataset.feature_type = "trajectory"
-        self.dataset.Conventions = "CF-1.6"
+        self.dataset.Conventions = "CF-1.6/CF-1.7"
         self.dataset.ncei_template_version = "NCEI_NetCDF_Trajectory_Template_v2.0"
 
         # Create ID variable according to CF conventions
-        self.trajectory = self.dataset.createVariable("trajectory", "i4", ("trajectory",))
-        self.trajectory.long_name = "Unique identifier for each particle"
-        self.trajectory.cf_role = "trajectory_id"
-        self.trajectory[:] = np.array([p.id for p in particleset])
+        if self.type is 'array':
+            self.trajectory = self.dataset.createVariable("trajectory", "i4", ("trajectory",))
+            self.trajectory.long_name = "Unique identifier for each particle"
+            self.trajectory.cf_role = "trajectory_id"
+            self.trajectory[:] = np.array([p.id for p in particleset])
+        elif self.type is 'indexed':
+            self.trajectory = self.dataset.createVariable("trajectory", "i4", ("obs",))
+            self.trajectory.long_name = "index of trajectory this obs belongs to"
 
         # Create time, lat, lon and z variables according to CF conventions:
-        self.time = self.dataset.createVariable("time", "f8", ("trajectory", "obs"), fill_value=np.nan)
+        self.time = self.dataset.createVariable("time", "f8", coords, fill_value=np.nan)
         self.time.long_name = ""
         self.time.standard_name = "time"
         if particleset.time_origin == 0:
@@ -50,19 +65,19 @@ class ParticleFile(object):
             self.time.calendar = "julian"
         self.time.axis = "T"
 
-        self.lat = self.dataset.createVariable("lat", "f4", ("trajectory", "obs"), fill_value=np.nan)
+        self.lat = self.dataset.createVariable("lat", "f4", coords, fill_value=np.nan)
         self.lat.long_name = ""
         self.lat.standard_name = "latitude"
         self.lat.units = "degrees_north"
         self.lat.axis = "Y"
 
-        self.lon = self.dataset.createVariable("lon", "f4", ("trajectory", "obs"), fill_value=np.nan)
+        self.lon = self.dataset.createVariable("lon", "f4", coords, fill_value=np.nan)
         self.lon.long_name = ""
         self.lon.standard_name = "longitude"
         self.lon.units = "degrees_east"
         self.lon.axis = "X"
 
-        self.z = self.dataset.createVariable("z", "f4", ("trajectory", "obs"), fill_value=np.nan)
+        self.z = self.dataset.createVariable("z", "f4", coords, fill_value=np.nan)
         self.z.long_name = ""
         self.z.standard_name = "depth"
         self.z.units = "m"
@@ -70,10 +85,10 @@ class ParticleFile(object):
 
         self.user_vars = []
         for v in particleset.ptype.variables:
-            if v.name in ['time', 'lat', 'lon', 'z']:
+            if v.name in ['time', 'lat', 'lon', 'z', 'id']:
                 continue
             if v.to_write is True:
-                setattr(self, v.name, self.dataset.createVariable(v.name, "f4", ("trajectory", "obs"), fill_value=0.))
+                setattr(self, v.name, self.dataset.createVariable(v.name, "f4", coords, fill_value=0.))
                 getattr(self, v.name).long_name = ""
                 getattr(self, v.name).standard_name = v.name
                 getattr(self, v.name).units = "unknown"
@@ -89,11 +104,23 @@ class ParticleFile(object):
 
     def write(self, pset, time):
         """Write particle set data to file"""
-        self.time[:, self.idx] = time
-        self.lat[:, self.idx] = np.array([p.lat for p in pset])
-        self.lon[:, self.idx] = np.array([p.lon for p in pset])
-        self.z[:, self.idx] = np.zeros(pset.size, dtype=np.float32)
-        for var in self.user_vars:
-            getattr(self, var)[:, self.idx] = np.array([getattr(p, var) for p in pset])
+        if self.type is 'array':
+            self.time[:, self.idx] = time
+            self.lat[:, self.idx] = np.array([p.lat for p in pset])
+            self.lon[:, self.idx] = np.array([p.lon for p in pset])
+            self.z[:, self.idx] = np.zeros(pset.size, dtype=np.float32)
+            for var in self.user_vars:
+                getattr(self, var)[:, self.idx] = np.array([getattr(p, var) for p in pset])
 
-        self.idx += 1
+            self.idx += 1
+        elif self.type is 'indexed':
+            ind = np.arange(pset.size) + self.idx
+            self.trajectory[ind] = np.array([p.id for p in pset])
+            self.time[ind] = time
+            self.lat[ind] = np.array([p.lat for p in pset])
+            self.lon[ind] = np.array([p.lon for p in pset])
+            self.z[ind] = np.zeros(pset.size, dtype=np.float32)
+            for var in self.user_vars:
+                getattr(self, var)[ind] = np.array([getattr(p, var) for p in pset])
+
+            self.idx += pset.size
