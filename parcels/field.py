Fixing bug in calculation of halo lons and lats

parcels/field.py
@@ -382,16 +382,18 @@ class Field(object):
     def add_periodic_halo(self, zonal, meridional, halosize=None):
         if zonal:
             halosize = int(len(self.lon) / 30) if halosize is None else halosize
+            lonshift = (self.lon[-1] - 2 * self.lon[0] + self.lon[1])
             self.data = np.concatenate((self.data[:, :, -halosize:], self.data,
                                         self.data[:, :, 0:halosize]), axis=len(self.data.shape)-1)
-            self.lon = np.concatenate((self.lon[-halosize:] - self.lon[-1],
-                                       self.lon, self.lon[0:halosize] + self.lon[-1]))
+            self.lon = np.concatenate((self.lon[-halosize:] - lonshift,
+                                       self.lon, self.lon[0:halosize] + lonshift))
         if meridional:
             halosize = int(len(self.lat) / 30) if halosize is None else halosize
+            latshift = (self.lat[-1] - 2 * self.lat[0] + self.lat[1])
             self.data = np.concatenate((self.data[:, -halosize:, :], self.data,
                                         self.data[:, 0:halosize, :]), axis=len(self.data.shape)-2)
-            self.lat = np.concatenate((self.lat[-halosize:] - self.lat[-1],
-                                       self.lat, self.lat[0:halosize] + self.lat[-1]))
+            self.lat = np.concatenate((self.lat[-halosize:] - latshift,
+                                       self.lat, self.lat[0:halosize] + latshift))
 
     def write(self, filename, varname=None):
         filepath = str(path.local('%s%s.nc' % (filename, self.name)))