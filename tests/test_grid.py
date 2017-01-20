0086d0ca146e184ab6d705801da72b6c32de37fb
Erik van Sebille <erikvansebille@gmail.com>
2017-01-20 14:56:54 +0000

    Using os.pardir instead of '...' in examples data directory

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -2,7 +2,7 @@ from parcels import Grid, ParticleSet, ScipyParticle, JITParticle
 from parcels.field import Field
 import numpy as np
 import pytest
-from os import path
+from os import path, pardir
 
 
 ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
@@ -72,7 +72,7 @@ def test_grid_from_file_subsets(indslon, indslat, tmpdir, filename='test_subsets
 
 @pytest.mark.parametrize('indstime', [range(10, 20), [4]])
 def test_moving_eddies_file_subsettime(indstime):
-    gridfile = path.join(path.dirname(__file__), '..', 'examples', 'MovingEddies_data', 'moving_eddies')
+    gridfile = path.join(path.dirname(__file__), pardir, 'examples', 'MovingEddies_data', 'moving_eddies')
     gridfull = Grid.from_nemo(gridfile, extra_vars={'P': 'P'})
     gridsub = Grid.from_nemo(gridfile, extra_vars={'P': 'P'}, indices={'time': indstime})
     assert np.allclose(gridsub.P.time, gridfull.P.time[indstime])
