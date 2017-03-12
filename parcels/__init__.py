13420ee5652776b71b5f9bddf64d821d04031192
Erik van Sebille <erikvansebille@gmail.com>
2017-03-12 14:22:42 +0900

    Providing `Grid` class for backward compatibility
    This Grid class simply returns equivalent method from the FieldSet
    class, after throwing a warning

--- parcels/__init__.py
+++ parcels/__init__.py
@@ -6,3 +6,6 @@ from parcels.kernel import *  # noqa
 import parcels.rng as random  # noqa
 from parcels.particlefile import *  # noqa
 from parcels.kernels import *  # noqa
+
+# Only for backward compatibility
+from parcels.grid import *  # noqa
