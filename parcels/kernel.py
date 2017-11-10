58ed3db60d40db3a2246e0710c72bf21c5f9326b
Simnator101 <simnator@gmail.com>
2017-11-10 10:45:01 +0100

    Fixed formatting and a typo

--- parcels/kernel.py
+++ parcels/kernel.py
@@ -107,12 +107,11 @@ class Kernel(object):
         # Clean-up the in-memory dynamic linked libraries.
         # This is not really necessary, as these programs are not that large, but with the new random
         # naming scheme which is required on Windows OS'es to deal with updates to a Parcels' kernel.
-        if not self._lib == None:
+        if self._lib is not None:
             _ctypes.FreeLibrary(self._lib._handle) if platform == 'win32' else _ctypes.dlclose(self._lib._handle)
             del self._lib
             self._lib = None
-            map(remove, [self.src_file, self.lib_file, self.log_file]) if path.isfile(self.lib_file) else None #We'll keep the logs.
-
+            map(remove, [self.src_file, self.lib_file, self.log_file]) if path.isfile(self.lib_file) else None
 
     @property
     def _cache_key(self):
@@ -123,8 +122,8 @@ class Kernel(object):
 
     def remove_lib(self):
         # Unload the currently loaded dynamic linked library to be secure
-        if not self._lib == None:
-            _ctypes.FreeLibrary(self._lib._handle) if platform == 'win32' else _ctypes.dlclose(elf._lib._handle)
+        if self._lib is not None:
+            _ctypes.FreeLibrary(self._lib._handle) if platform == 'win32' else _ctypes.dlclose(self._lib._handle)
             del self._lib
             self._lib = None
         # If file already exists, pull new names. This is necessary on a Windows machine, because
@@ -142,7 +141,6 @@ class Kernel(object):
             f.write(self.ccode)
         compiler.compile(self.src_file, self.lib_file, self.log_file)
         logger.info("Compiled %s ==> %s" % (self.name, self.lib_file))
-
 
     def load_lib(self):
         self._lib = npct.load_library(self.lib_file, '.')
