4c191d59ccf5106ffde1c494796927eca83f6363
delandmeter <philippe.delandmeter@gmail.com>
2017-12-07 17:25:17 +0100

    same bug fixed

--- parcels/include/parcels.h
+++ parcels/include/parcels.h
@@ -128,7 +128,7 @@ static inline ErrorCode search_linear_float(float x, float y, float z, int sizeX
       }
       if (z < zcol[0] || z > zcol[sizeZ-1]) {return ERROR_OUT_OF_BOUNDS;}
       while (*k < sizeZ-1 && z > zcol[*k+1]) ++(*k);
-      while (*k > 0 && x < zcol[*k]) --(*k);
+      while (*k > 0 && z < zcol[*k]) --(*k);
   
       /* Lowering index by 1 if last index, to avoid out-of-array sampling
          for index+1 in spatial-interpolation*/
