f58231b4544b838d1229c9abb13f9e1b095b55c7
delandmeter <philippe.delandmeter@gmail.com>
2018-01-31 17:36:28 +0100

    better printing for timers using timedelta if root time > 300 s

--- parcels/tools/timer.py
+++ parcels/tools/timer.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 import time
+import datetime
 
 
 class Timer():
     def __init__(self, name, parent=None, start=True):
-        self._start = -1
-        self._end = -1
+        self._start = None
         self._t = 0
         self._name = name
         self._children = []
@@ -17,25 +17,22 @@ class Timer():
 
     def start(self):
         if self._parent:
-            assert(self._parent._start > 0), ("Timer '%s' cannot be started. Its parent timer does not run" % self._name)
+            assert(self._parent._start), ("Timer '%s' cannot be started. Its parent timer does not run" % self._name)
         self._start = time.time()
 
     def stop(self):
-        assert(self._start > 0), ("Timer '%s' was stopped before being started" % self._name)
+        assert(self._start), ("Timer '%s' was stopped before being started" % self._name)
         self._t += time.time() - self._start
-        self._start = -1
+        self._start = None
 
     def print_local(self):
-        if self._start > 0:
+        if self._start:
             print("Timer '%s': %g s (process running)" % (self._name, self._t + time.time() - self._start))
         else:
             print("Timer '%s': %g s" % (self._name, self._t))
 
     def local_time(self):
-        if self._start > 0:
-            return self._t + time.time() - self._start
-        else:
-            return self._t
+        return self._t + time.time() - self._start if self._start else self._t
 
     def print_tree(self, step=0, root_time=0, parent_time=0):
         time = self.local_time()
@@ -46,6 +43,7 @@ class Timer():
             print('  ', end='')
         if step > 0:
             print('(%3d%%) ' % round(time/parent_time*100), end='')
-        print("Timer '%s': %1.1e s" % (self._name, time))
+        t_str = '%1.3e s' % time if root_time < 300 else datetime.timedelta(seconds=time)
+        print("Timer %s: %s" % ((self._name).ljust(20 - 2*step + 7*(step == 0)), t_str))
         for child in self._children:
             child.print_tree(step+1, root_time, time)
