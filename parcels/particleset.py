bc8bc810240804ba76322bb034a6c6cf3a80f5c8
delandmeter <philippe.delandmeter@gmail.com>
2018-02-20 11:37:56 +0100

    safer option for density (also works if the field is not in fset)

--- parcels/particleset.py
+++ parcels/particleset.py
@@ -509,10 +509,12 @@ class ParticleSet(object):
         density = np.zeros((field.grid.lat.size, field.grid.lon.size), dtype=np.float32)
 
         for pi, p in enumerate(self.particles):
-            if hasattr(p, 'GridIndexSet') and p.GridIndexSet[field.grid].ti >= 0:
+            try:  # breaks if either p.GridIndexSet does not exist (in scipy) or field not in fieldset
+                if p.GridIndexSet[field.grid].ti < 0:  # xi, yi, ti, not initialised
+                    raise('error')
                 xi = p.GridIndexSet[field.grid].xi
                 yi = p.GridIndexSet[field.grid].yi
-            else:
+            except:
                 _, _, _, xi, yi, _ = field.search_indices(p.lon, p.lat, p.depth, 0, 0, search2D=True)
             density[yi, xi] += particle_val[pi]
 
