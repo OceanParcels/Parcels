6b412566e660ac2ad19edb24ac59006f06b5becf
delandmeter <philippe.delandmeter@gmail.com>
2018-04-05 13:10:59 +0200

    bug fixed in ofam example

--- parcels/examples/example_ofam.py
+++ parcels/examples/example_ofam.py
@@ -7,17 +7,17 @@ from os import path
 ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
 
 
-def set_ofam_fieldset():
+def set_ofam_fieldset(full_load=False):
     filenames = {'U': path.join(path.dirname(__file__), 'OFAM_example_data', 'OFAM_simple_U.nc'),
                  'V': path.join(path.dirname(__file__), 'OFAM_example_data', 'OFAM_simple_V.nc')}
     variables = {'U': 'u', 'V': 'v'}
     dimensions = {'lat': 'yu_ocean', 'lon': 'xu_ocean', 'depth': 'st_ocean',
                   'time': 'Time'}
-    return FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True)
+    return FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True, full_load=full_load)
 
 
 def test_ofam_fieldset_fillvalues():
-    fieldset = set_ofam_fieldset()
+    fieldset = set_ofam_fieldset(full_load=True)
     # V.data[0, 0, 150] is a landpoint, that makes NetCDF4 generate a masked array, instead of an ndarray
     assert(fieldset.V.data[0, 0, 150] == 0)
 
