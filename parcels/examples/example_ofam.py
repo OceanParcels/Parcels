0b75c0a5ef987507d13b4c5877e2e8909217070b
delandmeter <philippe.delandmeter@gmail.com>
2018-04-05 12:04:28 +0200

    remove unnecessary py test

--- parcels/examples/example_ofam.py
+++ parcels/examples/example_ofam.py
@@ -16,16 +16,6 @@ def set_ofam_fieldset():
     return FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True)
 
 
-def test_ofam_fieldset():
-    fieldset = set_ofam_fieldset()
-    assert(fieldset.U.lon.size == 2001)
-    assert(fieldset.U.lat.size == 601)
-    assert(fieldset.U.data.shape == (4, 601, 2001))
-    assert(fieldset.V.lon.size == 2001)
-    assert(fieldset.V.lat.size == 601)
-    assert(fieldset.V.data.shape == (4, 601, 2001))
-
-
 def test_ofam_fieldset_fillvalues():
     fieldset = set_ofam_fieldset()
     # V.data[0, 0, 150] is a landpoint, that makes NetCDF4 generate a masked array, instead of an ndarray
