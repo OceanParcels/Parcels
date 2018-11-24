c79a181263096c5809dda2208ee72e81f83cbfac
Erik van Sebille <erikvansebille@gmail.com>
2018-11-24 17:46:43 +0100

    support for processing per variable only
    Also further clean-up of code

--- parcels/particlefile.py
+++ parcels/particlefile.py
@@ -48,7 +48,6 @@ class ParticleFile(object):
         self.name = name
         self.write_ondelete = write_ondelete
         self.outputdt = outputdt
-        self.lasttraj = 0  # id of last particle written
         self.lasttime_written = None  # variable to check if time has been written already
 
         self.dataset = None
@@ -57,24 +56,21 @@ class ParticleFile(object):
         self.var_names = []
         self.var_names_once = []
         for v in self.particleset.ptype.variables:
-            if v.name in ['time', 'lat', 'lon', 'depth', 'id']:
+            if v.to_write == 'once':
+                self.var_names_once += [v.name]
+            elif v.to_write is True:
                 self.var_names += [v.name]
-            elif v.to_write:
-                if v.to_write is True:
-                    self.var_names += [v.name]
-                elif v.to_write == 'once':
-                    self.var_names_once += [v.name]
         if len(self.var_names_once) > 0:
             self.written_once = []
             self.file_list_once = []
 
-        self.npy_path = os.path.join(gettempdir(), "parcels-%s" % getuid(), "out")
         self.file_list = []
         self.time_written = []
         self.maxid_written = -1
         self.dataset_open = True
-        if os.path.exists(self.npy_path):
-            os.system("rm -rf " + self.npy_path)
+
+        self.npy_path = os.path.join(gettempdir(), "parcels-%s" % getuid(), "out")
+        self.delete_npyfiles()
 
     def open_dataset(self, data_shape):
         extension = os.path.splitext(str(self.name))[1]
@@ -224,132 +220,49 @@ class ParticleFile(object):
             if not deleted_only:
                 self.lasttime_written = time
 
-    def export(self, batch_processing=False, batch_size=2**30):
-        """Exports outputs in temporary NPY-files to NetCDF file
+    def read_npy(self, file_list, time_steps, var):
+        """Read NPY-files using a loop over all files and return one array
+        for each variable.
 
-        :param batch_processing: If True batch processing is applied. Batches
-        of files are loaded and then written to the output netcdf-file. This
-        option is still work in progress. Default:True
-
-        :param batch_size: size of one batch in bytes. Default: 2**30 (1 Gbyte).
-
-        :type batch_processing: boolean
-        :type batch_size: int
+        :param file_list: List that  contains all file names in the output
+        directory
+        :param id_fill_value: FillValue for the IDs as used in the output
+        netcdf
         """
 
-        def get_available_memory():
-            """return available memory in bytes"""
-            memory_info = psutil.virtual_memory()
-            available_memory = memory_info.available
-            return available_memory
+        data = np.nan * np.zeros((self.maxid_written+1, time_steps))
+        time_index = np.zeros(self.maxid_written+1, dtype=int)
 
-        def read(file_list, time_steps, var_names):
-            """Read NPY-files using a loop over all files and return one array
-            for each variable.
+        # loop over all files
+        for npyfile in file_list:
+            data_dict = np.load(npyfile).item()
+            id_ind = np.array(data_dict["id"], dtype=int)
+            t_ind = time_index[id_ind] if 'once' not in file_list[0] else 0
+            data[id_ind, t_ind] = data_dict[var]
+            time_index[id_ind] = time_index[id_ind] + 1
 
-            :param file_list: List that  contains all file names in the output
-            directory
-            :param id_fill_value: FillValue for the IDs as used in the output
-            netcdf
+        # remove rows and columns that are completely filled with nan values
+        tmp = data[~np.isnan(data).all(axis=1)]
+        return tmp[:, ~np.isnan(data).all(axis=0)]
 
-            :type file_list: list
-            :type id_fill_value: int
+    def export(self):
+        """Exports outputs in temporary NPY-files to NetCDF file
+        """
 
-            :return: Python dictionary with the data that was read from the
-            NPY-files
-            """
+        memory_estimate_total = self.maxid_written+1 * len(self.time_written) * 8
+        if memory_estimate_total > 0.9*psutil.virtual_memory().available:
+            raise MemoryError("Not enough memory available for export. npy files are stored at %s", self.npy_path)
 
-            merge_dict = {}
-            for var in var_names:
-                merge_dict[var] = np.nan * np.zeros((self.maxid_written+1, time_steps))
-            time_index = np.zeros(self.maxid_written+1, dtype=int)
-
-            # loop over all files
-            for npyfile in file_list:
-                data_dict = np.load(npyfile).item()
-
-                id_ind = np.array(data_dict["id"], dtype=int)
-                t_ind = time_index[id_ind] if 'once' not in file_list[0] else 0
-                for key in var_names:
-                    merge_dict[key][id_ind, t_ind] = data_dict[key]
-
-                time_index[id_ind] = time_index[id_ind] + 1
-
-            # remove rows and columns that are completely filled with nan values
-            out_dict = {}
-            for var in var_names:
-                tmp = merge_dict[var][~np.isnan(merge_dict[var]).all(axis=1)]
-                out_dict[var] = tmp[:, ~np.isnan(merge_dict[var]).all(axis=0)]
-            return out_dict
-
-        if batch_processing:
-            print("=============convert NPY-files to NetCDF-file===============")
-
-            available_memory = get_available_memory()
-            print("Available memory: '" + str(available_memory/float(2**20)) + "' Mbytes")
-            self.batch_size = batch_size
-
-            # estimate the total size in bytes for merging array
-            self.memory_estimate_total = (self.maxid_written+1) * len(self.time_written) * len(self.var_names) * 8
-            self.memory_per_file = self.memory_estimate_total/len(self.file_list)
-            print("Estimated memory needed: '" + str(float(self.memory_estimate_total)/2**20) + "' Mbytes")
-            print("Estimated memory per file needed: '" + str(float(self.memory_per_file)/2**20) + "' Mbytes")
-
-            if self.memory_per_file > available_memory*0.9:
-                raise MemoryError("Too little available memory is available to load even one tempory output file. With 10% safety margin.")
-
-            if 0.7*available_memory < self.batch_size:
-                self.batch_size = 0.7 * available_memory
-                print("Too little available  memory available in ParticleFile.conversion_npy() for batch_size:" + str(batch_size/2**20) + " Mbytes.")
-                print("Setting batch_size to 70% of available  memory:"+str(float(self.batch_size)/2**20) + " Mbytes.")
-
-            if batch_size < self.memory_per_file:
-                self.batch_size = self.memory_per_file
-                print("'batch_size' size too little.'batch_size' has to be at least as big as the memory needed per file.")
-                print("Set batch the memory that is needed per file: " + str(self.batch_size/2**20) + "Mbytes.")
-
-            print("Convert NPY-files using batch processing in batches of max. size '"+str(float(self.batch_size)/2**20)+"' Mbytes.")
-
-            # divide the data set into chunks
-            n_reading_loops = int(self.memory_estimate_total//self.batch_size)
-            if n_reading_loops == 0:
-                n_reading_loops = 1
-
-            files_per_loop = len(self.file_list)//n_reading_loops
-
-            if files_per_loop == 0:
-                raise ValueError("'files_per_loop' can not be equal to 0. Some bug might exist in ParticleFile.export().")
-
-            time_list_splitted = [self.file_list[i:i + files_per_loop] for i in range(0, len(self.file_list), files_per_loop)]
-
-            print("Convert data using " + str(len(time_list_splitted))+" batch(es).")
-
-            # last time index that was filled
-            last_filled = 0
-
-            # reading loop
-            for time_list_loop in time_list_splitted:
-                n_time_step = len(time_list_loop)
-                data_dict = read(time_list_loop, n_time_step, self.var_names)
-
-                for var in self.var_names:
-                    getattr(self, var)[:, last_filled:last_filled+n_time_step] = data_dict[var]
-
-                last_filled += n_time_step
-
-        else:
-            data_dict = read(self.file_list, len(self.time_written), self.var_names)
-            self.open_dataset(data_dict["id"].shape)
-            for var in self.var_names:
-                if var == "depth":
-                    self.z[:, :] = data_dict["depth"]
-                else:
-                    getattr(self, var)[:, :] = data_dict[var]
+        for var in self.var_names:
+            data = self.read_npy(self.file_list, len(self.time_written), var)
+            if var == self.var_names[0]:
+                self.open_dataset(data.shape)
+            varout = 'z' if var == 'depth' else var
+            getattr(self, varout)[:, :] = data
 
         if len(self.var_names_once) > 0:
-            data_dict = read(self.file_list_once, 1, self.var_names_once)
             for var in self.var_names_once:
-                getattr(self, var)[:] = data_dict[var]
+                getattr(self, var)[:] = self.read_npy(self.file_list_once, 1, var)
 
     def delete_npyfiles(self):
         if os.path.exists(self.npy_path):
