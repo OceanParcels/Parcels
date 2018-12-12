4eb9c1f01967ffc3c17bdc6e24b8a43e2af9de23
delandmeter <philippe.delandmeter@gmail.com>
2018-12-12 15:37:54 +0100

    my new version of flake8 asks this

--- parcels/field.py
+++ parcels/field.py
@@ -188,11 +188,11 @@ class Field(object):
         indices = {} if indices is None else indices.copy()
         for ind in indices.values():
             assert np.min(ind) >= 0, \
-                ('Negative indices are currently not allowed in Parcels. ' +
-                 'This is related to the non-increasing dimension it could generate ' +
-                 'if the domain goes from lon[-4] to lon[6] for example. ' +
-                 'Please raise an issue on https://github.com/OceanParcels/parcels/issues ' +
-                 'if you would need such feature implemented.')
+                ('Negative indices are currently not allowed in Parcels. '
+                 + 'This is related to the non-increasing dimension it could generate '
+                 + 'if the domain goes from lon[-4] to lon[6] for example. '
+                 + 'Please raise an issue on https://github.com/OceanParcels/parcels/issues '
+                 + 'if you would need such feature implemented.')
 
         with NetcdfFileBuffer(lonlat_filename, dimensions, indices, netcdf_engine) as filebuffer:
             lon, lat = filebuffer.read_lonlat
