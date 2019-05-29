d476f1445a1a66f3c5c05b0da05b847153c587b1
davidwichmann <32638174+davidwichmann@users.noreply.github.com>
2019-05-29 15:08:18 +0200

    add script to check corrupted files

---
+++ parcels/scripts/check_corrupted_files.py
@@ -0,0 +1,35 @@
+"""
+Function to check if the data of a variable in a field list is corrupted
+"""
+
+import numpy as np
+import xarray as xr
+import os
+
+
+def check_corrupted_files(filelist, fieldname, outputfile=None):
+    """
+    parameter filelist: list of files which contain the field with name 'fieldname'
+    parameter fieldname: name of the field that is checked
+    parameter outputfile: list of corrupted files is written into a output text file
+    """
+
+    if outputfile is None:
+        outputfile = 'CorruptedFiles_field_' + fieldname + '.txt'
+
+    if os.path.isfile(outputfile):
+        print 'Outputfile exists already. Please delete or choose another name for the output file.'
+        return 0
+
+    myfile = open(outputfile, 'w')
+
+    for f in filelist:
+        print "Load file: ", f
+        try:
+            ds = xr.open_dataset(f, decode_cf=False)
+            np.array(getattr(ds, fieldname))
+        except:
+            myfile.write("%s\n" % f)
+            print 'file is corrupted:  ', f
+
+    myfile.close()
