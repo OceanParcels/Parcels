8317d3eea2685a868f4eadca2011f64fa4f9adb4
jbusecke <julius@ldeo.columbia.edu>
2019-06-11 09:15:46 -0400

    bugfix test_set_calendar

--- tests/test_particle_file.py
+++ tests/test_particle_file.py
@@ -4,6 +4,6 @@ import cftime
 
 def test_set_calendar():
     for calendar_name, cf_datetime in zip(_get_cftime_calendars(), _get_cftime_datetimes()):
-        date = cf_datetime(1990,1,1)
+        date = getattr(cftime,cf_datetime)(1990,1,1)
         assert _set_calendar(date.calendar) == date.calendar
     assert _set_calendar('np_datetime64') == 'standard'
