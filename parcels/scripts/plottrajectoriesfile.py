315456e844ec048653fbbcea14ac6bed1311f545
delandmeter <philippe.delandmeter@gmail.com>
2019-06-24 16:59:22 +0200

    test

--- parcels/scripts/plottrajectoriesfile.py
+++ parcels/scripts/plottrajectoriesfile.py
@@ -58,6 +58,8 @@ def plotTrajectoriesFile(filename, mode='2d', tracerfile=None, tracerfield='P',
             return  # creating axes was not possible
         titlestr = ' and ' + tracerfield
     else:
+        print('here')
+        return
         spherical = False if mode == '3d' or mesh == 'flat' else True
         plt, fig, ax, cartopy = create_parcelsfig_axis(spherical=spherical)
         if plt is None:
