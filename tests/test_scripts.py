c5920bdc50d6e4b6b2a20a349864e25b603f069c
delandmeter <philippe.delandmeter@gmail.com>
2019-06-25 09:47:03 +0200

    typo

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -35,7 +35,7 @@ def create_outputfiles(dir):
 
 @pytest.mark.parametrize('mode', ['2d', '3d', 'movie2d', 'hist2d'])
 def test_plotting(mode, tmpdir):
-    if 'mode' == 3d and sys.platform == 'linux':
+    if 'mode' == '3d' and sys.platform == 'linux':
         return
     fp = create_outputfiles(tmpdir)
     plotTrajectoriesFile(fp, mode=mode, show_plt=False)
