d5a384c67009f6eb493f33b392973dc32bbda7a1
delandmeter <philippe.delandmeter@gmail.com>
2019-08-22 14:52:24 +0200

    rm unnecessary statement

--- tests/test_fieldset.py
+++ tests/test_fieldset.py
@@ -469,7 +469,6 @@ def test_fieldset_defer_loading_function(zdim, scale_fac, tmpdir, filename='test
     def compute(fieldset):
         # Calculating vertical weighted average
         for f in [fieldset.U, fieldset.V]:
-            f.data = np.array(f.data)
             for tind in f.loaded_time_indices:
                 f.data[tind, :] = np.sum(f.data[tind, :] * DZ, axis=0) / sum(dz)
 
