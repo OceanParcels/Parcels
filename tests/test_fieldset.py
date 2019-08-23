55e2b1c12abc6d63c9bd9fa5a85b901ce2fa95e4
delandmeter <philippe.delandmeter@gmail.com>
2019-08-23 15:49:04 +0200

    variable not available in field anymore

--- tests/test_fieldset.py
+++ tests/test_fieldset.py
@@ -469,7 +469,7 @@ def test_fieldset_defer_loading_function(zdim, scale_fac, tmpdir, filename='test
     def compute(fieldset):
         # Calculating vertical weighted average
         for f in [fieldset.U, fieldset.V]:
-            for tind in f.loaded_time_indices:
+            for tind in range(3):
                 f.data[tind, :] = np.sum(f.data[tind, :] * DZ, axis=0) / sum(dz)
 
     fieldset.compute_on_defer = compute
