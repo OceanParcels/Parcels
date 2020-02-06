54b729cde022b611db7e9100b65863be72a06983
Christian Kehl <c.kehl@uu.nl>
2020-02-06 17:04:52 +0100

    enabled larger osx test log fix be able to fix all errors, not only getting one by one

--- .github/scripts/osx_test.sh
+++ .github/scripts/osx_test.sh
@@ -4,4 +4,4 @@ source activate parcels
 export CONDA_BUILD_SYSROOT=/
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/Applications/Xcode.app/Contents//Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
 
-py.test -v -s tests/
+py.test -vv -s tests/
