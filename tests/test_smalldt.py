d9ba50706f98baab360162708d36f06e0517dd3c
Christian Kehl <c.kehl@uu.nl>
2020-03-05 12:22:17 +0100

    #762 - changed test file structure to comply with pytest

--- tests/test_smalldt.py
+++ tests/test_smalldt.py
@@ -16,18 +16,22 @@ from parcels import FieldSet, ParticleSet, JITParticle, AdvectionRK4, ErrorCode
 import numpy as np
 from datetime import timedelta
 from os import path
+import pytest
 
 
 ### Functions ###
 
-def deleteparticle(particle, fieldset, time):
-    """ This function deletes particles as they exit the domain and prints a message about their attributes at that moment
-    """
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+@pytest.mark.parametrize('dt', [0.002, -0.002, 0.004,  -0.004, 0.01, -0.01, 1.0, -1.0])
+def test_consistent_time_accumulation(mode, dt):
+    def deleteparticle(particle, fieldset, time):
+        """ This function deletes particles as they exit the domain and prints a message about their attributes at that moment
+        """
+        print('Particle '+str(particle.id)+' has died at t = '+str(time)+' at lon, lat, depth = '+str(particle.lon)+', '+str(particle.lat)+', '+str(particle.depth))
+        particle.delete()
 
-    print('Particle '+str(particle.id)+' has died at t = '+str(time)+' at lon, lat, depth = '+str(particle.lon)+', '+str(particle.lat)+', '+str(particle.depth))
-    particle.delete()
-
-def run(outputdt=0.1,dt=0.004,runtime=1.5):
+    outputdt = timedelta(seconds=0.1)  # make this a parameter if the test result differs depending on this parameter value
+    runtime = timedelta(seconds=1.5)   # make this a parameter if the test result differs depending on this parameter value
     datafile = path.join(path.dirname(__file__), 'test_data', 'dt_field')
 
     fieldset = FieldSet.from_parcels(datafile, allow_time_extrapolation=True)
@@ -40,17 +44,12 @@ def run(outputdt=0.1,dt=0.004,runtime=1.5):
     inittime = np.asarray([0] * len(lons))
 
     pset = ParticleSet(fieldset=fieldset, pclass=JITParticle, lon=lons, lat=lats, time=inittime)
-
-    outputdt = timedelta(seconds=outputdt)  # timesteps to create output at
-    dt = timedelta(seconds=dt)  # timesteps to calculate particle trajectories
-    runtime = timedelta(seconds=runtime)
-
     output_file = pset.ParticleFile(name='TEST1', outputdt=outputdt)
 
     pset.execute(AdvectionRK4,
-                 runtime=runtime,
-                 dt=dt,
-                 recovery={ErrorCode.ErrorOutOfBounds: deleteparticle},
-                 output_file=output_file)
+                     runtime=runtime,
+                     dt=timedelta(seconds=dt),
+                     recovery={ErrorCode.ErrorOutOfBounds: deleteparticle},
+                     output_file=output_file)
 
     output_file.close()
