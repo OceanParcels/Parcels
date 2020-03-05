b13c57355c0b6b4d4652b96f4df38e7a9f73861c
Reint <33000433+reint-fischer@users.noreply.github.com>
2020-03-05 17:17:54 +0100

    Adding abortion to the test_smalldt

--- tests/test_smalldt.py
+++ tests/test_smalldt.py
@@ -12,10 +12,11 @@ Created on Thu Feb 27 14:44:03 2020
 #
 # sys.path.insert(0, "\\Users\\Gebruiker\\Documents\\GitHub\\parcels\\")  # Set path to find the newest parcels code
 
-from parcels import FieldSet, ParticleSet, JITParticle, AdvectionRK4, ErrorCode
+from parcels import FieldSet, ParticleSet, JITParticle, ScipyParticle, AdvectionRK4, ErrorCode, plotTrajectoriesFile
 import numpy as np
 from datetime import timedelta
 from os import path
+import threading
 
 
 ### Functions ###
@@ -24,10 +25,20 @@ def deleteparticle(particle, fieldset, time):
     """ This function deletes particles as they exit the domain and prints a message about their attributes at that moment
     """
 
-    print('Particle '+str(particle.id)+' has died at t = '+str(time)+' at lon, lat, depth = '+str(particle.lon)+', '+str(particle.lat)+', '+str(particle.depth))
+    # print('Particle '+str(particle.id)+' has died at t = '+str(time))
     particle.delete()
 
-def run(outputdt=0.1,dt=0.004,runtime=1.5):
+class Abortion():
+    abort_object = None
+    aborted = False
+    def __init__(self, object):
+        self.abort_object = object
+
+    def abort(self):
+        self.abort_object.aborted = True
+        self.aborted = True
+
+def run(ioutputdt=0.1,idt=0.004,iruntime=1.5):
     datafile = path.join(path.dirname(__file__), 'test_data', 'dt_field')
 
     fieldset = FieldSet.from_parcels(datafile, allow_time_extrapolation=True)
@@ -39,18 +50,26 @@ def run(outputdt=0.1,dt=0.004,runtime=1.5):
     lats = lats.flatten()
     inittime = np.asarray([0] * len(lons))
 
-    pset = ParticleSet(fieldset=fieldset, pclass=JITParticle, lon=lons, lat=lats, time=inittime)
+    pset = ParticleSet(fieldset=fieldset, pclass=ScipyParticle, lon=lons, lat=lats, time=inittime)
+    abort_object = Abortion(pset)
 
-    outputdt = timedelta(seconds=outputdt)  # timesteps to create output at
-    dt = timedelta(seconds=dt)  # timesteps to calculate particle trajectories
-    runtime = timedelta(seconds=runtime)
+    timer = threading.Timer(iruntime/ioutputdt+1000.,abort_object.abort)
+    outputdt = timedelta(seconds=ioutputdt)  # timesteps to create output at
+    dt = timedelta(seconds=idt)  # timesteps to calculate particle trajectories
+    runtime = timedelta(seconds=iruntime)
 
-    output_file = pset.ParticleFile(name='TEST1', outputdt=outputdt)
-
+    output_file = pset.ParticleFile(name='test_data/TEST2', outputdt=outputdt)
+    timer.start()
     pset.execute(AdvectionRK4,
                  runtime=runtime,
                  dt=dt,
-                 recovery={ErrorCode.ErrorOutOfBounds: deleteparticle},
-                 output_file=output_file)
-
+                 recovery={ErrorCode.ErrorOutOfBounds: deleteparticle}, output_file=output_file)
+    timer.cancel()
     output_file.close()
+    assert abort_object.aborted == False
+    # particles = pset.particles
+    # result = []
+    # for i in range(len(particles)):
+    #     result += [particles[i].time]
+    # result = np.asarray(result)
+    # assert np.allclose(result,iruntime)
