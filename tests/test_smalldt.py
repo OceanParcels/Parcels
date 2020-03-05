9fbfbcfdcffcde5a44f346b5a09bdc13c1254f32
Christian Kehl <c.kehl@uu.nl>
2020-03-05 18:58:30 +0100

    #762 - tested with Reint's test class - no error's apparent. Tests need to be a bit more exapnded.

--- tests/test_smalldt.py
+++ tests/test_smalldt.py
@@ -17,28 +17,34 @@ import numpy as np
 from datetime import timedelta
 from os import path
 import threading
+import pytest
 
 
 ### Functions ###
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+@pytest.mark.parametrize('dt', [0.004,  -0.004, 0.01, -0.01, 0.1, -0.1])
+def test_consistent_time_accumulation(mode, dt):
+    def deleteparticle(particle, fieldset, time):
+        """ This function deletes particles as they exit the domain and prints a message about their attributes at that moment
+        """
 
-def deleteparticle(particle, fieldset, time):
-    """ This function deletes particles as they exit the domain and prints a message about their attributes at that moment
-    """
+        # print('Particle '+str(particle.id)+' has died at t = '+str(time))
+        particle.delete()
 
-    # print('Particle '+str(particle.id)+' has died at t = '+str(time))
-    particle.delete()
+    class Abortion():
+        abort_object = None
+        aborted = False
+        def __init__(self, object):
+            self.abort_object = object
 
-class Abortion():
-    abort_object = None
-    aborted = False
-    def __init__(self, object):
-        self.abort_object = object
+        def abort(self):
+            self.abort_object.aborted = True
+            self.aborted = True
 
-    def abort(self):
-        self.abort_object.aborted = True
-        self.aborted = True
-
-def run(ioutputdt=0.1,idt=0.004,iruntime=1.5):
+    ioutputdt = 0.1
+    iruntime = 1.5
+    outputdt = timedelta(seconds=ioutputdt)  # make this a parameter if the test result differs depending on this parameter value
+    runtime = timedelta(seconds=iruntime)    # make this a parameter if the test result differs depending on this parameter value
     datafile = path.join(path.dirname(__file__), 'test_data', 'dt_field')
 
     fieldset = FieldSet.from_parcels(datafile, allow_time_extrapolation=True)
@@ -52,24 +58,22 @@ def run(ioutputdt=0.1,idt=0.004,iruntime=1.5):
 
     pset = ParticleSet(fieldset=fieldset, pclass=ScipyParticle, lon=lons, lat=lats, time=inittime)
     abort_object = Abortion(pset)
-
     timer = threading.Timer(iruntime/ioutputdt+1000.,abort_object.abort)
-    outputdt = timedelta(seconds=ioutputdt)  # timesteps to create output at
-    dt = timedelta(seconds=idt)  # timesteps to calculate particle trajectories
-    runtime = timedelta(seconds=iruntime)
 
     output_file = pset.ParticleFile(name='test_data/TEST2', outputdt=outputdt)
     timer.start()
     pset.execute(AdvectionRK4,
                  runtime=runtime,
-                 dt=dt,
+                 dt=timedelta(seconds=dt),
                  recovery={ErrorCode.ErrorOutOfBounds: deleteparticle}, output_file=output_file)
     timer.cancel()
     output_file.close()
     assert abort_object.aborted == False
     # particles = pset.particles
     # result = []
+    # time_prev = 0
     # for i in range(len(particles)):
-    #     result += [particles[i].time]
-    # result = np.asarray(result)
-    # assert np.allclose(result,iruntime)
+    #     result.append(particles[i].time-time_prev)
+    #     time_prev = particles[i].time
+    # result = np.array(result, dtype=np.float64)
+    # assert np.allclose(result,dt)
