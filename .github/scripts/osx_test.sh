96bdbe7609652bf707b254c8ebd10b4c2b2764cf
Erik van Sebille <erikvansebille@gmail.com>
2020-04-06 09:09:07 +0200

    updating CI runs to test all unit tests

--- .github/scripts/osx_test.sh
+++ .github/scripts/osx_test.sh
@@ -4,4 +4,5 @@ source activate parcels
 export CONDA_BUILD_SYSROOT=/
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/Applications/Xcode.app/Contents//Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
 
-py.test -v -s tests/
+parcels_get_examples examples/;
+py.test -v -s --nbval-lax -k "not documentation" tests/ examples/;
--- .github/scripts/windows_test.bat
+++ .github/scripts/windows_test.bat
@@ -1,5 +1,4 @@
 SET PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
 call activate parcels
-py.test -v -s tests/
 parcels_get_examples examples/
-py.test -v -s --nbval-lax examples/ -k "not documentation" 
+py.test -v -s --nbval-lax -k "not documentation" tests/ examples/;
