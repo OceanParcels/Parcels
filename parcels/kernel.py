33d92d11362ea263050db9303cf81087372d05e3
Christian Kehl <c.kehl@uu.nl>
2020-06-15 16:09:28 +0200

    incorporated the compilation changes in the kernel and original particle set.

--- parcels/kernel.py
+++ parcels/kernel.py
@@ -24,9 +24,12 @@ try:
 except:
     MPI = None
 
-from parcels.codegenerator import KernelGenerator
-from parcels.codegenerator import LoopGenerator
-from parcels.compiler import get_cache_dir
+# from parcels.codegenerator import KernelGenerator
+# from parcels.codegenerator import LoopGenerator
+# from parcels.compiler import get_cache_dir
+from parcels.wrapping.code_generator import KernelGenerator
+from parcels.wrapping.code_generator import LoopGenerator
+from parcels.tools import get_cache_dir
 from parcels.field import Field
 from parcels.field import FieldOutOfBoundError
 from parcels.field import FieldOutOfBoundSurfaceError
