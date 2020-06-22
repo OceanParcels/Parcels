84095009e5705e44723903fa69a68a237dd3b840
Christian Kehl <c.kehl@uu.nl>
2020-06-22 13:45:23 +0200

    sorted_pset_trials - fixed the random number generator tests.

--- parcels/__init__.py
+++ parcels/__init__.py
@@ -8,6 +8,7 @@ from parcels.particleset_benchmark import *  # noqa
 from parcels.field import *  # noqa
 from parcels.kernel_vectorized import *  # noqa
 import parcels.rng as random  # noqa
+# from parcels import rng as random
 from parcels.particlefile import *  # noqa
 from parcels.kernels import *  # noqa
 from parcels.scripts import *  # noqa
