46f97d0cb970e381028bc6f799e94d15468dd59f
Christian Kehl <c.kehl@uu.nl>
2020-07-08 17:55:10 +0200

    list_of_pset_array_trials - adapted the particle file to work with the new pset (basically: change the iterator). structure to be wrapped up and tested now.

--- parcels/particlefile.py
+++ parcels/particlefile.py
@@ -229,7 +229,8 @@ class ParticleFile(object):
 
         if self.lasttime_written != time and \
            (self.write_ondelete is False or deleted_only is True):
-            if pset.size == 0:
+            # if pset.size == 0:
+            if len(pset.particles) == 0 or pset.particles.shape[0] == 0:
                 logger.warning("ParticleSet is empty on writing as array at time %g" % time)
             else:
                 if deleted_only:
@@ -240,13 +241,15 @@ class ParticleFile(object):
                 # else:
                 #     pset_towrite = [p for p in pset if time + p.dt < p.time <= time - p.dt/2 and np.isfinite(p.id)]
                 else:
-                    pset_towrite = [p for p in pset if time - np.abs(p.dt/2) <= p.time < time + np.abs(p.dt) and np.isfinite(p.id)]
+                    # pset_towrite = [p for p in pset if time - np.abs(p.dt/2) <= p.time < time + np.abs(p.dt) and np.isfinite(p.id)]
+                    pset_towrite = [p for sublist in pset.particles for p in sublist if time - np.abs(p.dt / 2) <= p.time < time + np.abs(p.dt) and np.isfinite(p.id)]
                 if len(pset_towrite) > 0:
                     for var in self.var_names:
                         data_dict[var] = np.array([getattr(p, var) for p in pset_towrite])
                     self.maxid_written = np.max([self.maxid_written, np.max(data_dict['id'])])
 
-                pset_errs = [p for p in pset_towrite if p.state != ErrorCode.Delete and abs(time-p.time) > 1e-3]
+                # pset_errs = [p for p in pset_towrite if p.state != ErrorCode.Delete and abs(time-p.time) > 1e-3]
+                pset_errs = [p for sublist in pset_towrite.particles for p in sublist if p.state != ErrorCode.Delete and abs(time - p.time) > 1e-3]
                 for p in pset_errs:
                     logger.warning_once(
                         'time argument in pfile.write() is %g, but a particle has time % g.' % (time, p.time))
@@ -255,7 +258,8 @@ class ParticleFile(object):
                     self.time_written.append(time)
 
                 if len(self.var_names_once) > 0:
-                    first_write = [p for p in pset if (p.id not in self.written_once) and _is_particle_started_yet(p, time)]
+                    # first_write = [p for p in pset if (p.id not in self.written_once) and _is_particle_started_yet(p, time)]
+                    first_write = [p for sublist in pset.particles for p in sublist if (p.id not in self.written_once) and _is_particle_started_yet(p, time)]
                     data_dict_once['id'] = np.array([p.id for p in first_write])
                     for var in self.var_names_once:
                         data_dict_once[var] = np.array([getattr(p, var) for p in first_write])
