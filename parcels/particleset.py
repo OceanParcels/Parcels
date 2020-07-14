e71e4660089a52f582d0543ccdce4ca4ca77f5f1
Christian Kehl <c.kehl@uu.nl>
2020-07-14 18:20:29 +0200

    list_of_pset_array_trials - fixing linter errors

--- parcels/particleset.py
+++ parcels/particleset.py
@@ -30,6 +30,7 @@ if MPI:
 
 __all__ = ['ParticleSet']
 
+
 class ParticleSet(object):
     """Container class for storing particle and executing kernel over them.
 
@@ -276,7 +277,6 @@ class ParticleSet(object):
             return (bracket_index, slot_index)
         elif isinstance(pdata, self.pclass):
             # == parameter is a particle itself == #
-            # nparticle = 0
             bracket_index = 0
             bracket_info = self.pid_mapping_bounds[bracket_index]
             while bracket_index < len(self._plist) and ((pdata.id < bracket_info[0]) or (pdata.id > bracket_info[1])):
