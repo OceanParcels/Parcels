bfda735cb490e215733a809fece503282eab1613
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2020-09-03 15:51:37 +0200

    Interface elements needed for iteration

--- parcels/particlesets/baseparticleset.py
+++ parcels/particlesets/baseparticleset.py
@@ -12,9 +12,14 @@ class NDCluster(ABC):
 
 class BaseParticleAccessor(ABC):
     """Interface."""
+    @abstractmethod
+    def __next__(self):
+        """Returns the next value from ParticleSet object's lists"""
+        pass
 
 
 class BaseParticleSet(ParticleCollection, NDCluster):
     """Base ParticleSet."""
+    @abstractmethod
     def __iter__(self):
         pass
