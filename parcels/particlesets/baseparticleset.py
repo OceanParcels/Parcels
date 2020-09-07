9463ecac39ebd5c373178dd0a0d8f5d44d8b3227
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2020-09-07 15:06:20 +0200

    Separating Accessor and Iterator again

--- parcels/particlesets/baseparticleset.py
+++ parcels/particlesets/baseparticleset.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from abc import abstractmethod  # noqa
+from abc import abstractmethod
 
 
 class ParticleCollection(ABC):
@@ -10,16 +10,53 @@ class NDCluster(ABC):
     """Interface."""
 
 
-class BaseParticleAccessor(ABC):
-    """Interface."""
+class BaseParticleSetIterator(ABC):
+    """Interface for the ParticleSet iterator. Provides the ability to
+    iterate over the particles in the ParticleSet."""
+    def ___init___(self):
+        self._head = None
+        self._tail = None
+        self._current = None
+
     @abstractmethod
     def __next__(self):
-        """Returns the next value from ParticleSet object's lists"""
+        """Returns the next value from ParticleSet object's lists."""
         pass
+
+    @property
+    def head(self):
+        return self._head
+
+    @property
+    def tail(self):
+        return self._tail
+
+    @property
+    def current(self):
+        return self._current
+
+
+class BaseParticleAccessor(ABC):
+    """Interface for the ParticleAccessor. Implements a wrapper around
+    particles to provide easy access."""
 
 
 class BaseParticleSet(ParticleCollection, NDCluster):
     """Base ParticleSet."""
     @abstractmethod
     def __iter__(self):
+        """Returns an Iterator for the ParticleSet."""
         pass
+
+    @abstractmethod
+    def data_accessor(self):
+        """Returns an Accessor for the particles in this ParticleSet."""
+        pass
+
+    @property
+    @abstractmethod
+    def size(self):
+        pass
+
+    def __len__(self):
+        return self.size
