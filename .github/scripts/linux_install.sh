24c2cbd0fd2f36d3492a87c7a001be1ff0fe707b
Erik van Sebille <erikvansebille@gmail.com>
2020-09-17 13:20:51 +0200

    changing conda env create to conda create
    See also https://github.com/conda/conda/issues/3859

--- .github/scripts/linux_install.sh
+++ .github/scripts/linux_install.sh
@@ -3,6 +3,6 @@
 wget http://repo.continuum.io/miniconda/${MINICONDA_NAME} -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-conda env create --name parcels --file environment_py${PY_VERSION}_${OS_NAME}.yml
+conda create --name parcels --file environment_py${PY_VERSION}_${OS_NAME}.yml
 source activate parcels
 python setup.py install
