a90d36a26d72617fe4a5fc6711e7d862adb41ea1
Reint <33000433+reint-fischer@users.noreply.github.com>
2020-09-17 10:59:03 +0200

    Sample repeatdt
    Allowing particles to be written when dt=0

--- parcels/particleset.py
+++ parcels/particleset.py
@@ -40,7 +40,7 @@ def _to_write_particles(pd, time):
     Particle will be written if particle.time is between time-dt/2 and time+dt/2
     """
     return (np.less_equal(time - np.abs(pd['dt']/2), pd['time'], where=np.isfinite(pd['time']))
-            & np.greater(time + np.abs(pd['dt']/2), pd['time'], where=np.isfinite(pd['time']))
+            & np.greater_equal(time + np.abs(pd['dt']/2), pd['time'], where=np.isfinite(pd['time']))
             & (np.isfinite(pd['id']))
             & (np.isfinite(pd['time'])))
 
