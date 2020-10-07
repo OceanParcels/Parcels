3ee8df5cd99219997b6eed919c5ef6b52acdbb60
Christian Kehl <c.kehl@uu.nl>
2020-10-07 19:48:30 +0200

    #913 - moved some base-functionality functions from ParticleSetSoA to BaseParticleSet

--- parcels/particlesets/baseparticleset.py
+++ parcels/particlesets/baseparticleset.py
@@ -1,16 +1,29 @@
 import numpy as np
+import inspect
 from abc import ABC
 from abc import abstractmethod
 import warnings
 
 from parcels.tools.statuscodes import OperationCode
 from .collections import ParticleCollection
+from parcels.field import NestedField
+from parcels.field import SummedField
+import progressbar
 
 
 class NDCluster(ABC):
     """Interface."""
 
-
+# == Ammendment CK: The based ParticleSet probably itself does NOT need to be a specific subclass of ParticleCollection.
+# ==                Possibly, the constraint can be resolved by either (a) make the collection just a member variable of
+# ==                the particle set or (b) requiring a collection as 'unused' constraint of the constructor, so that
+# ==                the BaseParticleSet-derived class needs to provide its collection [type] as parameter for the
+# ==                super-class constructor call.
+# == i.e.
+# == class BaseParticleSetNDCluster):
+# ==     def __init__(self, collection_type): # this is NOT an abstract method then
+# ==         assert inspect.isclass(collection_type, ParticleCollection)
+#== END AMMENDMENT
 class BaseParticleSet(ParticleCollection, NDCluster):
     """Base ParticleSet."""
     def data_accessor(self):
@@ -22,6 +35,115 @@ class BaseParticleSet(ParticleCollection, NDCluster):
             DeprecationWarning,
             stacklevel=2
         )
+
+    @staticmethod
+    def lonlatdepth_dtype_from_field_interp_method(field):
+        if type(field) in [SummedField, NestedField]:
+            for f in field:
+                if f.interp_method == 'cgrid_velocity':
+                    return np.float64
+        else:
+            if field.interp_method == 'cgrid_velocity':
+                return np.float64
+        return np.float32
+
+    @abstractmethod
+    def cstruct(self):
+        """
+        'cstruct' returns the ctypes mapping of the combined collections cstruct and the fieldset cstruct.
+        This depends on the specific structure in question.
+        """
+        pass
+
+    def __create_progressbar(self, starttime, endtime):
+        pbar = None
+        try:
+            pbar = progressbar.ProgressBar(max_value=abs(endtime - starttime)).start()
+        except:  # for old versions of progressbar
+            try:
+                pbar = progressbar.ProgressBar(maxvalue=abs(endtime - starttime)).start()
+            except:  # for even older OR newer versions
+                pbar = progressbar.ProgressBar(maxval=abs(endtime - starttime)).start()
+        return pbar
+
+    @classmethod
+    def from_list(cls, fieldset, pclass, lon, lat, depth=None, time=None, repeatdt=None, lonlatdepth_dtype=None, **kwargs):
+        """Initialise the ParticleSet from lists of lon and lat
+
+        :param fieldset: :mod:`parcels.fieldset.FieldSet` object from which to sample velocity
+        :param pclass: mod:`parcels.particle.JITParticle` or :mod:`parcels.particle.ScipyParticle`
+                 object that defines custom particle
+        :param lon: List of initial longitude values for particles
+        :param lat: List of initial latitude values for particles
+        :param depth: Optional list of initial depth values for particles. Default is 0m
+        :param time: Optional list of start time values for particles. Default is fieldset.U.time[0]
+        :param repeatdt: Optional interval (in seconds) on which to repeat the release of the ParticleSet
+        :param lonlatdepth_dtype: Floating precision for lon, lat, depth particle coordinates.
+               It is either np.float32 or np.float64. Default is np.float32 if fieldset.U.interp_method is 'linear'
+               and np.float64 if the interpolation method is 'cgrid_velocity'
+        Other Variables can be initialised using further arguments (e.g. v=... for a Variable named 'v')
+       """
+        return cls(fieldset=fieldset, pclass=pclass, lon=lon, lat=lat, depth=depth, time=time, repeatdt=repeatdt, lonlatdepth_dtype=lonlatdepth_dtype, **kwargs)
+
+    @classmethod
+    def from_line(cls, fieldset, pclass, start, finish, size, depth=None, time=None, repeatdt=None, lonlatdepth_dtype=None):
+        """Initialise the ParticleSet from start/finish coordinates with equidistant spacing
+        Note that this method uses simple numpy.linspace calls and does not take into account
+        great circles, so may not be a exact on a globe
+
+        :param fieldset: :mod:`parcels.fieldset.FieldSet` object from which to sample velocity
+        :param pclass: mod:`parcels.particle.JITParticle` or :mod:`parcels.particle.ScipyParticle`
+                 object that defines custom particle
+        :param start: Starting point for initialisation of particles on a straight line.
+        :param finish: End point for initialisation of particles on a straight line.
+        :param size: Initial size of particle set
+        :param depth: Optional list of initial depth values for particles. Default is 0m
+        :param time: Optional start time value for particles. Default is fieldset.U.time[0]
+        :param repeatdt: Optional interval (in seconds) on which to repeat the release of the ParticleSet
+        :param lonlatdepth_dtype: Floating precision for lon, lat, depth particle coordinates.
+               It is either np.float32 or np.float64. Default is np.float32 if fieldset.U.interp_method is 'linear'
+               and np.float64 if the interpolation method is 'cgrid_velocity'
+        """
+
+        lon = np.linspace(start[0], finish[0], size)
+        lat = np.linspace(start[1], finish[1], size)
+        if type(depth) in [int, float]:
+            depth = [depth] * size
+        return cls(fieldset=fieldset, pclass=pclass, lon=lon, lat=lat, depth=depth, time=time, repeatdt=repeatdt, lonlatdepth_dtype=lonlatdepth_dtype)
+
+    @classmethod
+    @abstractmethod
+    def monte_carlo_sample(cls, start_field, size, mode='monte_carlo'):
+        """
+        Converts a starting field into a monte-carlo sample of lons and lats.
+
+        :param start_field: :mod:`parcels.fieldset.Field` object for initialising particles stochastically (horizontally)  according to the presented density field.
+
+        returns list(lon), list(lat)
+        """
+        pass
+
+    @classmethod
+    def from_field(cls, fieldset, pclass, start_field, size, mode='monte_carlo', depth=None, time=None, repeatdt=None, lonlatdepth_dtype=None):
+        """Initialise the ParticleSet randomly drawn according to distribution from a field
+
+        :param fieldset: :mod:`parcels.fieldset.FieldSet` object from which to sample velocity
+        :param pclass: mod:`parcels.particle.JITParticle` or :mod:`parcels.particle.ScipyParticle`
+                 object that defines custom particle
+        :param start_field: Field for initialising particles stochastically (horizontally)  according to the presented density field.
+        :param size: Initial size of particle set
+        :param mode: Type of random sampling. Currently only 'monte_carlo' is implemented
+        :param depth: Optional list of initial depth values for particles. Default is 0m
+        :param time: Optional start time value for particles. Default is fieldset.U.time[0]
+        :param repeatdt: Optional interval (in seconds) on which to repeat the release of the ParticleSet
+        :param lonlatdepth_dtype: Floating precision for lon, lat, depth particle coordinates.
+               It is either np.float32 or np.float64. Default is np.float32 if fieldset.U.interp_method is 'linear'
+               and np.float64 if the interpolation method is 'cgrid_velocity'
+        """
+
+        lon, lat = cls.monte_carlo_sample(start_field, mode)
+
+        return cls(fieldset=fieldset, pclass=pclass, lon=lon, lat=lat, depth=depth, time=time, lonlatdepth_dtype=lonlatdepth_dtype, repeatdt=repeatdt)
 
     @abstractmethod
     def execute(self, pyfunc, endtime, runtime, dt, moviedt, recovery,
@@ -55,4 +177,73 @@ class BaseParticleSet(ParticleCollection, NDCluster):
         :param postIterationCallbacks: (Optional) Array of functions that are to be called after each iteration (post-process, non-Kernel)
         :param callbackdt: (Optional, in conjecture with 'postIterationCallbacks) timestep inverval to (latestly) interrupt the running kernel and invoke post-iteration callbacks from 'postIterationCallbacks'
         """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_particlefile(cls, fieldset, pclass, filename, restart=True, restarttime=None, repeatdt=None, lonlatdepth_dtype=None, **kwargs):
+        """Initialise the ParticleSet from a netcdf ParticleFile.
+        This creates a new ParticleSet based on locations of all particles written
+        in a netcdf ParticleFile at a certain time. Particle IDs are preserved if restart=True
+
+        :param fieldset: :mod:`parcels.fieldset.FieldSet` object from which to sample velocity
+        :param pclass: mod:`parcels.particle.JITParticle` or :mod:`parcels.particle.ScipyParticle`
+                 object that defines custom particle
+        :param filename: Name of the particlefile from which to read initial conditions
+        :param restart: Boolean to signal if pset is used for a restart (default is True).
+               In that case, Particle IDs are preserved.
+        :param restarttime: time at which the Particles will be restarted. Default is the last time written.
+               Alternatively, restarttime could be a time value (including np.datetime64) or
+               a callable function such as np.nanmin. The last is useful when running with dt < 0.
+        :param repeatdt: Optional interval (in seconds) on which to repeat the release of the ParticleSet
+        :param lonlatdepth_dtype: Floating precision for lon, lat, depth particle coordinates.
+               It is either np.float32 or np.float64. Default is np.float32 if fieldset.U.interp_method is 'linear'
+               and np.float64 if the interpolation method is 'cgrid_velocity'
+        """
+        pass
+
+    def show(self, with_particles=True, show_time=None, field=None, domain=None, projection=None,
+             land=True, vmin=None, vmax=None, savefile=None, animation=False, **kwargs):
+        """Method to 'show' a Parcels ParticleSet
+
+        :param with_particles: Boolean whether to show particles
+        :param show_time: Time at which to show the ParticleSet
+        :param field: Field to plot under particles (either None, a Field object, or 'vector')
+        :param domain: dictionary (with keys 'N', 'S', 'E', 'W') defining domain to show
+        :param projection: type of cartopy projection to use (default PlateCarree)
+        :param land: Boolean whether to show land. This is ignored for flat meshes
+        :param vmin: minimum colour scale (only in single-plot mode)
+        :param vmax: maximum colour scale (only in single-plot mode)
+        :param savefile: Name of a file to save the plot to
+        :param animation: Boolean whether result is a single plot, or an animation
+        """
+
+    def density(self, field_name=None, particle_val=None, relative=False, area_scale=False):
+        """Method to calculate the density of particles in a ParticleSet from their locations,
+        through a 2D histogram.
+
+        :param field: Optional :mod:`parcels.field.Field` object to calculate the histogram
+                      on. Default is `fieldset.U`
+        :param particle_val: Optional numpy-array of values to weigh each particle with,
+                             or string name of particle variable to use weigh particles with.
+                             Default is None, resulting in a value of 1 for each particle
+        :param relative: Boolean to control whether the density is scaled by the total
+                         weight of all particles. Default is False
+        :param area_scale: Boolean to control whether the density is scaled by the area
+                           (in m^2) of each grid cell. Default is False
+        """
+        pass
+
+    @abstractmethod
+    def Kernel(self, pyfunc, c_include="", delete_cfiles=True):
+        """Wrapper method to convert a `pyfunc` into a :class:`parcels.kernel.Kernel` object
+        based on `fieldset` and `ptype` of the ParticleSet
+        :param delete_cfiles: Boolean whether to delete the C-files after compilation in JIT mode (default is True)
+        """
+        pass
+
+    @abstractmethod
+    def ParticleFile(self, *args, **kwargs):
+        """Wrapper method to initialise a :class:`parcels.particlefile.ParticleFile`
+        object from the ParticleSet"""
         pass
