e939287d97917b2a24a5ca15cb79b9004534cafc
Erik van Sebille <erikvansebille@gmail.com>
2020-10-14 17:22:54 +0200

    reverting back to original python random unit test

--- tests/test_kernel_language.py
+++ tests/test_kernel_language.py
@@ -4,6 +4,7 @@ from parcels.kernels.EOSseawaterproperties import PressureFromLatDepth, PtempFro
 from parcels import ParcelsRandom
 import numpy as np
 import pytest
+import random as py_random
 from os import path
 import sys
 
@@ -264,10 +265,11 @@ def test_fieldset_access(fieldset, mode):
 
 
 def random_series(npart, rngfunc, rngargs, mode):
-    ParcelsRandom.seed(1234)
-    func = getattr(ParcelsRandom, rngfunc)
+    random = ParcelsRandom if mode == 'jit' else py_random
+    random.seed(1234)
+    func = getattr(random, rngfunc)
     series = [func(*rngargs) for _ in range(npart)]
-    ParcelsRandom.seed(1234)  # Reset the RNG seed
+    random.seed(1234)  # Reset the RNG seed
     return series
 
 
@@ -285,8 +287,9 @@ def test_random_float(mode, rngfunc, rngargs, npart=10):
                        lon=np.linspace(0., 1., npart),
                        lat=np.zeros(npart) + 0.5)
     series = random_series(npart, rngfunc, rngargs, mode)
+    rnglib = 'ParcelsRandom' if mode == 'jit' else 'random'
     kernel = expr_kernel('TestRandom_%s' % rngfunc, pset,
-                         'ParcelsRandom.%s(%s)' % (rngfunc, ', '.join([str(a) for a in rngargs])))
+                         '%s.%s(%s)' % (rnglib, rngfunc, ', '.join([str(a) for a in rngargs])))
     pset.execute(kernel, endtime=1., dt=1.)
     assert np.allclose(pset.p, series, atol=1e-9)
 
