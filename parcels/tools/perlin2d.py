be7f684bacec08f3f14efcdbd6774c70e83a9bc3
Christian Kehl <c.kehl@uu.nl>
2020-10-26 18:46:33 +0100

    sorted_pset_trials - removed linter errors from perlin codes

--- parcels/tools/perlin2d.py
+++ parcels/tools/perlin2d.py
@@ -4,33 +4,35 @@ from numpy.random import RandomState, SeedSequence
 from scipy import ndimage
 from time import time
 
+
 def generate_perlin_noise_2d(shape, res):
     def f(t):
         return 6*t**5 - 15*t**4 + 10*t**3
-    
+
     delta = (res[0] / shape[0], res[1] / shape[1])
     d = (shape[0] // res[0], shape[1] // res[1])
-    grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]].transpose(1, 2, 0) % 1
     # Gradients
     angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
     gradients = np.dstack((np.cos(angles), np.sin(angles)))
-    g00 = gradients[0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1)
-    g10 = gradients[1:  ,0:-1].repeat(d[0], 0).repeat(d[1], 1)
-    g01 = gradients[0:-1,1:  ].repeat(d[0], 0).repeat(d[1], 1)
-    g11 = gradients[1:  ,1:  ].repeat(d[0], 0).repeat(d[1], 1)
+    g00 = gradients[0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g10 = gradients[1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g01 = gradients[0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1)
+    g11 = gradients[1:, 1:].repeat(d[0], 0).repeat(d[1], 1)
     # Ramps
-    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
-    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
-    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
-    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    n00 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1])) * g00, 2)
+    n10 = np.sum(np.dstack((grid[:, :, 0]-1, grid[:, :, 1])) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1]-1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:, :, 0]-1, grid[:, :, 1]-1)) * g11, 2)
     # Interpolation
     t = f(grid)
-    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
-    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
-    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
-        
+    n0 = n00*(1-t[:, :, 0]) + t[:, :, 0]*n10
+    n1 = n01*(1-t[:, :, 0]) + t[:, :, 0]*n11
+    return np.sqrt(2)*((1-t[:, :, 1])*n0 + t[:, :, 1]*n1)
+
+
 def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
-    rs = RandomState( MT19937(SeedSequence(int(round(time() * 1000)))) )
+    RandomState(MT19937(SeedSequence(int(round(time() * 1000)))))  # rs =
     noise = np.zeros(shape)
     frequency = 1
     amplitude = 1
@@ -40,8 +42,9 @@ def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
         amplitude *= persistence
     return noise
 
+
 def generate_fractal_noise_temporal2d(shape, tsteps, res, octaves=1, persistence=0.5, max_shift=((-1, 1), (-1, 1))):
-    rs = RandomState( MT19937(SeedSequence(int(round(time() * 1000)))) )
+    RandomState(MT19937(SeedSequence(int(round(time() * 1000)))))  # rs =
     noise = np.zeros(shape)
     frequency = 1
     amplitude = 1
@@ -54,21 +57,22 @@ def generate_fractal_noise_temporal2d(shape, tsteps, res, octaves=1, persistence
     result = np.zeros(ishape)
     timage = np.zeros(noise.shape)
     for i in range(0, tsteps):
-        result[i,:,:] = noise
+        result[i, :, :] = noise
         sx = np.random.randint(max_shift[0][0], max_shift[0][1], dtype=np.int32)
         sy = np.random.randint(max_shift[1][0], max_shift[1][1], dtype=np.int32)
         ndimage.shift(noise, (sx, sy), timage, order=3, mode='mirror')
         noise = timage
     return result
-    
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    
+
     np.random.seed(0)
     noise = generate_perlin_noise_2d((256, 256), (8, 8))
     plt.imshow(noise, cmap='gray', interpolation='lanczos')
     plt.colorbar()
-    
+
     np.random.seed(0)
     noise = generate_fractal_noise_2d((256, 256), (8, 8), 5)
     plt.figure()
--- parcels/tools/perlin3d.py
+++ parcels/tools/perlin3d.py
@@ -3,48 +3,50 @@ from numpy.random import MT19937
 from numpy.random import RandomState, SeedSequence
 from scipy import ndimage
 from time import time
-    
+
+
 def generate_perlin_noise_3d(shape, res):
     def f(t):
         return 6*t**5 - 15*t**4 + 10*t**3
-    
+
     delta = (res[0] / shape[0], res[1] / shape[1], res[2] / shape[2])
     d = (shape[0] // res[0], shape[1] // res[1], shape[2] // res[2])
-    grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1],0:res[2]:delta[2]]
+    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1], 0:res[2]:delta[2]]
     grid = grid.transpose(1, 2, 3, 0) % 1
     # Gradients
     theta = 2*np.pi*np.random.rand(res[0]+1, res[1]+1, res[2]+1)
     phi = 2*np.pi*np.random.rand(res[0]+1, res[1]+1, res[2]+1)
     gradients = np.stack((np.sin(phi)*np.cos(theta), np.sin(phi)*np.sin(theta), np.cos(phi)), axis=3)
-    g000 = gradients[0:-1,0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g100 = gradients[1:  ,0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g010 = gradients[0:-1,1:  ,0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g110 = gradients[1:  ,1:  ,0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g001 = gradients[0:-1,0:-1,1:  ].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g101 = gradients[1:  ,0:-1,1:  ].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g011 = gradients[0:-1,1:  ,1:  ].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
-    g111 = gradients[1:  ,1:  ,1:  ].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g000 = gradients[0:-1, 0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g100 = gradients[1:, 0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g010 = gradients[0:-1, 1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g110 = gradients[1:, 1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g001 = gradients[0:-1, 0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g101 = gradients[1:, 0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g011 = gradients[0:-1, 1:, 1:].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
+    g111 = gradients[1:, 1:, 1:].repeat(d[0], 0).repeat(d[1], 1).repeat(d[2], 2)
     # Ramps
-    n000 = np.sum(np.stack((grid[:,:,:,0]  , grid[:,:,:,1]  , grid[:,:,:,2]  ), axis=3) * g000, 3)
-    n100 = np.sum(np.stack((grid[:,:,:,0]-1, grid[:,:,:,1]  , grid[:,:,:,2]  ), axis=3) * g100, 3)
-    n010 = np.sum(np.stack((grid[:,:,:,0]  , grid[:,:,:,1]-1, grid[:,:,:,2]  ), axis=3) * g010, 3)
-    n110 = np.sum(np.stack((grid[:,:,:,0]-1, grid[:,:,:,1]-1, grid[:,:,:,2]  ), axis=3) * g110, 3)
-    n001 = np.sum(np.stack((grid[:,:,:,0]  , grid[:,:,:,1]  , grid[:,:,:,2]-1), axis=3) * g001, 3)
-    n101 = np.sum(np.stack((grid[:,:,:,0]-1, grid[:,:,:,1]  , grid[:,:,:,2]-1), axis=3) * g101, 3)
-    n011 = np.sum(np.stack((grid[:,:,:,0]  , grid[:,:,:,1]-1, grid[:,:,:,2]-1), axis=3) * g011, 3)
-    n111 = np.sum(np.stack((grid[:,:,:,0]-1, grid[:,:,:,1]-1, grid[:,:,:,2]-1), axis=3) * g111, 3)
+    n000 = np.sum(np.stack((grid[:, :, :, 0], grid[:, :, :, 1], grid[:, :, :, 2]), axis=3) * g000, 3)
+    n100 = np.sum(np.stack((grid[:, :, :, 0]-1, grid[:, :, :, 1], grid[:, :, :, 2]), axis=3) * g100, 3)
+    n010 = np.sum(np.stack((grid[:, :, :, 0], grid[:, :, :, 1]-1, grid[:, :, :, 2]), axis=3) * g010, 3)
+    n110 = np.sum(np.stack((grid[:, :, :, 0]-1, grid[:, :, :, 1]-1, grid[:, :, :, 2]), axis=3) * g110, 3)
+    n001 = np.sum(np.stack((grid[:, :, :, 0], grid[:, :, :, 1], grid[:, :, :, 2]-1), axis=3) * g001, 3)
+    n101 = np.sum(np.stack((grid[:, :, :, 0]-1, grid[:, :, :, 1], grid[:, :, :, 2]-1), axis=3) * g101, 3)
+    n011 = np.sum(np.stack((grid[:, :, :, 0], grid[:, :, :, 1]-1, grid[:, :, :, 2]-1), axis=3) * g011, 3)
+    n111 = np.sum(np.stack((grid[:, :, :, 0]-1, grid[:, :, :, 1]-1, grid[:, :, :, 2]-1), axis=3) * g111, 3)
     # Interpolation
     t = f(grid)
-    n00 = n000*(1-t[:,:,:,0]) + t[:,:,:,0]*n100
-    n10 = n010*(1-t[:,:,:,0]) + t[:,:,:,0]*n110
-    n01 = n001*(1-t[:,:,:,0]) + t[:,:,:,0]*n101
-    n11 = n011*(1-t[:,:,:,0]) + t[:,:,:,0]*n111
-    n0 = (1-t[:,:,:,1])*n00 + t[:,:,:,1]*n10
-    n1 = (1-t[:,:,:,1])*n01 + t[:,:,:,1]*n11
-    return ((1-t[:,:,:,2])*n0 + t[:,:,:,2]*n1)
-    
+    n00 = n000*(1-t[:, :, :, 0]) + t[:, :, :, 0]*n100
+    n10 = n010*(1-t[:, :, :, 0]) + t[:, :, :, 0]*n110
+    n01 = n001*(1-t[:, :, :, 0]) + t[:, :, :, 0]*n101
+    n11 = n011*(1-t[:, :, :, 0]) + t[:, :, :, 0]*n111
+    n0 = (1-t[:, :, :, 1])*n00 + t[:, :, :, 1]*n10
+    n1 = (1-t[:, :, :, 1])*n01 + t[:, :, :, 1]*n11
+    return (1-t[:, :, :, 2])*n0 + t[:, :, :, 2]*n1
+
+
 def generate_fractal_noise_3d(shape, res, octaves=1, persistence=0.5):
-    rs = RandomState( MT19937(SeedSequence(int(round(time() * 1000)))) )
+    RandomState(MT19937(SeedSequence(int(round(time() * 1000)))))
     noise = np.zeros(shape)
     frequency = 1
     amplitude = 1
@@ -54,8 +56,9 @@ def generate_fractal_noise_3d(shape, res, octaves=1, persistence=0.5):
         amplitude *= persistence
     return noise
 
+
 def generate_fractal_noise_temporal3d(shape, tsteps, res, octaves=1, persistence=0.5, max_shift=(1, 1, 1)):
-    rs = RandomState( MT19937(SeedSequence(int(round(time() * 1000)))) )
+    RandomState(MT19937(SeedSequence(int(round(time() * 1000)))))
     noise = np.zeros(shape)
     frequency = 1
     amplitude = 1
@@ -63,12 +66,11 @@ def generate_fractal_noise_temporal3d(shape, tsteps, res, octaves=1, persistence
         noise += amplitude * generate_perlin_noise_3d(shape, (frequency*res[0], frequency*res[1], frequency*res[2]))
         frequency *= 2
         amplitude *= persistence
-
     ishape = (tsteps, )+shape
     result = np.zeros(ishape)
     timage = np.zeros(noise.shape)
     for i in range(0, tsteps):
-        result[i,:,:] = noise
+        result[i, :, :] = noise
         sx = np.random.randint(-max_shift[0], max_shift[0], dtype=np.int32)
         sy = np.random.randint(-max_shift[1], max_shift[1], dtype=np.int32)
         sz = np.random.randint(-max_shift[2], max_shift[2], dtype=np.int32)
@@ -76,13 +78,14 @@ def generate_fractal_noise_temporal3d(shape, tsteps, res, octaves=1, persistence
         noise = timage
     return result
 
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
-    
+
     np.random.seed(0)
     noise = generate_fractal_noise_3d((32, 256, 256), (1, 4, 4), 4)
-    
+
     fig = plt.figure()
     images = [[plt.imshow(layer, cmap='gray', interpolation='lanczos', animated=True)] for layer in noise]
     animation = animation.ArtistAnimation(fig, images, interval=50, blit=True)
