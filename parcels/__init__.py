84b7964af5049d8f6ba6d19a66626dc0e461a5c2
Christian Kehl <c.kehl@uu.nl>
2020-11-06 20:28:00 +0100

    soa_benchmark - benchmarks are running, but REALLY REALLY slow - something's going wrong here.

--- parcels/__init__.py
+++ parcels/__init__.py
@@ -4,8 +4,10 @@ __version__ = version
 from parcels.fieldset import *  # noqa
 from parcels.particle import *  # noqa
 from parcels.particleset import *  # noqa
+from parcels.particleset_benchmark import *  # noga
 from parcels.field import *  # noqa
 from parcels.kernel import *  # noqa
+from parcels.kernel_benchmark import *  # noga
 import parcels.rng as ParcelsRandom  # noqa
 from parcels.particlefile import *  # noqa
 from parcels.kernels import *  # noqa
--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -3,3 +3,4 @@ from .statuscodes import *  # noqa
 from .interpolation_utils import *  # noqa
 from .loggers import *  # noqa
 from .timer import *  # noqa
+from .performance_logger import *  # noga
