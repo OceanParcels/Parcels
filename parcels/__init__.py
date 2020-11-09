919cccd9eaf2747859e6eb4e2be26105ecbea4de
Christian Kehl <c.kehl@uu.nl>
2020-11-09 13:54:45 +0100

    sorted_pset_trials - fixing some late errors with MPI nparticles logging.

--- parcels/__init__.py
+++ parcels/__init__.py
@@ -4,7 +4,7 @@ __version__ = version
 from parcels.fieldset import *  # noqa
 from parcels.particle import *  # noqa
 from parcels.particleset_vectorized import *  # noqa
-from parcels.particleset_benchmark import *  # noqa
+from parcels.particleset_vectorized_benchmark import *  # noqa
 from parcels.kernel_vectorized import *  # noqa
 from parcels.field import *  # noqa
 import parcels.rng as random  # noqa
