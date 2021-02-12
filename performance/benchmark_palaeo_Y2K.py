ec426afc37b29ad1e26596639acae4957c184b8f
Christian Kehl <c.kehl@uu.nl>
2021-02-12 18:23:33 +0100

    benchmarking - disabling periodic loading for bathymetry entirely.

--- performance/benchmark_palaeo_Y2K.py
+++ performance/benchmark_palaeo_Y2K.py
@@ -108,7 +108,7 @@ def set_nemo_fieldset(ufiles, vfiles, wfiles, tfiles, pfiles, dfiles, ifiles, bf
                   'CO2': {'lon': 'glamf', 'lat': 'gphif', 'time': 'time_counter'} } #,
     bfiles = {'lon': mesh_mask, 'lat': mesh_mask, 'data': [bfile, ]}
     bvariables = ('B', 'Bathymetry')
-    bdimensions = {'lon': 'glamf', 'lat': 'gphif', 'time': 'time'}
+    bdimensions = {'lon': 'glamf', 'lat': 'gphif'}
     bchs = False
 
     # ==== depth-split need to be 75 so all (2D- and 3D) fields are chunked the same way
@@ -160,10 +160,10 @@ def set_nemo_fieldset(ufiles, vfiles, wfiles, tfiles, pfiles, dfiles, ifiles, bf
         else:
             try:
                 fieldset = FieldSet.from_nemo(filenames, variables, dimensions, time_periodic=delta(days=366), field_chunksize=chs)
-                Bfield = Field.from_netcdf(bfiles, bvariables, bdimensions, time_periodic=1, interp_method='cgrid_tracer', field_chunksize=bchs)
+                Bfield = Field.from_netcdf(bfiles, bvariables, bdimensions, allow_time_extrapolation=True, interp_method='cgrid_tracer', field_chunksize=bchs)
             except (SyntaxError, ):
                 fieldset = FieldSet.from_nemo(filenames, variables, dimensions, time_periodic=delta(days=366), chunksize=nchs)
-                Bfield = Field.from_netcdf(bfiles, bvariables, bdimensions, time_periodic=1, interp_method='cgrid_tracer', chunksize=bchs)
+                Bfield = Field.from_netcdf(bfiles, bvariables, bdimensions, allow_time_extrapolation=True, interp_method='cgrid_tracer', chunksize=bchs)
         fieldset.add_field(Bfield, 'B')
         fieldset.U.vmax = 10
         fieldset.V.vmax = 10
