0ca0ae737ed8ef42924161c0160c98654f19a759
Christian Kehl <c.kehl@uu.nl>
2021-02-12 15:39:59 +0100

    generalize_particlefile - linter errors removed.

--- parcels/particlefile.py
+++ parcels/particlefile.py
@@ -1,5 +1,7 @@
 """Module controlling the writing of ParticleSets to NetCDF file"""
-from parcels.baseparticlefile import BaseParticleFile, _set_calendar  # NOGA
+
+from parcels.baseparticlefile import BaseParticleFile  # noqa: F401
+from parcels.baseparticlefile import _set_calendar  # noqa: F401
 from parcels.particlefilesoa import ParticleFileSOA
 
 ParticleFile = ParticleFileSOA
