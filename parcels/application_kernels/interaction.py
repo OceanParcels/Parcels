2c4b94469897052915a9c01b027d5214581ec401
Raoul Schram <r.d.schram@uu.nl>
2021-04-14 10:40:34 +0200

    Add assymetric force

--- parcels/application_kernels/interaction.py
+++ parcels/application_kernels/interaction.py
@@ -29,3 +29,39 @@ def DummyMoveNeighbour(particle, fieldset, time, neighbours, mutator):
         mutator[neighbour_ids[i_min_dist]].append(f)
 
     return StateCode.Success
+
+
+def AsymmetricAttraction(particle, fieldset, time, neighbours, mutator):
+    distances = []
+    na_neighbors = []
+    if not particle.attractor:
+        return StateCode.Success
+
+    for n in neighbours:
+        if n.attractor:
+            continue
+        distances.append(
+            relative_3d_distance(particle.lat, particle.lon, particle.depth,
+                                 n.lat, n.lon, n.depth))
+        na_neighbors.append(n)
+
+    assert fieldset.mesh == "flat"
+    velocity_param = 0.01
+    for n in na_neighbors:
+        assert n.dt == particle.dt
+        dx = np.array([particle.lat-n.lat, particle.long-n.long,
+                       particle.depth-n.depth])
+        dx_norm = np.linalg.norm(dx)
+        velocity = velocity_param/(dx_norm**2)
+
+        distance = velocity*n.dt
+        d_vec = distance*dx/dx_norm
+
+        def f(n):
+            n.lat += d_vec[0]
+            n.long += d_vec[1]
+            n.depth += d_vec[2]
+
+        mutator[n.id].append(f)
+
+    return StateCode.Success
