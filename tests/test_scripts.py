9148faa08dfdd6efdd1910e4c040b2cca0329408
Christian Kehl <c.kehl@uu.nl>
2021-05-25 19:19:16 +0200

    expand_AoS_to_master_tests (#1026) - added test for AoS to test_scripts.py

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,5 +1,6 @@
-from parcels import (FieldSet, ParticleSet, JITParticle, AdvectionRK4,
-                     plotTrajectoriesFile)
+from parcels import (FieldSet, JITParticle, AdvectionRK4, plotTrajectoriesFile)
+from parcels import ParticleSetSOA, ParticleFileSOA, KernelSOA  # noqa
+from parcels import ParticleSetAOS, ParticleFileAOS, KernelAOS  # noqa
 from datetime import timedelta as delta
 import numpy as np
 import pytest
@@ -8,11 +9,15 @@ from parcels.tools.loggers import logger
 import sys
 
 
-def create_outputfiles(dir):
+pset_type = {'soa': {'pset': ParticleSetSOA, 'pfile': ParticleFileSOA, 'kernel': KernelSOA},
+             'aos': {'pset': ParticleSetAOS, 'pfile': ParticleFileAOS, 'kernel': KernelAOS}}
+
+
+def create_outputfiles(dir, pset_mode):
     datafile = path.join(path.dirname(__file__), 'test_data', 'testfields')
 
     fieldset = FieldSet.from_parcels(datafile, allow_time_extrapolation=True)
-    pset = ParticleSet(fieldset=fieldset, lon=[], lat=[], pclass=JITParticle)
+    pset = pset_type[pset_mode]['pset'](fieldset=fieldset, lon=[], lat=[], pclass=JITParticle)
     npart = 10
     delaytime = delta(hours=1)
     endtime = delta(hours=24)
@@ -25,9 +30,8 @@ def create_outputfiles(dir):
 
     for t in range(npart):
         time = 0 if len(pset) == 0 else pset[0].time
-        pset.add(ParticleSet(pclass=JITParticle, lon=x, lat=lat[t], fieldset=fieldset, time=time))
-        pset.execute(AdvectionRK4, runtime=delaytime, dt=delta(minutes=5),
-                     output_file=output_file)
+        pset.add(pset_type[pset_mode]['pset'](pclass=JITParticle, lon=x, lat=lat[t], fieldset=fieldset, time=time))
+        pset.execute(AdvectionRK4, runtime=delaytime, dt=delta(minutes=5), output_file=output_file)
 
     pset.execute(AdvectionRK4, runtime=endtime-npart*delaytime,
                  dt=delta(minutes=5), output_file=output_file)
@@ -36,10 +40,11 @@ def create_outputfiles(dir):
     return fp
 
 
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
 @pytest.mark.parametrize('mode', ['2d', '3d', 'movie2d', 'hist2d'])
-def test_plotting(mode, tmpdir):
+def test_plotting(pset_mode, mode, tmpdir):
     if mode == '3d' and sys.platform in ['linux', 'linux2']:
         logger.info('Skipping 3d test in linux Travis, since it fails to find display to connect')
         return
-    fp = create_outputfiles(tmpdir)
+    fp = create_outputfiles(tmpdir, pset_mode)
     plotTrajectoriesFile(fp, mode=mode, show_plt=False)
