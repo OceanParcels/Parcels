50306a04c6878b4a0db341903d6fd5eb8919627a
Christian Kehl <c.kehl@uu.nl>
2021-05-25 19:15:20 +0200

    expand_AoS_to_master_tests (#1026) - added test for AoS to test_particle_collections.py

--- tests/test_particle_collections.py
+++ tests/test_particle_collections.py
@@ -1,10 +1,12 @@
 from parcels import FieldSet, ScipyParticle, JITParticle
-from parcels import ParticleSetSOA
+from parcels import ParticleSetSOA, ParticleFileSOA, KernelSOA  # noqa
+from parcels import ParticleSetAOS, ParticleFileAOS, KernelAOS  # noqa
 import numpy as np
 import pytest
 
 ptype = {'scipy': ScipyParticle, 'jit': JITParticle}
-psettype = {'soa': ParticleSetSOA}
+pset_type = {'soa': {'pset': ParticleSetSOA, 'pfile': ParticleFileSOA, 'kernel': KernelSOA},
+             'aos': {'pset': ParticleSetAOS, 'pfile': ParticleFileAOS, 'kernel': KernelAOS}}
 
 
 def fieldset(xdim=40, ydim=100):
@@ -23,39 +25,43 @@ def fieldset_fixture(xdim=40, ydim=100):
     return fieldset(xdim=xdim, ydim=ydim)
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_iteration_forward(fieldset, pt, npart=10):
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_iteration_forward(fieldset, pset_mode, npart=10):
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
     assert np.all(np.isclose(np.array([p.id for p in pset]), range(npart)+pset[0].id))
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_iteration_backward(fieldset, pt, npart=10):
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_iteration_backward(fieldset, pset_mode, npart=10):
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
     assert np.all(np.isclose(np.array([p.id for p in reversed(pset)]), pset[0].id+np.arange(npart-1, -1, -1)))
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_get(fieldset, pt, npart=10):
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_get(fieldset, pset_mode, npart=10):
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
     assert np.all(np.isclose([pset.collection.get(i).lon for i in range(npart)], np.linspace(0, 1, npart)))
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_get_single_by_index(fieldset, pt, npart=10):
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_get_single_by_index(fieldset, pset_mode, npart=10):
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
     assert np.all(np.isclose([pset.collection.get_single_by_index(i).lon for i in range(npart)], np.linspace(0, 1, npart)))
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_get_single_by_ID(fieldset, pt, npart=10):
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
-    ids = pset.collection._data['id']
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_get_single_by_ID(fieldset, pset_mode, npart=10):
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=np.zeros(npart), pclass=JITParticle)
+    ids = None
+    if pset_mode =='soa':
+        ids = pset.collection._data['id']
+    elif pset_mode == 'aos':
+        ids = np.array([pset.collection._data[i].id for i in range(len(pset))], dtype=np.int64)
     assert np.all(np.isclose([pset.collection.get_single_by_ID(np.int64(i)).lon for i in ids], np.linspace(0, 1, npart)))
 
 
-@pytest.mark.parametrize('pt', ['soa'])
-def test_pset_getattr(fieldset, pt, npart=10):
+@pytest.mark.parametrize('pset_mode', ['soa', 'aos'])
+def test_pset_getattr(fieldset, pset_mode, npart=10):
     lats = np.random.random(npart)
-    pset = psettype[pt](fieldset, lon=np.linspace(0, 1, npart), lat=lats, pclass=JITParticle)
+    pset = pset_type[pset_mode]['pset'](fieldset, lon=np.linspace(0, 1, npart), lat=lats, pclass=JITParticle)
     assert np.allclose(pset.lat, lats)
