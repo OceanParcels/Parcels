635bf573ddebb4f076f9f7d2ba5b082cae708269
Christian Kehl <c.kehl@uu.nl>
2021-06-07 18:01:40 +0200

    integrate_nodes_to_master (#1034) - integrate ID generators and messaging services into the tools-package init-function

--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -2,5 +2,11 @@ from .converters import *  # noqa
 from .global_statics import *  # noqa
 from .statuscodes import *  # noqa
 from .interpolation_utils import *  # noqa
+from .id_generators import *  # noga
 from .loggers import *  # noqa
 from .timer import *  # noqa
+
+# idgen = GenerateID_Service(SpatioTemporalIdGenerator)
+# idgen = GenerateID_Service(SequentialIdGenerator)
+# idgen.setDepthLimits(0, 100.0)  # TODO: to be changed to some acceptable gobal max, e.g. Marianna Trench depth in [m]
+# idgen.setTimeLine(0, 240.0)  # TODO: this is just for initialisation purposes - should be documented in the docstring why and how to set that
