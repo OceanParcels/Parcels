e3298f7008f2615cbf4a6e40a57151be05f3b8a0
Christian Kehl <c.kehl@uu.nl>
2021-07-07 18:23:45 +0200

    integrate_nodes_to_master - added node-based particle set to particleset module

--- parcels/particleset/__init__.py
+++ parcels/particleset/__init__.py
@@ -1,6 +1,7 @@
 from .baseparticleset import BaseParticleSet  # noqa
 from .particlesetaos import ParticleSetAOS  # noqa
 from .particlesetsoa import ParticleSetSOA  # noqa
+from .particlesetnodes import ParticleSetNodes  # noqa
 
 # ParticleSet is an alias for ParticleSetSOA, i.e. the default
 # implementation for storing particles is the Structure of Arrays
