114a72840863b24e18a57d280f66b605decf3939
Christian Kehl <c.kehl@uu.nl>
2021-07-07 18:15:34 +0200

    integrate_nodes_to_master - added node-based particle file to particlefile module

--- parcels/particlefile/__init__.py
+++ parcels/particlefile/__init__.py
@@ -4,5 +4,6 @@ from .baseparticlefile import BaseParticleFile  # noqa: F401
 from .baseparticlefile import _set_calendar  # noqa: F401
 from .particlefileaos import ParticleFileAOS  # noqa: F401
 from .particlefilesoa import ParticleFileSOA  # noqa: F401
+from .particlefilenodes import ParticleFileNodes  # noqa: F401
 
 ParticleFile = ParticleFileSOA
