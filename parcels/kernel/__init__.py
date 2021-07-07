754fc9a22972a7c58c2be3d85da9663c38c42542
Christian Kehl <c.kehl@uu.nl>
2021-07-07 18:13:24 +0200

    integrate_nodes_to_master - added node-based kernel to kernel module

--- parcels/kernel/__init__.py
+++ parcels/kernel/__init__.py
@@ -1,5 +1,6 @@
 from .basekernel import BaseKernel  # noqa
 from .kernelaos import KernelAOS  # noqa
 from .kernelsoa import KernelSOA  # noqa
+from .kernelnodes import KernelNodes  # noqa
 
 Kernel = KernelSOA
