804a03aebaaebba46a58c482d5618166709e3a04
Erik van Sebille <e.vansebille@uu.nl>
2021-10-04 17:37:33 +0200

    Writing version also by scm_tools in case of parcels install through conda

--- parcels/_version.py
+++ parcels/_version.py
@@ -1,3 +1,6 @@
 import subprocess
 import os
-version = subprocess.check_output(['git', '-C', os.path.dirname(__file__), 'describe', '--tags']).decode('ascii').strip()
+try:
+    version = subprocess.check_output(['git', '-C', os.path.dirname(__file__), 'describe', '--tags']).decode('ascii').strip()
+except:
+    from parcels._version_setup import version as version  # noqa
