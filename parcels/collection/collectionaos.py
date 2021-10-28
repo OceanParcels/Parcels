integrate_nodes_to_master (#1034) - trying to fix the fieldset test by correcting the sizeOf(pset) calculation.

parcels/collection/collectionaos.py
@@ -1171,7 +1171,8 @@ class ParticleCollectionAOS(ParticleCollection):
         sizeof(self) = len(self) * sizeof(pclass)
         :returns size of this collection in bytes; initiated by calling sys.getsizeof(object)
         """
-        sz = self._data.nbytes if isinstance(self._data, np.ndarray) else sys.getsizeof(self._data)
+        sz = super(ParticleCollection, self).__sizeof__()
+        sz += self._data.nbytes if isinstance(self._data, np.ndarray) else sys.getsizeof(self._data)
         if self._data_c is not None:
             sz += self._data_c.nbytes if isinstance(self._data_c, np.ndarray) else sys.getsizeof(self._data_c)
         return sz

parcels/collection/collections.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from abc import ABC
 from abc import abstractmethod
@@ -868,7 +869,7 @@ class Collection(ABC):
 
         sizeof(self) = len(self) * sizeof(pclass)
         """
-        pass
+        return 0
 
     def empty(self):
         """
@@ -918,6 +919,21 @@ class ParticleCollection(Collection):
         ParticleCollection - Destructor
         """
         pass
+
+    def __sizeof__(self):
+        """
+        This function returns the size in actual bytes required in memory to hold the collection. Ideally and simply,
+        the size is computed as follows:
+
+        sizeof(self) = len(self) * sizeof(pclass)
+        :returns size of this collection in bytes; initiated by calling sys.getsizeof(object)
+        """
+        sz = sys.getsizeof(super(ParticleCollection, self))
+        sz += self._pu_indicators.nbytes if isinstance(self._pu_indicators, np.ndarray) else sys.getsizeof(self._pu_indicators)
+        sz += self._pu_centers.nbytes if isinstance(self._pu_centers, np.ndarray) else sys.getsizeof(self._pu_centers)
+        sz += sys.getsizeof(self._offset)
+        # skip data counting here
+        return sz
 
     @property
     def pu_indicators(self):