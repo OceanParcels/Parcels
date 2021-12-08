benchmark_integration (#1104) - fixed the logging of runtimes in all structures by explicitly using the BenchmarkKernel class derivatives.

parcels/particleset/benchmarkparticleset.py
@@ -164,22 +164,16 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
             if not isinstance(self.kernel, BaseBenchmarkKernel):
                 self.compute_log.start_timing()
             time = self._execute_kernel_(next_time, dt, recovery, output_file=output_file, execute_once=execute_once)
+            if not isinstance(self.kernel, BaseBenchmarkKernel):
+                self.compute_log.stop_timing()
+                # self.compute_log.accumulate_timing()
             if abs(time-next_prelease) < tol:
                 # creating new particles equals a memory-io operation
-                if not isinstance(self.kernel, BaseBenchmarkKernel):
-                    self.compute_log.stop_timing()
-                    self.compute_log.accumulate_timing()
-
                 self.mem_io_log.start_timing()
                 self._add_periodic_release_particles_(time, dt)
                 self.mem_io_log.stop_timing()
                 self.mem_io_log.accumulate_timing()
                 next_prelease += self.repeatdt * np.sign(dt)
-            else:
-                if not isinstance(self.kernel, BaseBenchmarkKernel):
-                    self.compute_log.stop_timing()
-                else:
-                    pass
             if isinstance(self.kernel, BaseBenchmarkKernel):
                 self.compute_log.add_aux_measure(self.kernel.compute_timings.sum())
                 self.kernel.compute_timings.reset()
@@ -273,7 +267,7 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
             io_times = np.array(io_times)
         if mem_io_times is not None:
             mem_io_times = np.array(mem_io_times)
-            io_times += mem_io_times
+            # io_times += mem_io_times
         if plot_times is None or type(plot_times) not in [list, dict, np.ndarray]:
             plot_times = self.plot_log.get_values()
         if not isinstance(plot_times, np.ndarray):
@@ -297,6 +291,7 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
         plot_t = (total_times * t_scaler).tolist()
         plot_ct = (compute_times * t_scaler).tolist()
         plot_iot = (io_times * t_scaler).tolist()
+        plot_miot = (mem_io_times * t_scaler).tolist()
         plot_drawt = (plot_times * t_scaler).tolist()
         plot_npart = (nparticles * npart_scaler).tolist()
         plot_mem = []
@@ -308,6 +303,7 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
             plot_mem_async = (memory_used_async * mem_scaler).tolist()
 
         do_iot_plot = True
+        do_miot_plot = True
         do_drawt_plot = False
         do_mem_plot = True
         do_mem_plot_async = True
@@ -316,6 +312,9 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
         if len(plot_t) != len(plot_iot):
             print("plot_t and plot_iot have different lengths ({} vs {})".format(len(plot_t), len(plot_iot)))
             do_iot_plot = False
+        if len(plot_t) != len(plot_miot):
+            print("plot_t and plot_miot have different lengths ({} vs {})".format(len(plot_t), len(plot_miot)))
+            do_miot_plot = False
         if len(plot_t) != len(plot_drawt):
             print("plot_t and plot_drawt have different lengths ({} vs {})".format(len(plot_t), len(plot_iot)))
             do_drawt_plot = False
@@ -331,7 +330,9 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
         ax.plot(x, plot_t, 's-', label="total time_spent [100ms]")
         ax.plot(x, plot_ct, 'o-', label="compute-time spent [100ms]")
         if do_iot_plot:
-            ax.plot(x, plot_iot, 'o-', label="io-time spent [100ms]")
+            ax.plot(x, plot_iot, 'o-', label="(ext.) io-time spent [100ms]")
+        if do_miot_plot:
+            ax.plot(x, plot_miot, 'D-', label="(mem.) io-time spent [100ms]")
         if do_drawt_plot:
             ax.plot(x, plot_drawt, 'o-', label="draw-time spent [100ms]")
         if (memory_used is not None) and do_mem_plot:
@@ -351,7 +352,8 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
 
         sys.stdout.write("cumulative total runtime: {}\n".format(total_times.sum()))
         sys.stdout.write("cumulative compute time: {}\n".format(compute_times.sum()))
-        sys.stdout.write("cumulative I/O time: {}\n".format(io_times.sum()))
+        sys.stdout.write("cumulative (ext.) I/O time: {}\n".format(io_times.sum()))
+        sys.stdout.write("cumulative (mem.) I/O time: {}\n".format(mem_io_times.sum()))
         sys.stdout.write("cumulative plot time: {}\n".format(plot_times.sum()))
 
         csv_file = os.path.splitext(imageFilePath)[0]+".csv"
@@ -365,10 +367,10 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
             if MPI:
                 mpi_comm = MPI.COMM_WORLD
                 ncores = mpi_comm.Get_size()
-            header_string = "target_N, start_N, final_N, avg_N, ncores, avg_kt_total[s], avg_kt_compute[s], avg_kt_io[s], avg_kt_plot[s], cum_t_total[s], cum_t_compute[s], com_t_io[s], cum_t_plot[s], max_mem[MB]\n"
+            header_string = "target_N, start_N, final_N, avg_N, ncores, avg_kt_total[s], avg_kt_compute[s], avg_kt_mem-io[s], avg_kt_io[s], avg_kt_plot[s], cum_t_total[s], cum_t_compute[s], cum_t_mem-io[s], com_t_io[s], cum_t_plot[s], max_mem[MB]\n"
             f.write(header_string)
             data_string = "{}, {}, {}, {}, {}, ".format(target_N, nparticles_t0, nparticles_tN, nparticles.mean(), ncores)
-            data_string += "{:2.10f}, {:2.10f}, {:2.10f}, {:2.10f}, ".format(total_times.mean(), compute_times.mean(), io_times.mean(), plot_times.mean())
+            data_string += "{:2.10f}, {:2.10f}, {:2.10f}, {:2.10f}, ".format(total_times.mean(), compute_times.mean(), mem_io_times.mean(), io_times.mean(), plot_times.mean())
             max_mem_sync = 0
             if memory_used is not None and len(memory_used) > 1:
                 memory_used = np.floor(memory_used / (1024*1024))
@@ -381,5 +383,5 @@ class BaseBenchmarkParticleSet(BaseParticleSet):
                     memory_used_async = memory_used_async.astype(dtype=np.int64)
                     max_mem_async = memory_used_async.max()
             max_mem = max(max_mem_sync, max_mem_async)
-            data_string += "{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {}".format(total_times.sum(), compute_times.sum(), io_times.sum(), plot_times.sum(), max_mem)
+            data_string += "{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {}".format(total_times.sum(), compute_times.sum(), mem_io_times.sum(), io_times.sum(), plot_times.sum(), max_mem)
             f.write(data_string)

performance/benchmark_perlin.py
@@ -25,6 +25,7 @@ import sys
 import gc
 import os
 import time as ostime
+import string
 # import matplotlib.pyplot as plt
 from parcels.tools import perlin3d
 from parcels.tools import perlin2d
@@ -226,12 +227,13 @@ if __name__=='__main__':
     parser.add_argument("-N", "--n_particles", dest="nparticles", type=str, default="2**6", help="number of particles to generate and advect (default: 2e6)")
     parser.add_argument("-sN", "--start_n_particles", dest="start_nparticles", type=str, default="96", help="(optional) number of particles generated per release cycle (if --rt is set) (default: 96)")
     parser.add_argument("-m", "--mode", dest="compute_mode", choices=['jit','scipy'], default="jit", help="computation mode = [JIT, SciPy]")
-    parser.add_argument("-tp", "--type", dest="pset_type", choices=pset_modes, default="soa", help="particle set type = [SOA, AOS, Nodes]")
+    parser.add_argument("-tp", "--type", dest="pset_type", default="soa", help="particle set type = [SOA, AOS, Nodes]")  # , choices=pset_modes
     parser.add_argument("-G", "--GC", dest="useGC", action='store_true', default=False, help="using a garbage collector (default: false)")
     parser.add_argument("--dry", dest="dryrun", action="store_true", default=False, help="Start dry run (no benchmarking and its classes")
     args = parser.parse_args()
 
-    pset_type = args.pset_type
+    pset_type = str(args.pset_type).lower()
+    assert pset_type in pset_types
     ParticleSet = pset_types[pset_type]['pset']
     if args.dryrun:
         ParticleSet = pset_types_dry[pset_type]['pset']
@@ -262,7 +264,7 @@ if __name__=='__main__':
     # ======================================================= #
     idgen = None
     c_lib_register = None
-    if pset_type != 'nodes':
+    if pset_type == 'nodes':
         idgen = GenerateID_Service(SequentialIdGenerator)
         idgen.setDepthLimits(0., 1.0)
         idgen.setTimeLine(0, delta(days=time_in_days).total_seconds())
@@ -324,7 +326,7 @@ if __name__=='__main__':
             field_fpath = os.path.join(odir,"perlin")
         fieldset = perlin_fieldset_from_numpy(periodic_wrap=periodicFlag, write_out=field_fpath)
 
-    if args.compute_mode is 'scipy':
+    if args.compute_mode == 'scipy':
         Nparticle = 2**10
 
     if MPI:
@@ -362,28 +364,30 @@ if __name__=='__main__':
         if agingParticles:
             if repeatdtFlag:
                 pset = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(start_N_particles, 1) * a, lat=np.random.rand(start_N_particles, 1) * b, time=simStart, repeatdt=delta(minutes=repeatRateMinutes), idgen=idgen, c_lib_register=c_lib_register)
-                if pset_type is not 'nodes':
+                if pset_type != 'nodes':
                     psetA = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(int(addParticleN), 1) * a, lat=np.random.rand(int(addParticleN), 1) * b, time=simStart)
                     pset.add(psetA)
                 else:
                     lonlat_field = np.random.rand(int(addParticleN), 2)
                     lonlat_field *= np.array([a, b])
                     time_field = np.ones((int(addParticleN), 1), dtype=np.float64) * simStart
-                    pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    # pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    pdata = {'lon': lonlat_field[0], 'lat': lonlat_field[1], 'time': time_field}
                     pset.add(pdata)
             else:
                 pset = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(Nparticle, 1) * a, lat=np.random.rand(Nparticle, 1) * b, time=simStart, idgen=idgen, c_lib_register=c_lib_register)
         else:
             if repeatdtFlag:
                 pset = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(start_N_particles, 1) * a, lat=np.random.rand(start_N_particles, 1) * b, time=simStart, repeatdt=delta(minutes=repeatRateMinutes), idgen=idgen, c_lib_register=c_lib_register)
-                if pset_type is not 'nodes':
+                if pset_type != 'nodes':
                     psetA = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(int(addParticleN), 1) * a, lat=np.random.rand(int(addParticleN), 1) * b, time=simStart)
                     pset.add(psetA)
                 else:
                     lonlat_field = np.random.rand(int(addParticleN), 2)
                     lonlat_field *= np.array([a, b])
                     time_field = np.ones((int(addParticleN), 1), dtype=np.float64) * simStart
-                    pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    # pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    pdata = {'lon': lonlat_field[0], 'lat': lonlat_field[1], 'time': time_field}
                     pset.add(pdata)
             else:
                 pset = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(Nparticle, 1) * a, lat=np.random.rand(Nparticle, 1) * b, time=simStart, idgen=idgen, c_lib_register=c_lib_register)
@@ -392,28 +396,30 @@ if __name__=='__main__':
         if agingParticles:
             if repeatdtFlag:
                 pset = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(start_N_particles, 1) * a, lat=np.random.rand(start_N_particles, 1) * b, time=simStart, repeatdt=delta(minutes=repeatRateMinutes), idgen=idgen, c_lib_register=c_lib_register)
-                if pset_type is not 'nodes':
+                if pset_type != 'nodes':
                     psetA = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(int(addParticleN), 1) * a, lat=np.random.rand(int(addParticleN), 1) * b, time=simStart)
                     pset.add(psetA)
                 else:
                     lonlat_field = np.random.rand(int(addParticleN), 2)
                     lonlat_field *= np.array([a, b])
                     time_field = np.ones((int(addParticleN), 1), dtype=np.float64) * simStart
-                    pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    # pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    pdata = {'lon': lonlat_field[0], 'lat': lonlat_field[1], 'time': time_field}
                     pset.add(pdata)
             else:
                 pset = ParticleSet(fieldset=fieldset, pclass=age_ptype[(args.compute_mode).lower()], lon=np.random.rand(Nparticle, 1) * a, lat=np.random.rand(Nparticle, 1) * b, time=simStart, idgen=idgen, c_lib_register=c_lib_register)
         else:
             if repeatdtFlag:
                 pset = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(start_N_particles, 1) * a, lat=np.random.rand(start_N_particles, 1) * b, time=simStart, repeatdt=delta(minutes=repeatRateMinutes), idgen=idgen, c_lib_register=c_lib_register)
-                if pset_type is not 'nodes':
+                if pset_type != 'nodes':
                     psetA = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(int(addParticleN), 1) * a, lat=np.random.rand(int(addParticleN), 1) * b, time=simStart)
                     pset.add(psetA)
                 else:
                     lonlat_field = np.random.rand(int(addParticleN), 2)
                     lonlat_field *= np.array([a, b])
                     time_field = np.ones((int(addParticleN), 1), dtype=np.float64) * simStart
-                    pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    # pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
+                    pdata = {'lon': lonlat_field[0], 'lat': lonlat_field[1], 'time': time_field}
                     pset.add(pdata)
             else:
                 pset = ParticleSet(fieldset=fieldset, pclass=ptype[(args.compute_mode).lower()], lon=np.random.rand(Nparticle, 1) * a, lat=np.random.rand(Nparticle, 1) * b, time=simStart, idgen=idgen, c_lib_register=c_lib_register)