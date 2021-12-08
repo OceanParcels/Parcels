5ef891139d1cab216481280f008b8a28488a5972
Christian Kehl <c.kehl@uu.nl>
2021-12-08 14:21:06 +0100

    integrate_nodes_to_master (#1034) - removing comments in parcels/tools/__init__.py.

--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -6,8 +6,3 @@ from .id_generators import *  # noga
 from .loggers import *  # noqa
 from .timer import *  # noqa
 from .id_generators import *  # noqa
-#
-# idgen = GenerateID_Service(SpatioTemporalIdGenerator)
-# idgen = GenerateID_Service(SequentialIdGenerator)
-# idgen.setDepthLimits(0, 100.0)  # TODO: to be changed to some acceptable gobal max, e.g. Marianna Trench depth in [m]
-# idgen.setTimeLine(0, 240.0)  # TODO: this is just for initialisation purposes - should be documented in the docstring why and how to set that
