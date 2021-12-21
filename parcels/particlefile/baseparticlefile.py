6bbb979d75f910823b97d341a948bf12839580e6
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2021-12-21 15:39:37 +0000

    Cleaned up comments

--- parcels/particlefile/baseparticlefile.py
+++ parcels/particlefile/baseparticlefile.py
@@ -101,11 +101,9 @@ class BaseParticleFile(ABC):
                 if v.to_write == 'once' and self.write_ondelete == False:
                     self.var_names_once += [v.name]
                     self.var_dtypes_once += [v.dtype.__name__]
-                    # self.var_dtypes_once += [v.dtype.__name__[0] + str(int(int(v.dtype.__name__[-2:])/8))]
                 elif v.to_write is True or v.to_write == 'once':
                     self.var_names += [v.name]
                     self.var_dtypes += [v.dtype.__name__]
-                    # self.var_dtypes += [v.dtype.__name__[0] + str(int(int(v.dtype.__name__[-2:])/8))]
             if len(self.var_names_once) > 0:
                 self.written_once = []
                 self.file_list_once = []
