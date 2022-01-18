15d642916689e8bcb7433b4ee8a4a9bce8204f65
Christian Kehl <c.kehl@uu.nl>
2022-01-18 23:58:52 +0100

    lorenz_caching (#1126) - added caching to the default toolset.

--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -1,4 +1,5 @@
 from .converters import *  # noqa
+from .filecache import *  # noqa
 from .global_statics import *  # noqa
 from .statuscodes import *  # noqa
 from .interpolation_utils import *  # noqa
