76087e4fef7d9ea7aef9663fac914fbaa4427647
Christian Kehl <c.kehl@uu.nl>
2022-02-22 13:03:47 +0100

    lorenz_caching (#1126) - expanding information when FieldSet::parse_wildcards(...) fails.

--- parcels/fieldset.py
+++ parcels/fieldset.py
@@ -301,7 +301,7 @@ class FieldSet(object):
             raise IOError("FieldSet files not found for variable %s: %s" % (var, str(notfound_paths)))
         for fp in paths:
             if not path.exists(fp):
-                raise IOError("FieldSet file not found: %s" % str(fp))
+                raise IOError("FieldSet file not found for variable %s: %s" % (var, str(fp)))
         return paths
 
     @classmethod
