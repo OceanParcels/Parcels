348dc02d32e2ac2c05c5026019b55d512f00d97f
Christian Kehl <c.kehl@uu.nl>
2022-02-28 22:53:00 +0100

    benchmark_integration (#1104) - trying to patch-fix an issue with periodic time bounds. Error is emitted whenever the the 'time' parameter of grids is longer than the period - but may be shorter than the total runtime. This now now changed to clip the 'time' field to the period time.

--- parcels/field.py
+++ parcels/field.py
@@ -150,21 +150,20 @@ class Field(object):
         if self.time_periodic is True:
             raise ValueError("Unsupported time_periodic=True. time_periodic must now be either False or the length of the period (either float in seconds or datetime.timedelta object.")
         if self.time_periodic is not False:
+            logger.info("time for {}: {} to {}".format(self.name, self.grid.time[0], self.grid.time[-1]))
             if isinstance(self.time_periodic, datetime.timedelta):
                 self.time_periodic = self.time_periodic.total_seconds()
             if not np.isclose(self.grid.time[-1] - self.grid.time[0], self.time_periodic):
-                if (self.grid.time[-1] - self.grid.time[0]) > self.time_periodic:
-                    # tshift = 1.0
-                    # tishift = 0
+                if self.grid.time[-1] - self.grid.time[0] > self.time_periodic:
                     grid_dt = self.grid.time[1] - self.grid.time[0]
                     tshift = abs(math.ceil(float(self.time_periodic) / float(grid_dt)) * float(grid_dt))
-                    tishift = int(tshift / abs(grid_dt))
-                    self.grid.time = self.grid.time[0:tishift]
-                    self.grid._add_last_periodic_data_timestep = True
+                    tishift = int(math.ceil(tshift / abs(grid_dt)))
+                    self.grid.time = self.grid.time[0:tishift+1]
                     # raise ValueError("Time series provided is longer than the time_periodic parameter")
                 else:
-                    self.grid._add_last_periodic_data_timestep = True
                     self.grid.time = np.append(self.grid.time, self.grid.time[0] + self.time_periodic)
+                logger.info("clipped time: {} to {}".format(self.grid.time[0], self.grid.time[-1]))
+                self.grid._add_last_periodic_data_timestep = True
                 self.grid.time_full = self.grid.time
 
         self.vmin = vmin
