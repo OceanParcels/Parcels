3b4952d2d41a730d9d4185a72d93714fd3747986
Christian Kehl <c.kehl@uu.nl>
2022-03-14 20:59:41 +0100

    lorenz_caching (#1126) - found the error: if I am cutting out a specific variable, I shall not check that the filesize of in and out remains the same ...

--- parcels/tools/filecache.py
+++ parcels/tools/filecache.py
@@ -375,9 +375,8 @@ class FieldFileCache(object):
         # -4 -> output in NetCDF 4 format
         # --fix_rec_dmn=all -> remove the 'unlimited' dimension from the data to focus just on 1 file
         cmd = "ncks -4 --fix_rec_dmn=all -q -v {} {} {}".format(var_string, src_filepath, dst_filepath)
-        # if DEBUG:
-        #     logger.info("copy file via command: '{}'".format(cmd))
-        logger.info("copy file via command: '{}'".format(cmd))
+        if DEBUG:
+            logger.info("copy file via command: '{}'".format(cmd))
         if os.system(cmd) != 0:
             raise OSError("Failure executing NetCDF kitchen sink '{}'.")
 
@@ -929,10 +928,12 @@ class FieldFileCache(object):
                     # copy2(self._original_filepaths[name][i], self._global_files[name][i], follow_symlinks=True)
                     # copy(self._original_filepaths[name][i], self._global_files[name][i], follow_symlinks=True)
                     self.nc_copy(self._original_filepaths[name][i], self._global_files[name][i])
+                    checksize = False
                     if not os.path.exists(self._global_files[name][i]):
                         copy2(self._original_filepaths[name][i], self._global_files[name][i], follow_symlinks=True)
+                        checksize = True
                     assert os.path.exists(self._global_files[name][i])
-                    while os.path.getsize(self._global_files[name][i]) != os.path.getsize(self._original_filepaths[name][i]):
+                    while checksize and (os.path.getsize(self._global_files[name][i]) != os.path.getsize(self._original_filepaths[name][i])):
                         sleeptime = uniform(0.05, 0.12)
                         sleep(sleeptime)
                     if DEBUG:
