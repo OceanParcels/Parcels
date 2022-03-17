0e19c58322c71ad2f5e84560188de7d89d17d3a3
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2022-03-17 18:31:44 +0000

    Also added unsigned 8-bit ints

--- parcels/particlefile/particlefilesoa.py
+++ parcels/particlefile/particlefilesoa.py
@@ -115,12 +115,14 @@ class ParticleFileSOA(BaseParticleFile):
                 return  # export only on threat 0
 
         # Create dictionary to translate datatypes and fill_values
-        self.fmt_map = {np.float32: 'f4', np.float64: 'f8',
-                        np.bool_: 'i1', np.int16: 'i2', np.int32: 'i4', np.int64: 'i8',
+        self.fmt_map = {np.float16: 'f2', np.float32: 'f4', np.float64: 'f8',
+                        np.bool_: 'i1', np.int8: 'i1', np.int16: 'i2',
+                        np.int32: 'i4', np.int64: 'i8', np.uint8: 'u1',
                         np.uint16: 'u2', np.uint32: 'u4', np.uint64: 'u8'}
-        self.fill_value_map = {np.float32: np.nan, np.float64: np.nan,
-                               np.bool_: np.iinfo(np.int8).max, np.int16: np.iinfo(np.int16).max,
-                               np.int32: np.iinfo(np.int32).max, np.int64: np.iinfo(np.int64).max,
+        self.fill_value_map = {np.float16: np.nan, np.float32: np.nan, np.float64: np.nan,
+                               np.bool_: np.iinfo(np.int8).max, np.int8: np.iinfo(np.int8).max,
+                               np.int16: np.iinfo(np.int16).max, np.int32: np.iinfo(np.int32).max,
+                               np.int64: np.iinfo(np.int64).max, np.uint8: np.iinfo(np.uint8).max,
                                np.uint16: np.iinfo(np.uint16).max, np.uint32: np.iinfo(np.uint32).max,
                                np.uint64: np.iinfo(np.uint64).max}
 
