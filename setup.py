e39c587c21d3224e9c3340d8074421e5a224a012
Guillaume Maze <gmaze@ifremer.fr>
2022-04-29 14:58:42 +0200

    more on description

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='parcels',
       description="""Framework for Lagrangian tracking of virtual ocean particles in the petascale age.""",
       author="oceanparcels.org team",
       # use_scm_version={'write_to': 'parcels/_version_setup.py'},  # Once PR merged
-      version='0.0.5',  # dev
+      version='0.0.6',  # dev
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       python_requires='>=3.8',
       packages=find_packages(),
