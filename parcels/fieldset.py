2e546723ac0e72bd6230cbefaa4078d9d162bcf7
Christian Kehl <6020531+CKehl@users.noreply.github.com>
2022-09-27 16:02:01 +0200

    Update parcels/fieldset.py

--- parcels/fieldset.py
+++ parcels/fieldset.py
@@ -522,7 +522,7 @@ class FieldSet(object):
         :param chunksize: size of the chunks in dask loading. Default is None (no chunking)
         :param cache: a custom FieldFileCache object, or None for automatic construction (default: None)
         :param cache_dir: auto-constructing a FieldFileCache object with this given top-leave path as cache directory (default: None)
-        :param do_cache: switch to enable or disable caching (default: True [= enabled])
+        :param do_cache: switch to enable or disable caching (default: False [= disabled])
         :param use_threads: switch to enable or disable threaded operation of caching (default: False [= disabled])
         """
         if 'creation_log' not in kwargs.keys():
