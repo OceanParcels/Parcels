415d7e78cbce174a4f67ce458a166713f5757b2c
Erik van Sebille <erikvansebille@gmail.com>
2023-02-03 22:09:00 +0100

    Adding unittest for 2D linear_invdist_land interpolation
    And also fixing a small bug in field.py!

--- tests/test_fieldset_sampling.py
+++ tests/test_fieldset_sampling.py
@@ -250,14 +250,20 @@ def test_nearest_neighbour_interpolation3D(pset_mode, mode, k_sample_p, npart=81
 
 @pytest.mark.parametrize('pset_mode', pset_modes)
 @pytest.mark.parametrize('mode', ['scipy', 'jit'])
+@pytest.mark.parametrize('withDepth', [True, False])
 @pytest.mark.parametrize('arrtype', ['ones', 'rand'])
-def test_inversedistance_nearland(pset_mode, mode, arrtype, k_sample_p, npart=81):
-    dims = (4, 4, 6)
-    P = np.random.rand(dims[0], dims[1], dims[2])+2 if arrtype == 'rand' else np.ones(dims, dtype=np.float32)
-    P[1, 1:2, 1:6] = np.nan  # setting some values to land (NaN)
-    dimensions = {'lon': np.linspace(0., 1., dims[2], dtype=np.float32),
-                  'lat': np.linspace(0., 1., dims[1], dtype=np.float32),
-                  'depth': np.linspace(0., 1., dims[0], dtype=np.float32)}
+def test_inversedistance_nearland(pset_mode, mode, withDepth, arrtype, k_sample_p, npart=81):
+    dims = (4, 4, 6) if withDepth else (4, 6)
+    dimensions = {'lon': np.linspace(0., 1., dims[-1], dtype=np.float32),
+                  'lat': np.linspace(0., 1., dims[-2], dtype=np.float32)}
+    if withDepth:
+        dimensions['depth'] = np.linspace(0., 1., dims[0], dtype=np.float32)
+        P = np.random.rand(dims[0], dims[1], dims[2])+2 if arrtype == 'rand' else np.ones(dims, dtype=np.float32)
+        P[1, 1:2, 1:6] = np.nan  # setting some values to land (NaN)
+    else:
+        P = np.random.rand(dims[0], dims[1])+2 if arrtype == 'rand' else np.ones(dims, dtype=np.float32)
+        P[1:2, 1:6] = np.nan  # setting some values to land (NaN)
+
     data = {'U': np.zeros(dims, dtype=np.float32),
             'V': np.zeros(dims, dtype=np.float32),
             'P': P}
@@ -265,12 +271,12 @@ def test_inversedistance_nearland(pset_mode, mode, arrtype, k_sample_p, npart=81
     fieldset.P.interp_method = 'linear_invdist_land_tracer'
 
     xv, yv = np.meshgrid(np.linspace(0.1, 0.9, int(np.sqrt(npart))), np.linspace(0.1, 0.9, int(np.sqrt(npart))))
-    # combine a pset at 0m with pset at 1m, as meshgrid does not do 3D
     pset = pset_type[pset_mode]['pset'](fieldset, pclass=pclass(mode), lon=xv.flatten(), lat=yv.flatten(),
                                         depth=np.zeros(npart))
-    pset2 = pset_type[pset_mode]['pset'](fieldset, pclass=pclass(mode), lon=xv.flatten(), lat=yv.flatten(),
-                                         depth=np.ones(npart))
-    pset.add(pset2)
+    if withDepth:
+        pset2 = pset_type[pset_mode]['pset'](fieldset, pclass=pclass(mode), lon=xv.flatten(), lat=yv.flatten(),
+                                             depth=np.ones(npart))
+        pset.add(pset2)
     pset.execute(k_sample_p, endtime=1, dt=1)
     if arrtype == 'rand':
         assert np.all((pset.p > 2) & (pset.p < 3))
