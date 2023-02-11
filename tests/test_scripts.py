d85e6599e3b6c46ee797d14473c1ccf573cb85e4
Erik van Sebille <erikvansebille@gmail.com>
2023-02-11 16:23:54 +0100

    Adding further tests for plotting

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,11 +1,9 @@
 from parcels import (FieldSet, ParticleSet, JITParticle, AdvectionRK4, plotTrajectoriesFile)
 from parcels.scripts.plottrajectoriesfile import main
-import pytest
 from os import path
 
 
-@pytest.mark.parametrize('from_main', [True, False])
-def test_plotting(from_main, tmpdir):
+def test_plotting(tmpdir):
     datafile = path.join(path.dirname(__file__), 'test_data', 'testfields')
 
     fieldset = FieldSet.from_parcels(datafile, allow_time_extrapolation=True)
@@ -16,9 +14,9 @@ def test_plotting(from_main, tmpdir):
 
     pset.execute(AdvectionRK4, runtime=10, dt=1, output_file=output_file)
 
+    for fld in [fieldset.U, 'vector', None]:
+        pset.show(field=fld)
+
     for mode in ['2d', '3d', 'movie2d', 'hist2d']:
-        if from_main:
-            pset.show(field=fieldset.U)
-            main([mode, f'-p{fp}'])
-        else:
-            plotTrajectoriesFile(fp, tracerfield=fieldset.U, mode=mode, show_plt=False)
+        main([mode, f'-p{fp}'])
+        plotTrajectoriesFile(fp, tracerfield=fieldset.U, mode=mode, show_plt=False)
