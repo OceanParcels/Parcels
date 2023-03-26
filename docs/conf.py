a40d584882d4efdf3f1bc737501f91e0cd7dc5bf
Vecko <VeckoTheGecko@users.noreply.github.com>
2023-03-26 11:43:03 +0800

    Preliminart numpydoc sphinx support
    Only adding napolean for now to visualise docstrings. Will add numpydocs to validate later one the docstrings are in a better state.

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     "sphinx.ext.linkcode",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
