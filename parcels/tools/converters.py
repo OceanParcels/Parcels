4030ad8bd8cd25a91569813f910e99739c08d30c
Vecko <VeckoTheGecko@users.noreply.github.com>
2023-06-02 22:19:34 +0800

    Update remaining nbviewer to docs links
    Reran `tutorial_timestamps.ipynb` to get new error message

--- parcels/tools/converters.py
+++ parcels/tools/converters.py
@@ -267,6 +267,6 @@ def convert_xarray_time_units(ds, time):
     except ValueError:
         raise RuntimeError('Xarray could not convert the calendar. If you''re using from_netcdf, '
                            'try using the timestamps keyword in the construction of your Field. '
-                           'See also the tutorial at https://nbviewer.jupyter.org/github/OceanParcels/'
-                           'parcels/blob/master/docs/examples/tutorial_timestamps.ipynb')
+                           'See also the tutorial at https://docs.oceanparcels.org/en/latest/'
+                           'examples/tutorial_timestamps.html')
     ds[time] = da2[time]
