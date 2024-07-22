035159a1b4002bc1550cebd275f93c0adaa8513d
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-07-22 11:07:27 +0200

    Documentation updates and numpy randint bugfix (#1604)
    - Update installation instructions to remove 'setting solver' section (Libmamba solver is no longer experimental and is default behaviour https://www.anaconda.com/blog/conda-is-fast-now)
    - Update documentatation title (fixes #1602)
    - numpy randint bugfix in MPI_default (fixes #1594)
    - Update `.gitignore` to add Mac specific files
    - Update integration tests to always run
    - Readthedocs compatability changes (as mentioned via email, and [via their blog post](https://about.readthedocs.com/blog/2024/07/addons-by-default/))

--- parcels/particledata.py
+++ parcels/particledata.py
@@ -37,7 +37,7 @@ def partitionParticlesMPI_default(coords, mpi_size=1):
     else:  # assigning random labels if no KMeans (see https://github.com/OceanParcels/parcels/issues/1261)
         logger.warning_once('sklearn needs to be available if MPI is installed. '
                             'See https://docs.oceanparcels.org/en/latest/installation.html#installation-for-developers for more information')
-        mpiProcs = np.randint(0, mpi_size, size=coords.shape[0])
+        mpiProcs = np.random.randint(0, mpi_size, size=coords.shape[0])
 
     return mpiProcs
 
