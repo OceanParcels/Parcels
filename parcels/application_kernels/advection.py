acba3fd62fd1567467d940c16a5c054a116b7f54
Erik van Sebille <erikvansebille@gmail.com>
2024-07-23 11:52:58 +0200

    Adding particle.ti as function input

--- parcels/application_kernels/advection.py
+++ parcels/application_kernels/advection.py
@@ -281,7 +281,7 @@ def AdvectionAnalytical_2D_JIT(particle, fieldset, time):
     when using AdvectionAnalytical in JIT mode.
     """
     calcAdvectionAnalytical_2D_JIT('parcels_customed_Cfunc_pointer_args', fieldset.U, fieldset.V, # noqa
-                                   particle.xi[0], particle.yi[0], particle.zi[0],  # TODO call with particle.xi without subscript
+                                   particle.xi[0], particle.yi[0], particle.zi[0], particle.ti[0],  # TODO call with particle.xi without subscript
                                    particle.lon, particle.lat, particle.depth, time, particle.dt,
                                    particle_dlon, particle_dlat)  # noqa
 
