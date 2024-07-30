f4fa1e9c75562794f795e4103aa3205099614350
Erik van Sebille <erikvansebille@gmail.com>
2024-07-30 13:48:32 +0200

    Skip Kernel execution when no particles need to be executed

--- parcels/particleset.py
+++ parcels/particleset.py
@@ -944,6 +944,11 @@ class ParticleSet(ABC):
 
         tol = 1e-12
         while (time < endtime and dt > 0) or (time > endtime and dt < 0) or dt == 0:
+            # Check if we can fast-forward to the next time needed for the particles
+            if dt > 0:
+                skip_kernel = True if min(self.time) > (time + dt) else False
+            else:
+                skip_kernel = True if max(self.time) < (time + dt) else False
             time_at_startofloop = time
 
             if dt > 0:
@@ -953,9 +958,10 @@ class ParticleSet(ABC):
 
             # If we don't perform interaction, only execute the normal kernel efficiently.
             if self.interaction_kernel is None:
-                res = self.kernel.execute(self, endtime=next_time, dt=dt)
-                if res == StatusCode.StopAllExecution:
-                    return StatusCode.StopAllExecution
+                if not skip_kernel:
+                    res = self.kernel.execute(self, endtime=next_time, dt=dt)
+                    if res == StatusCode.StopAllExecution:
+                        return StatusCode.StopAllExecution
             # Interaction: interleave the interaction and non-interaction kernel for each time step.
             # E.g. Normal -> Inter -> Normal -> Inter if endtime-time == 2*dt
             else:
