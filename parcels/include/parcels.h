ab2e876f9a3f7f89364c45a4e08cba461019eb00
Erik van Sebille <erikvansebille@gmail.com>
2024-08-07 13:12:31 +0200

    Cleaning up spaces in parcels.h

--- parcels/include/parcels.h
+++ parcels/include/parcels.h
@@ -666,7 +666,7 @@ static inline StatusCode temporal_interpolationUV_c_grid(type_coord x, type_coor
     status = search_indices(x, y, z, grid, &xi[igrid], &yi[igrid], &zi[igrid], &xsi, &eta, &zeta, gtype, ti[igrid], time, t0, t1, CGRID_VELOCITY, gridindexingtype); CHECKSTATUS(status);
     if (grid->zdim==1){
       float data2D_U[2][2][2], data2D_V[2][2][2];
-      if (gridindexingtype == NEMO){
+      if (gridindexingtype == NEMO) {
         status = getCell2D(U, xi[igrid], yi[igrid], ti[igrid], data2D_U, 0); CHECKSTATUS(status);
         status = getCell2D(V, xi[igrid], yi[igrid], ti[igrid], data2D_V, 0); CHECKSTATUS(status);
       }
@@ -679,7 +679,7 @@ static inline StatusCode temporal_interpolationUV_c_grid(type_coord x, type_coor
 
     } else {
       float data3D_U[2][2][2][2], data3D_V[2][2][2][2];
-      if (gridindexingtype == NEMO){
+      if (gridindexingtype == NEMO) {
         status = getCell3D(U, xi[igrid], yi[igrid], zi[igrid], ti[igrid], data3D_U, 0); CHECKSTATUS(status);
         status = getCell3D(V, xi[igrid], yi[igrid], zi[igrid], ti[igrid], data3D_V, 0); CHECKSTATUS(status);
       }
@@ -698,11 +698,11 @@ static inline StatusCode temporal_interpolationUV_c_grid(type_coord x, type_coor
     status = search_indices(x, y, z, grid, &xi[igrid], &yi[igrid], &zi[igrid], &xsi, &eta, &zeta, gtype, ti[igrid], t0, t0, t0+1, CGRID_VELOCITY, gridindexingtype); CHECKSTATUS(status);
     if (grid->zdim==1){
       float data2D_U[2][2][2], data2D_V[2][2][2];
-      if (gridindexingtype == NEMO){
+      if (gridindexingtype == NEMO) {
         status = getCell2D(U, xi[igrid], yi[igrid], ti[igrid], data2D_U, 1); CHECKSTATUS(status);
         status = getCell2D(V, xi[igrid], yi[igrid], ti[igrid], data2D_V, 1); CHECKSTATUS(status);
       }
-      else if ((gridindexingtype == MITGCM) || (gridindexingtype == CROCO)){
+      else if ((gridindexingtype == MITGCM) || (gridindexingtype == CROCO)) {
         status = getCell2D(U, xi[igrid], yi[igrid]-1, ti[igrid], data2D_U, 1); CHECKSTATUS(status);
         status = getCell2D(V, xi[igrid]-1, yi[igrid], ti[igrid], data2D_V, 1); CHECKSTATUS(status);
       }
@@ -710,11 +710,11 @@ static inline StatusCode temporal_interpolationUV_c_grid(type_coord x, type_coor
     }
     else{
       float data3D_U[2][2][2][2], data3D_V[2][2][2][2];
-      if (gridindexingtype == NEMO){
+      if (gridindexingtype == NEMO) {
         status = getCell3D(U, xi[igrid], yi[igrid], zi[igrid], ti[igrid], data3D_U, 1); CHECKSTATUS(status);
         status = getCell3D(V, xi[igrid], yi[igrid], zi[igrid], ti[igrid], data3D_V, 1); CHECKSTATUS(status);
       }
-      else if ((gridindexingtype == MITGCM) || (gridindexingtype == CROCO)){
+      else if ((gridindexingtype == MITGCM) || (gridindexingtype == CROCO)) {
         status = getCell3D(U, xi[igrid], yi[igrid]-1, zi[igrid], ti[igrid], data3D_U, 1); CHECKSTATUS(status);
         status = getCell3D(V, xi[igrid]-1, yi[igrid], zi[igrid], ti[igrid], data3D_V, 1); CHECKSTATUS(status);
       }
