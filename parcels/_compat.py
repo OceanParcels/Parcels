5f85aa71ec7c6ddc68b846bb3f0331e6a6ebd3f0
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-08-27 14:19:09 +0200

    Remove GHA keys (not supported by miniconda, only micromamba) and typing

--- parcels/_compat.py
+++ parcels/_compat.py
@@ -2,13 +2,18 @@
 
 __all__ = ["MPI", "KMeans"]
 
+from typing import Any
+
+MPI: Any | None = None
+KMeans: Any | None = None
+
 try:
-    from mpi4py import MPI
+    from mpi4py import MPI  # type: ignore[no-redef]
 except ModuleNotFoundError:
-    MPI = None
+    pass
 
 # KMeans is used in MPI. sklearn not installed by default
 try:
-    from sklearn.cluster import KMeans
+    from sklearn.cluster import KMeans  # type: ignore[no-redef]
 except ModuleNotFoundError:
-    KMeans = None
+    pass
