2087da482b743f0934e50e5eeb2b64ed1c809650
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-09-23 16:38:04 +0200

    add tracking issue

--- parcels/tools/_helpers.py
+++ parcels/tools/_helpers.py
@@ -49,5 +49,5 @@ def deprecated_made_private(func: Callable) -> Callable:
     return deprecated(
         "It has moved to the internal API as it is not expected to be directly used by "
         "the end-user. If you feel that you use this code directly in your scripts, please "
-        "comment on our tracking issue at <>.",  # TODO: Add tracking issue
+        "comment on our tracking issue at https://github.com/OceanParcels/Parcels/issues/1695.",
     )(func)
