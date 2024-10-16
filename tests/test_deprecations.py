f03f80e778e03161f0adb16afa5fc6001549bf60
Vecko <VeckoTheGecko@users.noreply.github.com>
2024-10-16 10:13:27 +0200

    Rename ParticleFile._analytical to ._is_analytical

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -256,7 +256,7 @@ actions = [
     Action("ParticleFile",     "pids_written",                   "make_private"  ),
     Action("ParticleFile",     "mpi_rank",                       "make_private"  ),
     Action("ParticleFile",     "fill_value_map",                 "make_private"  ),
-    Action("ParticleFile",     "analytical",                     "make_private"  ),
+    Action("ParticleFile",     "analytical",                     "make_private"  , skip_reason = "Was also renamed"),
     Action("Grid",             "lon_grid_to_target()",           "remove"        ),
     Action("Grid",             "lon_remapping",                  "remove"        ),
     Action("Grid",             "lon_grid_to_source()",           "remove"        ),
