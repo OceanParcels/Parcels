839209bab2f5e2299775e9fb4f298a5d478828da
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-11-27 00:56:57 +0800

    Cleanup getuid call

--- parcels/tools/global_statics.py
+++ parcels/tools/global_statics.py
@@ -6,10 +6,11 @@ from tempfile import gettempdir
 
 try:
     from os import getuid
+
+    USER_ID = getuid()
 except:
-    # Windows does not have getuid(), so define to simply return 'tmp'
-    def getuid():  # type: ignore
-        return "tmp"
+    # Windows does not have getuid()
+    USER_ID = "tmp"
 
 
 __all__ = ["cleanup_remove_files", "cleanup_unload_lib", "get_package_dir", "get_cache_dir"]
@@ -34,6 +35,6 @@ def get_package_dir():
 
 
 def get_cache_dir():
-    directory = os.path.join(gettempdir(), f"parcels-{getuid()}")
+    directory = os.path.join(gettempdir(), f"parcels-{USER_ID}")
     Path(directory).mkdir(exist_ok=True)
     return directory
