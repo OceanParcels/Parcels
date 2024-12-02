5d8fd32da38ba21e3a5a9c5dfb6fee9ba908ac03
Erik van Sebille <erikvansebille@gmail.com>
2024-12-02 08:21:09 +0100

    First attempt to add nonlinear depth-to-sigma conversion in C

--- parcels/compilation/codegenerator.py
+++ parcels/compilation/codegenerator.py
@@ -425,10 +425,13 @@ class KernelGenerator(ast.NodeVisitor):
         self.fieldset = fieldset
         self.ptype = ptype
         self.field_args = collections.OrderedDict()
-        if isinstance(fieldset.U, Field) and fieldset.U.gridindexingtype == "croco" and hasattr(fieldset, "H"):
-            self.field_args["H"] = fieldset.H  # CROCO requires H field
         self.vector_field_args = collections.OrderedDict()
         self.const_args = collections.OrderedDict()
+        if isinstance(fieldset.U, Field) and fieldset.U.gridindexingtype == "croco" and hasattr(fieldset, "H"):
+            self.field_args["H"] = fieldset.H  # CROCO requires H field
+            self.field_args["Zeta"] = fieldset.Zeta  # CROCO requires Zeta field
+            self.field_args["Cs_w"] = fieldset.Cs_w  # CROCO requires CS_w field
+            self.const_args["hc"] = fieldset.hc  # CROCO requires hc constant
 
     def generate(self, py_ast, funcvars: list[str]):
         # Replace occurrences of intrinsic objects in Python AST
@@ -825,16 +828,56 @@ class KernelGenerator(ast.NodeVisitor):
         self.visit(node.field)
         self.visit(node.args)
         args = self._check_FieldSamplingArguments(node.args.ccode)
-        statements_croco = []
-        if "croco" in node.field.obj.gridindexingtype and node.field.obj.name != "H":
-            statements_croco.append(
+        if "croco" in node.field.obj.gridindexingtype and node.field.obj.name != "H" and node.field.obj.name != "Zeta":
+            # Get Cs_w values directly from fieldset (since they are 1D in vertical only)
+            Cs_w = [self.fieldset.Cs_w.data[0][zi][0][0] for zi in range(self.fieldset.Cs_w.data.shape[1])]
+            statements_croco = [
+                c.Value("float", "local_h"),
+                c.Value("float", "local_zeta"),
                 c.Assign(
                     "parcels_interp_state",
-                    f"temporal_interpolation({args[3]}, {args[2]}, 0, time, H, &particles->xi[pnum*ngrid], &particles->yi[pnum*ngrid], &particles->zi[pnum*ngrid], &particles->ti[pnum*ngrid], &{node.var}, LINEAR, {node.field.obj.gridindexingtype.upper()})",
-                )
-            )
-            statements_croco.append(c.Statement(f"{node.var} = {args[1]}/{node.var}"))
+                    f"temporal_interpolation({args[3]}, {args[2]}, 0, time, H, &particles->xi[pnum*ngrid], &particles->yi[pnum*ngrid], &particles->zi[pnum*ngrid], &particles->ti[pnum*ngrid], &local_h, LINEAR, {node.field.obj.gridindexingtype.upper()})",
+                ),
+                c.Assign(
+                    "parcels_interp_state",
+                    f"temporal_interpolation({args[3]}, {args[2]}, 0, time, Zeta, &particles->xi[pnum*ngrid], &particles->yi[pnum*ngrid], &particles->zi[pnum*ngrid], &particles->ti[pnum*ngrid], &local_zeta, LINEAR, {node.field.obj.gridindexingtype.upper()})",
+                ),
+                c.Assign("CStructuredGrid *grid", "U->grid->grid"),
+                c.Assign("float *sigma_levels", "grid->depth"),
+                c.Assign("int zdim", "grid->zdim"),
+                c.Value("float", "zvec[zdim]"),
+                c.Value("float", "z0"),
+                c.Value("int", "zi"),
+                c.Statement(f"float cs_w[] = {*Cs_w, }".replace("(", "{").replace(")", "}")),
+                c.For(
+                    "zi = 0",
+                    "zi < zdim",
+                    "zi++",
+                    c.Block(
+                        [
+                            c.Assign("z0", "hc*sigma_levels[zi] + (local_h - hc) *cs_w[zi]"),
+                            c.Assign("zvec[zi]", "z0 + local_zeta * (1 + z0 / local_h)"),
+                        ]
+                    ),
+                ),
+                c.Assign("double z", f"{args[1]}"),
+                c.If(
+                    "z >= zvec[zdim-1]",
+                    c.Assign("zi", "zdim - 2"),
+                    c.For(
+                        "zi = 0",
+                        "zi < zdim-1",
+                        "zi++",
+                        c.Block([c.If("(z >= zvec[zi])  && (z < zvec[zi+1])", c.Statement("break"))]),
+                    ),
+                ),
+                c.Statement(
+                    f"{node.var} = sigma_levels[zi] + (z - zvec[zi]) * (sigma_levels[zi + 1] - sigma_levels[zi]) / (zvec[zi + 1] - zvec[zi])"
+                ),
+            ]
             args = (args[0], node.var, args[2], args[3])
+        else:
+            statements_croco = []
         ccode_eval = node.field.obj._ccode_eval(node.var, *args)
         stmts = [
             c.Assign("parcels_interp_state", ccode_eval),
