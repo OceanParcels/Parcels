26b133fe61f7c3fdefd70956e2bf49e60d16c375
Erik van Sebille <erikvansebille@gmail.com>
2025-01-06 09:51:51 +0100

    Fixing mypy attempt

--- parcels/particlefile.py
+++ parcels/particlefile.py
@@ -351,7 +351,7 @@ class ParticleFile:
                 else:
                     store = zarr.DirectoryStore(self.fname)
                 Z = zarr.group(store=store, overwrite=False)
-                obs = pset.particledata.getvardata("obs_written", indices_to_write)  # type: ignore[type-var]
+                obs = pset.particledata.getvardata("obs_written", indices_to_write)
                 for var in self.vars_to_write:
                     varout = self._convert_varout_name(var)
                     if self._maxids > Z[varout].shape[0]:
@@ -360,7 +360,7 @@ class ParticleFile:
                         if len(once_ids) > 0:
                             Z[varout].vindex[ids_once] = pset.particledata.getvardata(var, indices_to_write_once)
                     else:
-                        if max(obs) >= Z[varout].shape[1]:
+                        if max(obs) >= Z[varout].shape[1]:  # type: ignore[type-var]
                             self._extend_zarr_dims(Z[varout], store, dtype=self.vars_to_write[var], axis=1)
                         Z[varout].vindex[ids, obs] = pset.particledata.getvardata(var, indices_to_write)
 
