08f560c27344b0bd5d6fc8a37578359a718696fc
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-01-07 13:35:54 +0100

    Rename parse_particletime

--- parcels/tools/statuscodes.py
+++ parcels/tools/statuscodes.py
@@ -72,16 +72,14 @@ def _raise_out_of_bound_surface_error(z, y, x, field=None):
 class TimeExtrapolationError(RuntimeError):
     """Utility error class to propagate erroneous time extrapolation sampling."""
 
-    def __init__(self, time, field=None, msg="allow_time_extrapoltion"):
+    def __init__(self, time, field=None, msg=""):
         if field is not None and field.grid.time_origin and time is not None:
             time = field.grid.time_origin.fulltime(time)
         message = f"{field.name if field else 'Field'} sampled outside time domain at time {time}."
-        if msg == "allow_time_extrapoltion":
-            message += " Try setting allow_time_extrapolation to True"
-        elif msg == "show_time":
-            message += " Try explicitly providing a 'show_time'"
+        if msg == "show_time":
+            message += " Try explicitly providing a 'show_time'."
         else:
-            message += msg + " Try setting allow_time_extrapolation to True"
+            message += msg + " Try setting allow_time_extrapolation to True."
         super().__init__(message)
 
 
@@ -92,7 +90,7 @@ class KernelError(RuntimeError):
         message = (
             f"{particle.state}\n"
             f"Particle {particle}\n"
-            f"Time: {parse_particletime(particle.time, fieldset)}\n"
+            f"Time: {_parse_particletime(particle.time, fieldset)}\n"
             f"timestep dt: {particle.dt}\n"
         )
         if msg:
@@ -100,7 +98,7 @@ class KernelError(RuntimeError):
         super().__init__(message)
 
 
-def parse_particletime(time, fieldset):
+def _parse_particletime(time, fieldset):
     if fieldset is not None and fieldset.time_origin:
         time = fieldset.time_origin.fulltime(time)
     return time
