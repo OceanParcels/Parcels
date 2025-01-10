637b1f374b7894610533e994ef22bd88ea5f300a
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-01-10 14:12:57 +0100

    remove unused _latlondepth_dtype

--- parcels/particledata.py
+++ parcels/particledata.py
@@ -51,7 +51,6 @@ class ParticleData:
         self._offset = 0
         self._pclass = None
         self._ptype = None
-        self._latlondepth_dtype = np.float32
         self._data = None
 
         if depth is None:
