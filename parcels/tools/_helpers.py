92b3ef749173dea36490520e2b4bcc4f5e767cb6
Erik van Sebille <erikvansebille@gmail.com>
2025-03-12 12:14:17 +0100

    Removing type-checking

--- parcels/tools/_helpers.py
+++ parcels/tools/_helpers.py
@@ -154,6 +154,6 @@ def timedelta_to_float(dt: float | timedelta | np.timedelta64) -> float:
     return float(dt)
 
 
-def should_calculate_next_ti(ti: int, tau: float, tdim: int) -> np.bool:
+def should_calculate_next_ti(ti: int, tau: float, tdim: int):
     """Check if the time is beyond the last time in the field"""
     return not np.equal(tau, 0) and ti < tdim - 1
