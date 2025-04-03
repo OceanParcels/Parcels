9eb7aa5190b4ba6476f0ba761299371fd763adcc
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-04-03 13:48:46 +0200

    Review feedback

--- docs/conf.py
+++ docs/conf.py
@@ -207,7 +207,7 @@ html_theme_options = {
             "type": "fontawesome",
         }
     ],
-    "announcement": "Parcels is having a 10 year anniversary event on 1-3 Oct 2025! See <a href='https://oceanparcels.org/10year-event'>our website</a> or details.",
+    "announcement": "Join the Parcels 10 year anniversary event 🎉 on 1-3 Oct 2025! See <a href='https://oceanparcels.org/10year-event'>our website</a> for details.",
 }
 
 html_context = {
