5203e6cd1b06d08b4a190f640356f26022d6b572
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-06-05 21:11:43 +0200

    error message typo

--- parcels/field.py
+++ parcels/field.py
@@ -155,7 +155,7 @@ class Field:
         if not isinstance(name, str):
             raise ValueError(f"Expected `name` to be a string, got {type(name)}.")
         if not isinstance(grid, (UxGrid, XGrid)):
-            raise ValueError(f"Expected `grid` to be a parcels UxGrid, or parcels Grid object, got {type(grid)}.")
+            raise ValueError(f"Expected `grid` to be a parcels UxGrid, or parcels XGrid object, got {type(grid)}.")
 
         assert_valid_mesh(mesh_type)
 
