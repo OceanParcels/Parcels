8d6cc6cd68fe8666d55d40b05ec44a9ebaa1273b
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-06-12 11:27:42 +0200

    Add assert_valid_lon_lat to xgrid.py and update helpers

--- parcels/xgrid.py
+++ parcels/xgrid.py
@@ -192,12 +192,7 @@ def get_position_from_dim_name(axes: _XGCM_AXES, dim: str) -> _AXIS_POSITION | N
     return None
 
 
-def assert_valid_field_array(da: xr.DataArray, axes: _XGCM_AXES):
-    """
-    Asserts that for a data array:
-    - All dimensions are associated with a direction on the grid
-    - These directions are T, Z, Y, X and the array is ordered as T, Z, Y, X
-    """
+def assert_all_dimensions_correspond_with_axis(da: xr.DataArray, axes: _XGCM_AXES) -> None:
     dim_to_axis = {dim: get_axis_from_dim_name(axes, dim) for dim in da.dims}
 
     for dim, direction in dim_to_axis.items():
@@ -206,6 +201,16 @@ def assert_valid_field_array(da: xr.DataArray, axes: _XGCM_AXES):
                 f"Dimension {dim!r} for DataArray {da.name!r} with dims {da.dims} is not associated with a direction on the provided grid."
             )
 
+
+def assert_valid_field_array(da: xr.DataArray, axes: _XGCM_AXES):
+    """
+    Asserts that for a data array:
+    - All dimensions are associated with a direction on the grid
+    - These directions are T, Z, Y, X and the array is ordered as T, Z, Y, X
+    """
+    assert_all_dimensions_correspond_with_axis(da, axes)
+
+    dim_to_axis = {dim: get_axis_from_dim_name(axes, dim) for dim in da.dims}
     dim_to_axis = cast(dict[Hashable, _AXIS_DIRECTION], dim_to_axis)
 
     # Assert all dimensions are present
@@ -227,7 +232,7 @@ def assert_valid_lon_lat(da_lon, da_lat, axes: _XGCM_AXES):
     Asserts that the provided longitude and latitude DataArrays are defined appropriately
     on the F points to match the internal representation in Parcels.
 
-    - Longitude and latitude must be 1D or 2D
+    - Longitude and latitude must be 1D or 2D (both must have the same dimensionality)
     - Both are defined on the left points (i.e., not the centers)
     - If 1D:
       - Longitude is associated with the X axis
@@ -236,4 +241,46 @@ def assert_valid_lon_lat(da_lon, da_lat, axes: _XGCM_AXES):
       - Lon and lat are defined on the same dimensions
       - Lon and lat are transposed such they're Y, X
     """
-    ...
+    assert_all_dimensions_correspond_with_axis(da_lon, axes)
+    assert_all_dimensions_correspond_with_axis(da_lat, axes)
+
+    dim_to_position = {dim: get_position_from_dim_name(axes, dim) for dim in da_lon.dims}
+    dim_to_position.update({dim: get_position_from_dim_name(axes, dim) for dim in da_lat.dims})
+
+    for dim in da_lon.dims:
+        if get_position_from_dim_name(axes, dim) == "center":
+            raise ValueError(
+                f"Longitude DataArray {da_lon.name!r} with dims {da_lon.dims} is defined on the center of the grid, but must be defined on the F points."
+            )
+    for dim in da_lat.dims:
+        if get_position_from_dim_name(axes, dim) == "center":
+            raise ValueError(
+                f"Latitude DataArray {da_lat.name!r} with dims {da_lat.dims} is defined on the center of the grid, but must be defined on the F points."
+            )
+
+    if da_lon.ndim != da_lat.ndim:
+        raise ValueError(
+            f"Longitude DataArray {da_lon.name!r} with dims {da_lon.dims} and Latitude DataArray {da_lat.name!r} with dims {da_lat.dims} have different dimensionalities."
+        )
+    if da_lon.ndim not in (1, 2):
+        raise ValueError(
+            f"Longitude DataArray {da_lon.name!r} with dims {da_lon.dims} and Latitude DataArray {da_lat.name!r} with dims {da_lat.dims} must be 1D or 2D."
+        )
+
+    if da_lon.ndim == 1:
+        if get_axis_from_dim_name(da_lon.dims[0]) != "X":
+            raise ValueError(
+                f"Longitude DataArray {da_lon.name!r} with dims {da_lon.dims} is not associated with the X axis."
+            )
+        if get_axis_from_dim_name(da_lat.dims[0]) != "Y":
+            raise ValueError(
+                f"Latitude DataArray {da_lat.name!r} with dims {da_lat.dims} is not associated with the Y axis."
+            )
+
+    if da_lon.ndim == 2:
+        lon_axes = [get_axis_from_dim_name(dim) for dim in da_lon.dims]
+        lat_axes = [get_axis_from_dim_name(dim) for dim in da_lat.dims]
+        if lon_axes != lat_axes != ["Y", "X"]:
+            raise ValueError(
+                f"Longitude DataArray {da_lon.name!r} with dims {da_lon.dims} and Latitude DataArray {da_lat.name!r} with dims {da_lat.dims} must be defined on the X and Y axes and transposed to have dimensions in order of Y, X."
+            )
