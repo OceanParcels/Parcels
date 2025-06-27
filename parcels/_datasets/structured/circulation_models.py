a1c73fd9dc3db81ad24941e30c72173f1d3333cd
Erik van Sebille <erikvansebille@gmail.com>
2025-06-27 16:52:15 +0200

    adding metadata to copernicusmarine coordinates

--- parcels/_datasets/structured/circulation_models.py
+++ parcels/_datasets/structured/circulation_models.py
@@ -18,10 +18,50 @@ datasets = {
             "vo": (["depth", "latitude", "longitude", "time"], np.random.rand(T, Z, Y, X)),
         },
         coords={
-            "depth": (["depth"], np.linspace(0.49, 5727.92, Z), {"axis": "Z"}),
-            "latitude": (["latitude"], np.linspace(-90, 90, Y), {"axis": "Y"}),
-            "longitude": (["longitude"], np.linspace(-180, 180, X), {"axis": "X"}),
-            "time": (["time"], TIME, {"axis": "T"}),
+            "depth": (
+                ["depth"],
+                np.linspace(0.49, 5727.92, Z),
+                {
+                    "unit_long": "Meters",
+                    "units": "m",
+                    "axis": "Z",
+                    "long_name": "depth",
+                    "standard_name": "depth",
+                    "positive": "down",
+                },
+            ),
+            "latitude": (
+                ["latitude"],
+                np.linspace(-90, 90, Y),
+                {
+                    "unit_long": "Degrees North",
+                    "units": "degrees_north",
+                    "axis": "Y",
+                    "long_name": "Latitude",
+                    "standard_name": "latitude",
+                },
+            ),
+            "longitude": (
+                ["longitude"],
+                np.linspace(-180, 180, X),
+                {
+                    "unit_long": "Degrees East",
+                    "units": "degrees_east",
+                    "axis": "X",
+                    "long_name": "Longitude",
+                    "standard_name": "longitude",
+                },
+            ),
+            "time": (
+                ["time"],
+                TIME,
+                {
+                    "unit_long": "Hours Since 1950-01-01",
+                    "axis": "T",
+                    "long_name": "Time",
+                    "standard_name": "time",
+                },
+            ),
         },
     ),
 }
