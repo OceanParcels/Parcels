1dd2eef6c96315314c40a796ca69347d34d0bb37
Erik van Sebille <erikvansebille@gmail.com>
2025-06-27 16:14:12 +0200

    Adding ds_copernicusmarine to circulation_models dataset
    Adding a circulation model dataset mimicking the layout of copernicusmarine

---
+++ parcels/_datasets/structured/circulation_models.py
@@ -0,0 +1,27 @@
+"""Datasets mimicking the layout of real-world hydrodynamic models"""
+
+import numpy as np
+import xarray as xr
+
+from . import T, X, Y, Z
+
+__all__ = ["T", "X", "Y", "Z", "datasets"]
+
+TIME = xr.date_range("2000", "2001", T)
+
+
+datasets = {
+    "ds_copernicusmarine": xr.Dataset(
+        # Copernicus Marine Service dataset as retrieved by the `copernicusmarine` toolkit
+        {
+            "uo": (["depth", "latitude", "longitude", "time"], np.random.rand(Z, Y, X, T)),
+            "vo": (["depth", "latitude", "longitude", "time"], np.random.rand(Z, Y, X, T)),
+        },
+        coords={
+            "depth": (["depth"], np.linspace(0, 5000, Z), {"axis": "Z"}),
+            "latitude": (["latitude"], np.linspace(-90, 90, Y), {"axis": "Y"}),
+            "longitude": (["longitude"], np.linspace(-180, 180, X), {"axis": "X"}),
+            "time": (["time"], TIME, {"axis": "T"}),
+        },
+    ),
+}
