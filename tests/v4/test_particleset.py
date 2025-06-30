f2531bf0a3e1100e14cdff7c600c3e4c7fe5432c
Joe Schoonover <joe@fluidnumerics.com>
2025-06-30 11:23:10 -0400

    [#2034] Add test for particleset.execute on unstructure stommel gyre

---
+++ tests/v4/test_particleset.py
@@ -0,0 +1,50 @@
+from datetime import timedelta
+
+from parcels import (
+    AdvectionEE,
+    Field,
+    FieldSet,
+    Particle,
+    ParticleSet,
+    UXPiecewiseConstantFace,
+    VectorField,
+)
+from parcels._datasets.unstructured.generic import datasets as datasets_unstructured
+from parcels.uxgrid import UxGrid
+
+
+def test_uxstommel_gyre():
+    ds = datasets_unstructured["stommel_gyre_delaunay"]
+    grid = UxGrid(grid=ds.uxgrid, z=ds.coords["nz"])
+    U = Field(
+        name="U",
+        data=ds.U,
+        grid=grid,
+        mesh_type="spherical",
+        interp_method=UXPiecewiseConstantFace,
+    )
+    V = Field(
+        name="V",
+        data=ds.V,
+        grid=grid,
+        mesh_type="spherical",
+        interp_method=UXPiecewiseConstantFace,
+    )
+    P = Field(
+        name="P",
+        data=ds.p,
+        grid=grid,
+        mesh_type="spherical",
+        interp_method=UXPiecewiseConstantFace,
+    )
+    UV = VectorField(name="UV", U=U, V=V)
+    fieldset = FieldSet([UV, UV.U, UV.V, P])
+    pset = ParticleSet(
+        fieldset,
+        lon=[30.0],
+        lat=[5.0],
+        depth=[50.0],
+        time=[0.0],  # important otherwise initialization appears to take forever?
+        pclass=Particle,
+    )
+    pset.execute(endtime=timedelta(minutes=10), dt=timedelta(seconds=60), pyfunc=AdvectionEE, verbose_progress=False)
