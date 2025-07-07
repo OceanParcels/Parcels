c7b30c1ab9e41111886e3bf203779915729538df
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-07-07 11:22:08 +0200

    refactor

--- parcels_tutorial.py
+++ parcels_tutorial.py
@@ -1,8 +1,43 @@
+import tempfile
+from dataclasses import dataclass
 from datetime import timedelta
 
 import xarray as xr
 
 import parcels
+
+
+@dataclass
+class Params:
+    outputdt: timedelta
+    runtime: timedelta = None
+    endtime: timedelta = None
+    dt: timedelta
+
+
+def execute_particles(fieldset, lon, lat, params: Params) -> xr.Dataset:
+    pset = parcels.ParticleSet.from_list(
+        fieldset=fieldset,
+        pclass=parcels.JITParticle,
+        lon=lon,
+        lat=lat,
+    )
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        output_file = pset.ParticleFile(
+            name=f"{tmpdir}/EddyParticles.zarr",
+            outputdt=params.outputdt,
+        )
+
+        pset.execute(
+            parcels.AdvectionRK4,
+            runtime=params.runtime,
+            dt=params.dt,
+            output_file=output_file,
+        )
+
+        return xr.open_zarr(f"{tmpdir}/EddyParticles.zarr").load()
+
 
 example_dataset_folder = parcels.download_example_dataset("MovingEddies_data")
 
@@ -16,23 +51,17 @@ fieldset = parcels.FieldSet.from_netcdf(filenames, variables, dimensions)
 
 fieldset.computeTimeChunk()
 
-pset = parcels.ParticleSet.from_list(
-    fieldset=fieldset,
-    pclass=parcels.JITParticle,
-    lon=[3.3e5, 3.3e5],
-    lat=[1e5, 2.8e5],
-)
-
-output_file = pset.ParticleFile(
-    name="EddyParticles.zarr",
+params = Params(
     outputdt=timedelta(hours=1),
-)
-
-pset.execute(
-    parcels.AdvectionRK4,
     runtime=timedelta(days=6),
     dt=timedelta(minutes=5),
-    output_file=output_file,
 )
 
-ds = xr.open_zarr("EddyParticles.zarr")
+
+ds = execute_particles(
+    fieldset,
+    lon=[3.3e5, 3.3e5],
+    lat=[1e5, 2.8e5],
+    params=params,
+)
+print(ds)
