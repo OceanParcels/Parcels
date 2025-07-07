ee4817de086d669c730a76ff3a3c3897e204620c
Erik van Sebille <erikvansebille@gmail.com>
2025-07-07 16:38:16 +0200

    Using verbose_print function

--- parcels/_datasets/utils.py
+++ parcels/_datasets/utils.py
@@ -86,31 +86,32 @@ def _dicts_equal(d1, d2):
 def compare_datasets(ds1, ds2, ds1_name="Dataset 1", ds2_name="Dataset 2", verbose=True):
     print(f"Comparing {ds1_name} and {ds2_name}\n")
 
-    if verbose:
-        print("Dataset Attributes Comparison:")
-        if ds1.attrs == ds2.attrs:
-            print("  Dataset attributes are identical.")
-        else:
-            print("  Dataset attributes differ.")
-            for attr_name in set(ds1.attrs.keys()) | set(ds2.attrs.keys()):
-                if attr_name not in ds1.attrs:
-                    print(f"    Attribute '{attr_name}' only in {ds2_name}")
-                elif attr_name not in ds2.attrs:
-                    print(f"    Attribute '{attr_name}' only in {ds1_name}")
-                elif ds1.attrs[attr_name] != ds2.attrs[attr_name]:
-                    print(f"    Attribute '{attr_name}' differs:")
-                    print(f"      {ds1_name}: {ds1.attrs[attr_name]}")
-                    print(f"      {ds2_name}: {ds2.attrs[attr_name]}")
-        print("-" * 30)
+    def verbose_print(*args, **kwargs):
+        if verbose:
+            print(*args, **kwargs)
+
+    verbose_print("Dataset Attributes Comparison:")
+    if ds1.attrs == ds2.attrs:
+        verbose_print("  Dataset attributes are identical.")
+    else:
+        print("  Dataset attributes differ.")
+        for attr_name in set(ds1.attrs.keys()) | set(ds2.attrs.keys()):
+            if attr_name not in ds1.attrs:
+                print(f"    Attribute '{attr_name}' only in {ds2_name}")
+            elif attr_name not in ds2.attrs:
+                print(f"    Attribute '{attr_name}' only in {ds1_name}")
+            elif ds1.attrs[attr_name] != ds2.attrs[attr_name]:
+                print(f"    Attribute '{attr_name}' differs:")
+                print(f"      {ds1_name}: {ds1.attrs[attr_name]}")
+                print(f"      {ds2_name}: {ds2.attrs[attr_name]}")
+    verbose_print("-" * 30)
 
     # Compare dimensions
-    if verbose:
-        print("Dimensions Comparison:")
+    verbose_print("Dimensions Comparison:")
     ds1_dims = set(ds1.dims)
     ds2_dims = set(ds2.dims)
     if ds1_dims == ds2_dims:
-        if verbose:
-            print("  Dimension names are identical.")
+        verbose_print("  Dimension names are identical.")
     else:
         print("  Dimension names differ:")
         print(f"    {ds1_name} dims: {sorted(list(ds1_dims))}")
@@ -119,11 +120,9 @@ def compare_datasets(ds1, ds2, ds1_name="Dataset 1", ds2_name="Dataset 2", verbo
     # For common dimensions, compare order (implicit by comparing coordinate values for sortedness)
     # and size (though size is parameterized and expected to be different)
     for dim_name in ds1_dims.intersection(ds2_dims):
-        if verbose:
-            print(f"  Dimension '{dim_name}':")
+        verbose_print(f"  Dimension '{dim_name}':")
         # Sizes will differ due to DIM_SIZE, so we don't strictly compare them.
-        if verbose:
-            print(f"    {ds1_name} size: {ds1.dims[dim_name]}, {ds2_name} size: {ds2.dims[dim_name]}")
+        verbose_print(f"    {ds1_name} size: {ds1.dims[dim_name]}, {ds2_name} size: {ds2.dims[dim_name]}")
         # Check if coordinates associated with dimensions are sorted (increasing)
         if dim_name in ds1.coords and dim_name in ds2.coords:
             check_val = (
@@ -136,24 +135,20 @@ def compare_datasets(ds1, ds2, ds1_name="Dataset 1", ds2_name="Dataset 2", verbo
                 np.all(np.diff(ds2[dim_name].values) >= check_val) if len(ds2[dim_name].values) > 1 else True
             )
             if is_ds1_sorted == is_ds2_sorted:
-                if verbose:
-                    print(f"    Order for '{dim_name}' is consistent (both sorted: {is_ds1_sorted})")
+                verbose_print(f"    Order for '{dim_name}' is consistent (both sorted: {is_ds1_sorted})")
             else:
                 print(
                     f"    Order for '{dim_name}' differs: {ds1_name} sorted: {is_ds1_sorted}, {ds2_name} sorted: {is_ds2_sorted}"
                 )
-    if verbose:
-        print("-" * 30)
+    verbose_print("-" * 30)
 
     # Compare variables (name, attributes, dimensions used)
-    if verbose:
-        print("Variables Comparison:")
+    verbose_print("Variables Comparison:")
     ds1_vars = set(ds1.variables.keys())
     ds2_vars = set(ds2.variables.keys())
 
     if ds1_vars == ds2_vars:
-        if verbose:
-            print("  Variable names are identical.")
+        verbose_print("  Variable names are identical.")
     else:
         print("  Variable names differ:")
         print(f"    {ds1_name} vars: {sorted(list(ds1_vars - ds2_vars))}")
@@ -161,15 +156,13 @@ def compare_datasets(ds1, ds2, ds1_name="Dataset 1", ds2_name="Dataset 2", verbo
         print(f"    Common vars: {sorted(list(ds1_vars.intersection(ds2_vars)))}")
 
     for var_name in ds1_vars.intersection(ds2_vars):
-        if verbose:
-            print(f"  Variable '{var_name}':")
+        verbose_print(f"  Variable '{var_name}':")
         var1 = ds1[var_name]
         var2 = ds2[var_name]
 
         # Compare attributes
         if _dicts_equal(var1.attrs, var2.attrs):
-            if verbose:
-                print("    Attributes are identical.")
+            verbose_print("    Attributes are identical.")
         else:
             print("    Attributes differ.")
             for attr_name in set(var1.attrs.keys()) | set(var2.attrs.keys()):
@@ -184,11 +177,9 @@ def compare_datasets(ds1, ds2, ds1_name="Dataset 1", ds2_name="Dataset 2", verbo
 
         # Compare dimensions used by the variable
         if var1.dims == var2.dims:
-            if verbose:
-                print(f"    Dimensions used are identical: {var1.dims}")
+            verbose_print(f"    Dimensions used are identical: {var1.dims}")
         else:
             print("    Dimensions used differ:")
             print(f"      {ds1_name}: {var1.dims}")
             print(f"      {ds2_name}: {var2.dims}")
-    if verbose:
-        print("=" * 30 + " End of Comparison " + "=" * 30)
+    verbose_print("=" * 30 + " End of Comparison " + "=" * 30)
