67ee87db05572179fabd35028c3e6cedcafbe538
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-07-07 13:33:23 +0200

    update to proper test functions

--- tests/test_particleset_execute.py
+++ tests/test_particleset_execute.py
@@ -10,9 +10,9 @@ import parcels
 @dataclass
 class Params:
     outputdt: timedelta
+    dt: timedelta
     runtime: timedelta = None
     endtime: timedelta = None
-    dt: timedelta
 
 
 def execute_particles(fieldset, lon, lat, params: Params) -> xr.Dataset:
@@ -54,44 +54,162 @@ def get_some_fieldset():
     return fieldset
 
 
-params = Params(
-    outputdt=timedelta(hours=1),
-    runtime=timedelta(days=6),
-    dt=timedelta(minutes=5),
-)
-
-runs = {
-    ("run1", r"If runtime % outputdt==0 and runtime % dt == 0"): Params(...),
-    ("run2", r"If runtime % outputdt==0 and runtime % dt != 0"): Params(...),
-    ("run3", r"If runtime % outputdt!=0 and runtime % dt == 0"): Params(...),
-    ("run4", r"If runtime % outputdt!=0 and runtime % dt != 0"): Params(...),
-    ("siren", r"what if outputdt is not a multiple of dt?"): Params(...),
-    ("run5", r"runtime is 0"): Params(...),
-    ("run6", r"dt is 0"): Params(...),
-    ("run7", r"outputdt > runtime"): Params(...),
-    ("run8", r"outputdt < dt"): Params(...),
-    (
-        "run9",
-        "If you have a timed particle release, is the runtime the time "
-        "from the first particle release or from the first fieldset time?",
-    ): ...,
-}
-#
-# outputdt is tied to the particle release -> if the first particle is released at 43min with outputdt=1h then all subsequent particles will be recorded at 43min
+def test_runtime_and_outputdt_evenly_divisible():
+    """Test case where runtime % outputdt==0 and runtime % dt == 0"""
+    params = Params(
+        outputdt=timedelta(hours=1),
+        runtime=timedelta(hours=6),  # divisible by both outputdt and dt
+        dt=timedelta(minutes=30),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
 
 
-# How is the particle writing done with adding and deleting particles to the simulation?
-# - Are the initial particle locations written?
-# - if you delete particles, is the deletion point recorded?
-#
+def test_runtime_divisible_by_outputdt_only():
+    """Test case where runtime % outputdt==0 and runtime % dt != 0"""
+    params = Params(
+        outputdt=timedelta(hours=1),
+        runtime=timedelta(hours=6),
+        dt=timedelta(minutes=17),  # not evenly divisible
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
 
-# Usecases:
-# 1. Meike -> intialisation and then running the simulation
 
-ds = execute_particles(
-    get_some_fieldset(),
-    lon=[3.3e5, 3.3e5],
-    lat=[1e5, 2.8e5],
-    params=params,
-)
-print(ds)
+def test_runtime_divisible_by_dt_only():
+    """Test case where runtime % outputdt!=0 and runtime % dt == 0"""
+    params = Params(
+        outputdt=timedelta(minutes=45),
+        runtime=timedelta(hours=2),  # divisible by dt but not outputdt
+        dt=timedelta(minutes=30),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_no_even_divisions():
+    """Test case where runtime % outputdt!=0 and runtime % dt != 0"""
+    params = Params(
+        outputdt=timedelta(minutes=45),
+        runtime=timedelta(minutes=137),  # prime number of minutes
+        dt=timedelta(minutes=17),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_outputdt_not_multiple_of_dt():
+    """Test case where outputdt is not a multiple of dt"""
+    params = Params(
+        outputdt=timedelta(minutes=17),
+        runtime=timedelta(hours=1),
+        dt=timedelta(minutes=5),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_zero_runtime():
+    """Test case where runtime is 0"""
+    params = Params(
+        outputdt=timedelta(hours=1),
+        runtime=timedelta(0),
+        dt=timedelta(minutes=5),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_zero_dt():
+    """Test case where dt is 0"""
+    params = Params(
+        outputdt=timedelta(hours=1),
+        runtime=timedelta(hours=6),
+        dt=timedelta(0),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_outputdt_greater_than_runtime():
+    """Test case where outputdt > runtime"""
+    params = Params(
+        outputdt=timedelta(hours=2),
+        runtime=timedelta(hours=1),
+        dt=timedelta(minutes=5),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_outputdt_less_than_dt():
+    """Test case where outputdt < dt"""
+    params = Params(
+        outputdt=timedelta(minutes=1),
+        runtime=timedelta(hours=1),
+        dt=timedelta(minutes=5),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
+
+
+def test_timed_particle_release():
+    """Test to verify if runtime is from first particle release or first fieldset time"""
+    # TODO: This test needs additional setup for timed particle release
+    params = Params(
+        outputdt=timedelta(hours=1),
+        runtime=timedelta(hours=6),
+        dt=timedelta(minutes=5),
+    )
+    ds = execute_particles(
+        get_some_fieldset(),
+        lon=[3.3e5, 3.3e5],
+        lat=[1e5, 2.8e5],
+        params=params,
+    )
+    # TODO: Add validation code here
