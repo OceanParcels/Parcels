5b6cd83836a4a256a87e73ab0d9b6b757a30fb39
Erik van Sebille <erikvansebille@gmail.com>
2025-07-21 12:34:10 +0200

    speeding up time_search by calling numpy arrays directly

--- parcels/_index_search.py
+++ parcels/_index_search.py
@@ -44,28 +44,28 @@ def _search_time_index(field: Field, time: datetime):
     if time not in field.time_interval:
         _raise_time_extrapolation_error(time, field=None)
 
-    time_index = field.data.time <= time
+    time_index = field.data.time.data <= time
 
     if time_index.all():
         # If given time > last known field time, use
         # the last field frame without interpolation
-        ti = len(field.data.time) - 1
-
+        ti = len(field.data.time.data) - 1
     elif np.logical_not(time_index).all():
         # If given time < any time in the field, use
         # the first field frame without interpolation
         ti = 0
     else:
         ti = int(time_index.argmin() - 1) if time_index.any() else 0
-    if len(field.data.time) == 1:
+
+    if len(field.data.time.data) == 1:
         tau = 0
-    elif ti == len(field.data.time) - 1:
+    elif ti == len(field.data.time.data) - 1:
         tau = 1
     else:
         tau = (
-            (time - field.data.time[ti]).dt.total_seconds()
-            / (field.data.time[ti + 1] - field.data.time[ti]).dt.total_seconds()
-            if field.data.time[ti] != field.data.time[ti + 1]
+            (time - field.data.time.data[ti])
+            / (field.data.time.data[ti + 1] - field.data.time.data[ti])
+            if field.data.time.data[ti] != field.data.time.data[ti + 1]
             else 0
         )
     return tau, ti
