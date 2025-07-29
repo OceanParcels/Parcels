f0ef29cb70b6a67b1b1764ad1521b12805156f5e
Erik van Sebille <e.vansebille@uu.nl>
2025-07-29 07:42:05 +0200

    Update tests/v4/test_advection.py
    Co-authored-by: Nick Hodgskin <36369090+VeckoTheGecko@users.noreply.github.com>

--- tests/v4/test_advection.py
+++ tests/v4/test_advection.py
@@ -133,7 +133,7 @@ def test_advection_3D_outofbounds(direction, wErrorThroughSurface):
 @pytest.mark.parametrize("u", [-0.3, np.array(0.2)])
 @pytest.mark.parametrize("v", [0.2, np.array(1)])
 @pytest.mark.parametrize("w", [None, -0.2, np.array(0.7)])
-def test_length1dimensions(u, v, w):
+def test_length1dimensions(u, v, w): # TODO: Refactor this test to be more readable (and isolate test setup)
     (lon, xdim) = (np.linspace(-10, 10, 21), 21) if isinstance(u, np.ndarray) else (np.array([0]), 1)
     (lat, ydim) = (np.linspace(-15, 15, 31), 31) if isinstance(v, np.ndarray) else (np.array([-4]), 1)
     (depth, zdim) = (
