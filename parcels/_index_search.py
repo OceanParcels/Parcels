4a499cb808e660dc3e9f7e1ba480a6e834a814dc
Joe Schoonover <11430768+fluidnumerics-joe@users.noreply.github.com>
2025-08-13 08:02:49 -0400

    Revert "2119 spatial hashing"

--- parcels/_index_search.py
+++ parcels/_index_search.py
@@ -275,9 +275,11 @@ def _search_indices_curvilinear_2d(
     grid: XGrid, y: float, x: float, yi_guess: int | None = None, xi_guess: int | None = None
 ):
     yi, xi = yi_guess, xi_guess
-    if yi is None or xi is None:
-        faces = grid.get_spatial_hash().query(np.column_stack((y, x)))
-        yi, xi = faces[0]
+    if yi is None:
+        yi = int(grid.ydim / 2) - 1
+
+    if xi is None:
+        xi = int(grid.xdim / 2) - 1
 
     xsi = eta = -1.0
     invA = np.array(
@@ -348,6 +350,7 @@ def _search_indices_curvilinear_2d(
 
     if not ((0 <= xsi <= 1) and (0 <= eta <= 1)):
         _raise_field_sampling_error(y, x)
+
     return (yi, eta, xi, xsi)
 
 
--- tests/v4/test_spatialhash.py
+++
@@ -1,9 +0,0 @@
-from parcels._datasets.structured.generic import datasets
-from parcels.xgrid import XGrid
-
-
-def test_spatialhash_init():
-    ds = datasets["2d_left_rotated"]
-    grid = XGrid.from_dataset(ds)
-    spatialhash = grid.get_spatial_hash()
-    assert spatialhash is not None
