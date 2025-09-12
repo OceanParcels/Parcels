8c4aaed5a6e7d12d700c03f5a501de1b4375719d
Nick Hodgskin <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-12 17:17:18 +0200

    Add Parcels v4 warning at import (#2209)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- parcels/__init__.py
+++ parcels/__init__.py
@@ -2,6 +2,8 @@ from ._version import version
 
 __version__ = version
 
+import warnings as _warnings
+
 from parcels.application_kernels import *
 from parcels.field import *
 from parcels.fieldset import *
@@ -11,3 +13,9 @@ from parcels.particle import *
 from parcels.particlefile import *
 from parcels.particleset import *
 from parcels.tools import *
+
+_warnings.warn(
+    "This is an alpha version of Parcels v4. The API is not stable and may change without deprecation warnings.",
+    UserWarning,
+    stacklevel=2,
+)
