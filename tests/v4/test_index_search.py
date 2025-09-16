40fd8ca05a159a7ca370fdd83602c6746b073a91
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-16 15:09:09 +0200

    Update import

--- tests/v4/test_index_search.py
+++ tests/v4/test_index_search.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 import xarray as xr
-from xgcm import Grid
+import xgcm
 
 from parcels._datasets.structured.generic import datasets
 from parcels._index_search import _search_indices_curvilinear_2d
@@ -62,7 +62,7 @@ def test_indexing_nemo_curvilinear():
     ds = ds.isel({"time_counter": 0, "time": 0, "z_a": 0}, drop=True).rename(
         {"glamf": "lon", "gphif": "lat", "z": "depth"}
     )
-    xgcm_grid = Grid(ds, coords={"X": {"left": "x"}, "Y": {"left": "y"}}, periodic=False)
+    xgcm_grid = xgcm.Grid(ds, coords={"X": {"left": "x"}, "Y": {"left": "y"}}, periodic=False)
     grid = XGrid(xgcm_grid, mesh="spherical")
 
     # Test points on the NEMO 1/4 degree curvilinear grid
