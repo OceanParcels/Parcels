bec618867e31e2a3317e38ca574b3a2fd770184d
Joe Schoonover <joe@fluidnumerics.com>
2025-09-25 07:57:41 -0400

    Clip searchsorted result to valid range
    When np.searchsorted doesn't find an index, it returns N, where N is the
    length of the array being searched. This causes indexing errors.
    
    The `valid` mask ensures we're not using invalid values where pos >=
    len(keys), however, since pos is still used in other indexing
    operations, we clip to avoid oob indexing errors

--- parcels/spatialhash.py
+++ parcels/spatialhash.py
@@ -339,7 +339,11 @@ class SpatialHash:
         pos = np.searchsorted(keys, query_codes)  # pos is shape (num_queries,)
 
         # Valid hits: inside range with finite query coordinates and query codes give exact morton code match.
-        valid = (pos < len(keys)) & np.isfinite(x) & np.isfinite(y) & (query_codes == keys[pos])
+        valid = (pos < len(keys)) & np.isfinite(x) & np.isfinite(y)
+        # Clip pos to valid range to avoid out-of-bounds indexing
+        pos = np.clip(pos, 0, len(keys) - 1)
+        # Further filter out false positives from searchsorted by checking for exact code match
+        valid[valid] &= query_codes[valid] == keys[pos[valid]]
 
         # Pre-allocate i and j indices of the best match for each query
         # Default values to -1 (no match case)
