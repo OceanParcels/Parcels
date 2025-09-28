04389ba02672c44f29f70199245abf9f8d9f77b9
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-28 13:42:17 +0200

    update broken import

--- parcels/fieldset.py
+++ parcels/fieldset.py
@@ -15,7 +15,7 @@ from parcels._logger import logger
 from parcels._typing import Mesh
 from parcels.field import Field, VectorField
 from parcels.converters import Geographic, GeographicPolar
-from parcels.xgrid import _DEFAULT_XGCM_KWARGS, XGrid
+from parcels._core.xgrid import _DEFAULT_XGCM_KWARGS, XGrid
 
 if TYPE_CHECKING:
     from parcels._typing import TimeLike
