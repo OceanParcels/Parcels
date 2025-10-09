2c9813222a9ae1dc9465fc99b14f410ad4efde71
Danilo Silva <nilodna@gmail.com>
2025-10-09 13:36:50 -0300

    cleaning old function

--- src/parcels/_core/utils/string.py
+++ src/parcels/_core/utils/string.py
@@ -1,17 +1,5 @@
 from keyword import iskeyword, kwlist
 
-# def _assert_valid_python_varname(name):
-#     try:
-#         if name.isidentifier():
-#             if not iskeyword(name):
-#                 return
-#             raise ValueError(f"Received invalid Python variable name {name!r}: it is a reserved keyword. Avoid using the following names: {', '.join(kwlist)}")
-#         else:
-#             raise ValueError(f"Received invalid Python variable name {name!r}: not a valid identifier.")
-#     except Exception as e:
-#         raise ValueError(f"Error validating variable name {name!r}: {e}")
-    
-
 def _assert_str_and_python_varname(name):
     if not isinstance(name, str):
         raise TypeError(f"Expected a string for variable name, got {type(name).__name__} instead.")
